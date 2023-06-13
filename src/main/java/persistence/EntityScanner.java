4ef62e21a4a78fa39badc7f74255ebf3230e85c5
wooobo <irooori89@gmail.com>
2023-06-13 22:41:51 +0900

    feat : `EntityScanner` 추가
    리플렉션을 사용하여 클래스정보 추출

--- src/main/java/persistence/EntityScanner.java
+++ src/main/java/persistence/EntityScanner.java
@@ -1,9 +1,31 @@
 package persistence;
 
+
+import jakarta.persistence.Id;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EntityScanner {
     private final Class<?> entity;
 
     public EntityScanner(Class<?> entity) {
         this.entity = entity;
+    }
+
+    public Table table() {
+        return new Table(entity.getSimpleName());
+    }
+
+    public Columns columns() {
+        return Arrays.stream(entity.getDeclaredFields())
+                .map(it -> Column.of(
+                        it.getName(),
+                        it.getType(),
+                        it.isAnnotationPresent(Id.class)
+                ))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Columns::new));
     }
 }
