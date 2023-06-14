82451fbca6a6458b4f84cb2da1679f862984816d
leeyohan93 <good_1411@naver.com>
2023-06-15 01:38:01 +0900

    feat: 데이터베이스 컬럼 과 매핑하는 Dialect 구현

---
+++ src/main/java/persistence/sql/ddl/h2/H2Dialect.java
@@ -0,0 +1,30 @@
+package persistence.sql.ddl.h2;
+
+import persistence.sql.ddl.Dialect;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class H2Dialect implements Dialect {
+
+
+    private final Map<Class<?>, String> h2TypeJavaClassMappings;
+
+    public H2Dialect() {
+        h2TypeJavaClassMappings = h2TypeJavaClassMappings();
+    }
+
+    private Map<Class<?>, String> h2TypeJavaClassMappings() {
+        ConcurrentHashMap<Class<?>, String> h2TypeJavaClassMap = new ConcurrentHashMap<>();
+        h2TypeJavaClassMap.put(Integer.class, "integer");
+        h2TypeJavaClassMap.put(Long.class, "bigint");
+        h2TypeJavaClassMap.put(String.class, "varchar");
+
+        return h2TypeJavaClassMap;
+    }
+
+    @Override
+    public String columnType(Class<?> clazz) {
+        return h2TypeJavaClassMappings.get(clazz);
+    }
+}
