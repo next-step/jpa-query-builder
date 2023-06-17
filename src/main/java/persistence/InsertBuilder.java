4a7c27fc48068a5de3a4ef83f47cd8f4ea74e1fe
wooobo <irooori89@gmail.com>
2023-06-17 09:57:42 +0900

    feat : `InsertBuilder` 빌더 추가

---
+++ src/main/java/persistence/InsertBuilder.java
@@ -0,0 +1,20 @@
+package persistence;
+
+public class InsertBuilder {
+    private static final String KEYWORD = "insert into %s (%s) values (%s)";
+    private final ColumnMap columnMap;
+    private final Table table;
+
+    public InsertBuilder(Table table, ColumnMap columnMap) {
+        this.columnMap = columnMap;
+        this.table = table;
+    }
+
+    public String query() {
+        return String.format(KEYWORD,
+                table.expression(),
+                String.join(",", columnMap.names()),
+                columnMap.values());
+    }
+
+}
