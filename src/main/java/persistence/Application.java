0f97a3de725487823433839b8e701a2129179688
leeyohan93 <good_1411@naver.com>
2023-06-18 20:19:51 +0900

    remove: 기존 코드 제거

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -2,14 +2,8 @@ package persistence;
 
 import database.DatabaseServer;
 import database.H2;
-import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import persistence.sql.ddl.ColumnMapper;
-import persistence.sql.ddl.Person;
-import persistence.sql.ddl.TableQueryMapper;
-import persistence.sql.ddl.h2.H2Dialect;
-import persistence.sql.ddl.h2.H2GeneratedValueStrategy;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -20,14 +14,7 @@ public class Application {
             final DatabaseServer server = new H2();
             server.start();
 
-            final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
-
-            final TableQueryMapper tableQueryMapper =
-                    new TableQueryMapper(new ColumnMapper(new H2Dialect()), new H2GeneratedValueStrategy());
-            String sql = tableQueryMapper.create(Person.class);
-            jdbcTemplate.execute(sql);
-
-            server.stop();
+//            server.stop();
         } catch (Exception e) {
             logger.error("Error occurred", e);
         } finally {
