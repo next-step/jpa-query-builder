e760c8aa9eb7778927aadefa744d798fd028a87c
seondongpyo <seondongpyo@gmail.com>
2023-06-19 17:09:40 +0900

    test: 실제 데이터베이스 서버에서의 DML 쿼리 생성 및 실행

--- src/main/java/persistence/sql/ddl/Person.java
+++ src/main/java/persistence/sql/ddl/Person.java
@@ -22,9 +22,52 @@ public class Person {
     @Transient
     private Integer index;
 
+    public Person() {
+    }
+
     public Person(String name, Integer age, String email) {
         this.name = name;
         this.age = age;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
     }
 }
