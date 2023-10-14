e604d226b5a9309e8bc3c39e3f464dadef877ccc
Jivebreaddev <jivebreaddev@gmail.com>
2023-10-14 15:40:31 +0900

    test(number): 간단한 test 로 push test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class StringTest {
 
   @Test
-  void convrt() {
+  void convert() {
     int number = 123;
     String numString = String.valueOf(number);
     assertThat(numString).isEqualTo("123");
