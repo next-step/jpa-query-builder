3063e3c1af9cd7261ae954e4689d184d634864af
beng9re <kbh0581@gmail.com>
2023-10-14 22:55:52 +0900

    test: 요구사항 1 - 클래스 정보 출력
    Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # jpa-query-builder
+
+
+
+## 1단계 - Reflection
+* **요구사항 1 - Car 클래스의 정보 출력**
+  - [X] src/test/java/persistence/study > Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+
---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,28 @@
+package persistence.study;
+
+
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+
+    @Test
+    @DisplayName("요구사항 1 - Car 클래스 정보(모든 필드, 생성자, 메소드) 출력")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+
+        logger.debug("Car 클래스의 모든 필드 {}",  Arrays.toString(carClass.getDeclaredFields()));
+        logger.debug("Car 클래스의 생성자 {}",  Arrays.toString(carClass.getDeclaredFields()));
+        logger.debug("Car 클래스의 메소드 {}",  Arrays.toString(carClass.getDeclaredFields()));
+    }
+
+}
