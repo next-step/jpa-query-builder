60a3313d7cde33f01879b2ba0e50d3111351f9ac
beng9re <kbh0581@gmail.com>
2023-10-14 23:40:00 +0900

    [ 1단계 ] - Reflection  PR 요청 (#42)
    * test: 요구사항 1 - 클래스 정보 출력
    
    Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
    
    * test: 요구사항 2 - Car 클래스의 test로 시작하는 메소드 실행
    
    * test: 요구사항 3 - @PrintView 애노테이션 메소드 실행
    
    * test: 요구사항 4 - private field에 값 할당
    
    * test: 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
    
    * chore: PrintView 어노테이션 타겟 메소드로 지정

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # jpa-query-builder
+
+
+
+## 1단계 - Reflection
+* **요구사항 1 - Car 클래스의 정보 출력**
+  - [X] src/test/java/persistence/study > Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+
+* **요구사항 2 - Car 클래스의 test로 시작하는 메소드 실행**
+  - [X] Car 객체의 메소드 중 test로 시작하는 메소드를 자동으로 실행한다. 
+  - [X] 같이 Car 클래스에서 test로 시작하는 메소드만 Java Reflection을 활용해 실행하도록 구현한다.
+  - [X] 구현은 src/test/java/persistence/study > ReflectionTest 클래스의 testMethodRun() 메소드에 한다.
+
+* **요구사항 3 - @PrintView 애노테이션 메소드 실행**
+  - [X] @PrintView애노테이션일 설정되어 있는 메소드를 자동으로 실행한다. 
+  - [X] Java Reflection을 활용해 실행하도록 구현한다.
+  - [X] 구현은 src/test/java/persistence/study > ReflectionTest 클래스의 testAnnotationMethodRun() 메소드에 한다.
+
+* **요구사항 4 - private field에 값 할당**
+  - [X] 자바 Reflection API를 활용해 다음 Car 클래스의 name과 price 필드에 값을 할당한 후 getter 메소드를 통해 값을 확인한다.
+  - [X] 구현은 ReflectionTest 클래스의 privateFieldAccess() 메소드에 한다.
+
+* **요구사항 5 - 인자를 가진 생성자의 인스턴스 생성**
+  - [X] Car 클래스의 인스턴스를 자바 Reflection API를 활용해 Car 인스턴스를 생성한다.
+  - [X] 구현은 src/test/java/persistence/study > ReflectionTest 클래스의 constructorWithArgs() 메소드에 한다.
---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,106 @@
+package persistence.study;
+
+
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+
+    @Test
+    @DisplayName("요구사항 1 - Car 클래스 정보(모든 필드, 생성자, 메소드) 출력")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+
+        logger.debug("Car 클래스의 모든 필드 {}",  Arrays.toString(carClass.getDeclaredFields()));
+        logger.debug("Car 클래스의 생성자 {}",  Arrays.toString(carClass.getDeclaredFields()));
+        logger.debug("Car 클래스의 메소드 {}",  Arrays.toString(carClass.getDeclaredFields()));
+    }
+
+    @Test
+    @DisplayName("요구사항 2 - test로 시작하는 메소드 실행")
+    void testMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+
+        Car car = carClass.getDeclaredConstructor().newInstance();
+
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                logger.debug("메소드 호출 {}", method.invoke(car));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("요구사항 3 - @PrintView 애노테이션 메소드 실행")
+    void testAnnotationMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+
+        Car car = carClass.getDeclaredConstructor().newInstance();
+
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(PrintView.class)) {
+                logger.debug("메소드 호출 {}", method.invoke(car));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("요구사항 4 - private field에 값 할당")
+    void privateFieldAccess() throws Exception {
+        //given
+        final String carName = "소나타";
+        final int price = 10;
+
+        //when
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getDeclaredConstructor().newInstance();
+
+        for (Field field : carClass.getDeclaredFields()) {
+            if (field.getName().equals("name")) {
+                field.setAccessible(true);
+                field.set(car, carName);
+            }
+            if (field.getName().equals("price")) {
+                field.setAccessible(true);
+                field.set(car, price);
+            }
+        }
+
+        //then
+        assertSoftly((it -> {
+            it.assertThat(car.testGetName()).isEqualTo("test : " + carName);
+            it.assertThat(car.testGetPrice()).contains("test : " + price);
+        }));
+    }
+
+    @Test
+    @DisplayName("요구사항 5 - 인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws Exception {
+        //given
+        final String carName = "소나타";
+        final int price = 10;
+
+        //when
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getConstructor(String.class, int.class).newInstance(carName, 10);
+
+        //then
+        assertSoftly((it -> {
+            it.assertThat(car.testGetName()).isEqualTo("test : " + carName);
+            it.assertThat(car.testGetPrice()).contains("test : " + price);
+        }));
+    }
+
+}
