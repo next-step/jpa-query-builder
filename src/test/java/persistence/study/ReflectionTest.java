0641a8bd931cd98732a2bc83b6ac81a18ac9ded7
limdingdong <38725277+limdingdong@users.noreply.github.com>
2023-10-15 00:03:13 +0900

    step 1-1) 1단계 - Reflection 리뷰요청 드립니다! (#38)
    * feat) 실습을 위한 Car, PrintView 클래스 추가
    
    * test) ReflectionTest 클래스 추가, showClass 테스트 작성
    
    * test) testMethodRun, testAnnotationMethodRun 테스트 작성
    
    * test) constructorWithArgs 테스트코드 작성
    
    ---------
    
    Co-authored-by: limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,107 @@
+package persistence.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static java.util.Arrays.stream;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("test 로 시작하는 메서드 실행")
+    void testMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+
+        Method[] declaredMethods = carClass.getDeclaredMethods();
+        List<Method> testMethods = stream(declaredMethods)
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+
+        Car car = carClass.getDeclaredConstructor().newInstance();
+        List<String> methodResults = testMethods.stream()
+                .map(testMethod -> {
+                    try {
+                        return (String) testMethod.invoke(car);
+                    } catch (Exception e) {
+                        fail("Exception occurred invoking test method : {}", testMethod.getName());
+                    }
+                    return null;
+                }).toList();
+
+        assertThat(methodResults.size()).isEqualTo(2);
+        assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"));
+    }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션 메서드 실행")
+    void testAnnotationMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+
+        Method[] declaredMethods = carClass.getDeclaredMethods();
+        List<Method> annotationMethods = stream(declaredMethods)
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .toList();
+
+        Car car = carClass.getDeclaredConstructor().newInstance();
+        annotationMethods.forEach(annotationMethod -> {
+            try {
+                annotationMethod.invoke(car);
+            } catch (Exception e) {
+                fail("Exception occurred invoking test method : {}", annotationMethod.getName());
+            }
+        });
+
+        assertThat(annotationMethods.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    void privateFieldAccess() throws Exception {
+        Class<Car> carClass = Car.class;
+        Field nameField = carClass.getDeclaredField("name");
+        nameField.setAccessible(true);
+        Field priceField = carClass.getDeclaredField("price");
+        priceField.setAccessible(true);
+
+        Car car = carClass.getDeclaredConstructor().newInstance();
+        nameField.set(car, "소나타");
+        priceField.set(car, 10000);
+
+        assertThat(car.testGetName()).contains("소나타");
+        assertThat(car.testGetPrice()).contains("10000");
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws Exception {
+        Class<Car> carClass = Car.class;
+        Constructor<?> constructorArgs = stream(carClass.getDeclaredConstructors())
+                .filter(constructor -> constructor.getParameterCount() == 2)
+                .findFirst()
+                .orElseThrow();
+
+        Car carInstance = (Car)constructorArgs.newInstance("소나타", 10000);
+
+        assertThat(carInstance.testGetName()).contains("소나타");
+        assertThat(carInstance.testGetPrice()).contains("10000");
+    }
+
+}
