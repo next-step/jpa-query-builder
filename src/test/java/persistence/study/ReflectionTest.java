f613dff60a7c6360196c9daf3b00eb24907abe24
whale.beom <whale.beom@woowahan.com>
2023-10-14 16:26:05 +0900

    feat: step1 - 요구사항2 test로 시작하는 메서드 실행

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,5 +1,8 @@
 package persistence.study;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -14,5 +17,22 @@ class ReflectionTest {
 		Class<Car> carClass = Car.class;
 
 		logger.debug(carClass.getName());
+	}
+
+	@Test
+	@DisplayName("Car 메서드 네임이 test로 시작하는 메서드 실행하기")
+	void executeMethodStartsWithTest() throws Exception {
+		// given
+		Class<Car> carClass = Car.class;
+		Method[] declaredMethods = carClass.getDeclaredMethods();
+		Constructor<Car> declaredConstructor = carClass.getDeclaredConstructor();
+		Car car = declaredConstructor.newInstance();
+
+		// when
+		for (Method declaredMethod : declaredMethods) {
+			if (declaredMethod.getName().startsWith("test")) {
+				declaredMethod.invoke(car);
+			}
+		}
 	}
 }
