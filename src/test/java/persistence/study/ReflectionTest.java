224dea1417e822d9fca342e575443c3c6da7cc27
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-15 18:17:50 +0900

    refactor) 자바버전조정, assertAll 적용

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -9,10 +9,12 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
 
@@ -33,7 +35,7 @@ public class ReflectionTest {
         Method[] declaredMethods = carClass.getDeclaredMethods();
         List<Method> testMethods = stream(declaredMethods)
                 .filter(method -> method.getName().startsWith("test"))
-                .toList();
+                .collect(Collectors.toList());
 
         Car car = carClass.getDeclaredConstructor().newInstance();
         List<String> methodResults = testMethods.stream()
@@ -44,10 +46,12 @@ public class ReflectionTest {
                         fail("Exception occurred invoking test method : {}", testMethod.getName());
                     }
                     return null;
-                }).toList();
+                }).collect(Collectors.toList());
 
-        assertThat(methodResults.size()).isEqualTo(2);
-        assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"));
+        assertAll(
+                () -> assertThat(methodResults.size()).isEqualTo(2),
+                () -> assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"))
+        );
     }
 
     @Test
@@ -58,7 +62,7 @@ public class ReflectionTest {
         Method[] declaredMethods = carClass.getDeclaredMethods();
         List<Method> annotationMethods = stream(declaredMethods)
                 .filter(method -> method.isAnnotationPresent(PrintView.class))
-                .toList();
+                .collect(Collectors.toList());
 
         Car car = carClass.getDeclaredConstructor().newInstance();
         annotationMethods.forEach(annotationMethod -> {
@@ -85,8 +89,11 @@ public class ReflectionTest {
         nameField.set(car, "소나타");
         priceField.set(car, 10000);
 
-        assertThat(car.testGetName()).contains("소나타");
-        assertThat(car.testGetPrice()).contains("10000");
+        assertAll(
+                () -> assertThat(car.testGetName()).contains("소나타"),
+                () -> assertThat(car.testGetPrice()).contains("10000")
+        );
+
     }
 
     @Test
@@ -98,10 +105,13 @@ public class ReflectionTest {
                 .findFirst()
                 .orElseThrow();
 
-        Car carInstance = (Car)constructorArgs.newInstance("소나타", 10000);
+        Car carInstance = (Car) constructorArgs.newInstance("소나타", 10000);
 
-        assertThat(carInstance.testGetName()).contains("소나타");
-        assertThat(carInstance.testGetPrice()).contains("10000");
+        assertAll(
+                () -> assertThat(carInstance.testGetName()).contains("소나타"),
+                () -> assertThat(carInstance.testGetPrice()).contains("10000")
+        );
+
     }
 
 }
