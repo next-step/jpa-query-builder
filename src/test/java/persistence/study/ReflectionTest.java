6e9489f13e8f4282ab23413ca8919d58b23ae5fe
jiyeong-dt <imjee02@gmail.com>
2023-10-15 21:13:19 +0900

    feat(reflection): 요구사항 1 - 클래스 정보 출력
    - Car 클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,18 +1,45 @@
 package persistence.study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    private Class<Car> carClass;
+
+    @BeforeEach
+    void setUp() {
+        carClass = Car.class;
+    }
 
     @Test
-    @DisplayName("Car 객체 정보 가져오기")
+    @DisplayName("요구사항1. Car 클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
     void showClass() {
-        Class<Car> carClass = Car.class;
-        logger.debug(carClass.getName());
+        List<String> fields = convertArrayToListWithMapping(carClass.getDeclaredFields(), Field::getName);
+        List<String> constructor = convertArrayToListWithMapping(carClass.getDeclaredConstructors(), Constructor::getName);
+        List<String> methods = convertArrayToListWithMapping(carClass.getDeclaredMethods(), Method::getName);
+
+        assertThat(fields).containsOnly("name", "price");
+        assertThat(constructor).containsOnly("persistence.study.Car", "persistence.study.Car");
+        assertThat(methods).containsOnly("testGetPrice", "printView", "testGetName");
+    }
+
+    private <T, R> List<R> convertArrayToListWithMapping(T[] array, Function<T, R> map) {
+        return Arrays.stream(array).map(map).collect(Collectors.toList());
     }
 
 }
