cd1646d3b07fa221c3fc33f27bf5c6fd3e33fbfd
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-15 18:19:22 +0900

    Revert "Revert "refactor) 자바버전 조정""
    This reverts commit ae6840b246d6902f45e9b987f50e8e09c8165389.

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,7 +34,7 @@ public class ReflectionTest {
         Method[] declaredMethods = carClass.getDeclaredMethods();
         List<Method> testMethods = stream(declaredMethods)
                 .filter(method -> method.getName().startsWith("test"))
-                .toList();
+                .collect(Collectors.toList());
 
         Car car = carClass.getDeclaredConstructor().newInstance();
         List<String> methodResults = testMethods.stream()
@@ -44,7 +45,7 @@ public class ReflectionTest {
                         fail("Exception occurred invoking test method : {}", testMethod.getName());
                     }
                     return null;
-                }).toList();
+                }).collect(Collectors.toList());
 
         assertThat(methodResults.size()).isEqualTo(2);
         assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"));
@@ -58,7 +59,7 @@ public class ReflectionTest {
         Method[] declaredMethods = carClass.getDeclaredMethods();
         List<Method> annotationMethods = stream(declaredMethods)
                 .filter(method -> method.isAnnotationPresent(PrintView.class))
-                .toList();
+                .collect(Collectors.toList());
 
         Car car = carClass.getDeclaredConstructor().newInstance();
         annotationMethods.forEach(annotationMethod -> {
