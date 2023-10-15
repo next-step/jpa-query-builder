e3dfc6bc9eb3d5947c2b08b2c8122182919b1606
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-15 16:43:53 +0900

    feat) AUTO_INCREMENT 빌드옵션 추가

--- src/main/java/persistence/sql/ddl/ColumnBuilder.java
+++ src/main/java/persistence/sql/ddl/ColumnBuilder.java
@@ -1,15 +1,20 @@
 package persistence.sql.ddl;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import persistence.sql.util.StringConstant;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static persistence.sql.util.StringConstant.BLANK;
+import static persistence.sql.util.StringConstant.EMPTY_STRING;
+
 public class ColumnBuilder {
 
+    private static final String AUTO_INCREMENT = "AUTO_INCREMENT";
     private static final String PRIMARY_KEY = "PRIMARY KEY";
     private static final String COLUMN_JOIN = ", ";
 
@@ -26,8 +31,9 @@ public class ColumnBuilder {
     private static String toSql(Field field) {
         return new StringBuilder()
                 .append(field.getName().toLowerCase())
-                .append(StringConstant.BLANK)
+                .append(BLANK)
                 .append(getSqlType(field.getType()))
+                .append(getGenerationStrategy(field))
                 .append(getPrimaryKey(field))
                 .toString();
     }
@@ -36,12 +42,23 @@ public class ColumnBuilder {
         return ColumnType.getSqlType(type);
     }
 
+    private static String getGenerationStrategy(Field field) {
+        GeneratedValue generatedValue = field.getDeclaredAnnotation(GeneratedValue.class);
+        if (generatedValue == null) {
+            return EMPTY_STRING;
+        }
+        if (generatedValue.strategy() == GenerationType.IDENTITY) {
+            return BLANK + AUTO_INCREMENT;
+        }
+        return EMPTY_STRING;
+    }
+
     private static String getPrimaryKey(Field field) {
         Id pkAnnotation = field.getDeclaredAnnotation(Id.class);
         if (pkAnnotation == null) {
-            return StringConstant.EMPTY_STRING;
+            return EMPTY_STRING;
         }
-        return StringConstant.BLANK + PRIMARY_KEY;
+        return BLANK + PRIMARY_KEY;
     }
 
 
