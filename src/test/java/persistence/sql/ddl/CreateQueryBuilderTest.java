f7d99be075f13f11382ac4735d91569a1937f8e1
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-15 12:01:15 +0900

    test) CreateQueryBuilderTest 작성

---
+++ src/test/java/persistence/sql/ddl/CreateQueryBuilderTest.java
@@ -0,0 +1,28 @@
+package persistence.sql.ddl;
+
+import domain.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CreateQueryBuilderTest {
+
+    @Test
+    @DisplayName("Entity, PK 애노테이션 검증 정상 통과")
+    void existsAnnotation() {
+        assertDoesNotThrow(() -> CreateQueryBuilder.getQuery(Person.class));
+    }
+
+    @Test
+    @DisplayName("Entity 애노테이션 미존재")
+    void doNotHaveEntityAnnotation() {
+        assertThrows(IllegalArgumentException.class, () -> CreateQueryBuilder.getQuery(PureDomain.class), "Create Query 빌드 대상이 아닙니다.");
+    }
+
+    private static class PureDomain {
+        private String domainKey;
+    }
+
+}
