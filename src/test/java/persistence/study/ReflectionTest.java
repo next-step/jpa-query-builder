e0c9da2a63d4f0aeca34d0311a86e24731d3c7c8
ichang-gwon <changgunyee@github.com>
2023-10-16 08:57:44 +0900

    name, price 필드에 값 설정 테스트

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -3,21 +3,12 @@ package persistence.study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-<<<<<<< HEAD
-import java.lang.reflect.Field;
-=======
->>>>>>> f026bf9 (printView 어노테이션 메서드 실행)
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-<<<<<<< HEAD
-import static org.assertj.core.api.Assertions.assertThat;
-
-=======
->>>>>>> f026bf9 (printView 어노테이션 메서드 실행)
 public class ReflectionTest {
 
     @DisplayName("test로 시작하는 메소드 실행")
@@ -46,24 +37,4 @@ public class ReflectionTest {
             method.invoke(carClazz.newInstance());
         }
     }
-<<<<<<< HEAD
-
-    @DisplayName("name, price field에 값을 할당한다")
-    @Test
-    void privateFieldAccess() throws InstantiationException, IllegalAccessException, NoSuchFieldException {
-        Class<Car> carClazz = Car.class;
-        Car car = carClazz.newInstance();
-        Field name = carClazz.getDeclaredField("name");
-        Field price = carClazz.getDeclaredField("price");
-
-        name.setAccessible(true);
-        price.setAccessible(true);
-        name.set(car, "sonata");
-        price.set(car, 10000);
-
-        assertThat(car.getName()).isEqualTo("sonata");
-        assertThat(car.getPrice()).isEqualTo(10000);
-    }
-=======
->>>>>>> f026bf9 (printView 어노테이션 메서드 실행)
 }
