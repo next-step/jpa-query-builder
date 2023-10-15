3683084d3b05b8c25ece26b93e10a399b7f6738b
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-15 18:19:23 +0900

    Revert "Revert "refactor) assertAll 적용""
    This reverts commit 5da58d346f8e19aac2ba00a3a9c77d0e8838917e.

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
 
@@ -47,8 +48,10 @@ public class ReflectionTest {
                     return null;
                 }).collect(Collectors.toList());
 
-        assertThat(methodResults.size()).isEqualTo(2);
-        assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"));
+        assertAll(
+                () -> assertThat(methodResults.size()).isEqualTo(2),
+                () -> assertThat(methodResults).allSatisfy(methodResult -> assertThat(methodResult).startsWith("test"))
+        );
     }
 
     @Test
@@ -86,8 +89,11 @@ public class ReflectionTest {
         nameField.set(car, "소나타");
         priceField.set(car, 10000);
 
-        assertThat(car.testGetName()).contains("소나타");
-        assertThat(car.testGetPrice()).contains("10000");
+        assertAll(
+                () -> assertThat(car.testGetName()).contains("소나타"),
+                () -> assertThat(car.testGetPrice()).contains("10000")
+        );
+
     }
 
     @Test
@@ -99,10 +105,13 @@ public class ReflectionTest {
                 .findFirst()
                 .orElseThrow();
 
-        Car carInstance = (Car)constructorArgs.newInstance("소나타", 10000);
+        Car carInstance = (Car) constructorArgs.newInstance("소나타", 10000);
 
-        assertThat(carInstance.testGetName()).contains("소나타");
-        assertThat(carInstance.testGetPrice()).contains("10000");
+        assertAll(
+                () -> assertThat(carInstance.testGetName()).contains("소나타"),
+                () -> assertThat(carInstance.testGetPrice()).contains("10000")
+        );
+
     }
 
 }
