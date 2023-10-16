d7109b7555fcae2df5da58264f7eef1c60dc69e2
jiyeong-dt <imjee02@gmail.com>
2023-10-17 07:00:31 +0900

    feat(step1): 리뷰 반영해 리팩토링
    - @Target annotation 추가
    - 전역 변수 NAME,PRICE 로컬 변수로 이동
    - assertAll 로 변경
    - Exception 최상위 예외로 변경
    - forEach문 stream으로 변경

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,6 +1,5 @@
 package persistence.study;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -16,18 +15,12 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
-    private Class<Car> carClass;
+    final private Class<Car> carClass = Car.class;
 
-    final String NAME = "메르세데스 벤츠";
-    final int PRICE = 1000;
-
-    @BeforeEach
-    void setUp() {
-        carClass = Car.class;
-    }
 
     @Test
     @DisplayName("요구사항1. Car 클래스의 모든 필드, 생성자, 메소드 정보를 출력한다.")
@@ -35,10 +28,11 @@ public class ReflectionTest {
         List<String> fields = convertArrayToListWithMapping(carClass.getDeclaredFields(), Field::getName);
         List<String> constructor = convertArrayToListWithMapping(carClass.getDeclaredConstructors(), Constructor::getName);
         List<String> methods = convertArrayToListWithMapping(carClass.getDeclaredMethods(), Method::getName);
-
-        assertThat(fields).containsOnly("name", "price");
-        assertThat(constructor).containsOnly("persistence.study.Car", "persistence.study.Car");
-        assertThat(methods).containsOnly("testGetPrice", "printView", "testGetName", "getName", "getPrice");
+        assertAll(
+                () -> assertThat(fields).containsOnly("name", "price"),
+                () -> assertThat(constructor).containsOnly("persistence.study.Car", "persistence.study.Car"),
+                () -> assertThat(methods).containsOnly("testGetPrice", "printView", "testGetName", "getName", "getPrice")
+        );
     }
 
     private <T, R> List<R> convertArrayToListWithMapping(T[] array, Function<T, R> map) {
@@ -47,50 +41,57 @@ public class ReflectionTest {
 
     @Test
     @DisplayName("test로 시작하는 메소드 실행")
-    void testMethodRun() throws InvocationTargetException, IllegalAccessException {
-        carClass = Car.class;
+    void testMethodRun() {
         Car car = new Car();
-        Method[] methods = carClass.getMethods();
-        for (Method method : methods) {
-            isStartWithTest(car, method);
-        }
+        Arrays.stream(carClass.getMethods()).forEach((method) -> {
+            try {
+                isStartWithTest(car, method);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    private void isStartWithTest(final Car car, final Method method) throws IllegalAccessException, InvocationTargetException {
+    private void isStartWithTest(final Car car, final Method method) throws Exception {
         if (method.getName().startsWith("test")) {
             String invoke = (String) method.invoke(car);
             assertThat(invoke).startsWith("test : ");
         }
     }
 
-
     @Test
     @DisplayName("@PrintView 애노테이션 메소드 실행")
-    void testAnnotationMethodRun() throws InvocationTargetException, IllegalAccessException {
+    void testAnnotationMethodRun() {
         Car car = new Car();
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
 
-        List<Method> methodWithPrintView = Arrays.stream(carClass.getMethods())
-                .filter(method -> method.isAnnotationPresent(PrintView.class)).collect(Collectors.toList());
-        for (Method method : methodWithPrintView) {
-            method.invoke(car);
-            assertThat(out.toString().trim()).isEqualTo("자동차 정보를 출력 합니다.");
-        }
+        Arrays.stream(carClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(car);
+                    } catch (Exception e) {
+                        assertThat(out.toString().trim()).isEqualTo("자동차 정보를 출력 합니다.");
+                    }
+                });
     }
 
     @Test
     @DisplayName("private field에 값 할당")
-    void privateFieldAccess() throws IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+    void privateFieldAccess() throws Exception {
         Car car = carClass.getDeclaredConstructor().newInstance();
-
+        final String NAME = "메르세데스 벤츠";
+        final int PRICE = 1000;
         Field nameFiled = getAccessibleFiled(carClass, "name");
         Field priceFiled = getAccessibleFiled(carClass, "price");
         nameFiled.set(car, NAME);
         priceFiled.set(car, PRICE);
 
-        assertThat(car.getName()).isEqualTo(NAME);
-        assertThat(car.getPrice()).isEqualTo(PRICE);
+        assertAll(
+                () -> assertThat(car.getName()).isEqualTo(NAME),
+                () -> assertThat(car.getPrice()).isEqualTo(PRICE)
+        );
     }
 
     private Field getAccessibleFiled(final Class<?> clazz, final String filedName) throws NoSuchFieldException {
@@ -102,18 +103,22 @@ public class ReflectionTest {
     @Test
     @DisplayName("인자를 가진 생성자의 인스턴스 생성")
     void constructorWithArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-
+        final String NAME = "메르세데스 벤츠";
+        final int PRICE = 2000;
         Constructor<?> constructorWithArgs = Arrays.stream(carClass.getDeclaredConstructors())
                 .filter(constructor -> hasParameters(constructor.getParameters()))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("인자를 가진 생성자가 존재하지 않습니다."));
 
         Car car = (Car) constructorWithArgs.newInstance(NAME, PRICE);
-        assertThat(car.getName()).isEqualTo(NAME);
-        assertThat(car.getPrice()).isEqualTo(PRICE);
+        assertAll(
+                () -> assertThat(car.getName()).isEqualTo(NAME),
+                () -> assertThat(car.getPrice()).isEqualTo(PRICE)
+        );
     }
 
     private boolean hasParameters(final Parameter[] parameters) {
         return parameters.length > 0;
     }
+
 }
