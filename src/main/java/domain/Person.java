6f3cb4849ad4f43fe178a49b141a2c281f3c53d9
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-18 08:02:40 +0900

    feat) Person 클래스 protected 생성자 추가

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 @Entity
 public class Person {
 
+    protected Person() {}
+
     public Person(String name, Integer age, String email, Integer index) {
         this.name = name;
         this.age = age;
@@ -28,5 +30,4 @@ public class Person {
 
     @Transient
     private Integer index;
-
 }
