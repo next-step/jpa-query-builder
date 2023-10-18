e92213ca5fb1217accdfad842c9f82b11f70af96
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-18 22:51:40 +0900

    feat: EntityClass Table 어노테이션 네임 파싱 추가 및 테스트

--- src/main/java/hibernate/EntityClass.java
+++ src/main/java/hibernate/EntityClass.java
@@ -1,12 +1,27 @@
 package hibernate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 public class EntityClass {
+
+    private final String tableName;
 
     public EntityClass(final Class<?> clazz) {
         if (!clazz.isAnnotationPresent(Entity.class)) {
             throw new IllegalArgumentException("Entity 어노테이션이 없는 클래스는 입력될 수 없습니다.");
         }
+        this.tableName = parseTableName(clazz);
+    }
+
+    private String parseTableName(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(Table.class)) {
+            return clazz.getAnnotation(Table.class).name();
+        }
+        return clazz.getSimpleName();
+    }
+
+    public String tableName() {
+        return tableName;
     }
 }
--- src/test/java/hibernate/EntityClassTest.java
+++ src/test/java/hibernate/EntityClassTest.java
@@ -1,7 +1,10 @@
 package hibernate;
 
+import domain.Person;
+import domain.Person3;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EntityClassTest {
@@ -11,5 +14,17 @@ class EntityClassTest {
         assertThatThrownBy(() -> new EntityClass(EntityClass.class))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Entity 어노테이션이 없는 클래스는 입력될 수 없습니다.");
+    }
+
+    @Test
+    void Table_어노테이션의_name이_있으면_생성_시_tableName이_된다() {
+        String actual = new EntityClass(Person3.class).tableName();
+        assertThat(actual).isEqualTo("users");
+    }
+
+    @Test
+    void Table_어노테이션의_name이_없으면_tableName은_클래스명이_된다() {
+        String actual = new EntityClass(Person.class).tableName();
+        assertThat(actual).isEqualTo("Person");
     }
 }
