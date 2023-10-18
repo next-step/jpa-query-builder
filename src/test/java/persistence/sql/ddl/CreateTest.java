c5a8a3092815b94d6ccf44fd9691a2a0d6bf00cd
planter-dev <ismyelmo@gmail.com>
2023-10-19 02:52:55 +0900

    요구사항 1

--- src/test/java/persistence/sql/ddl/CreateTest.java
+++ src/test/java/persistence/sql/ddl/CreateTest.java
@@ -1,8 +1,11 @@
 package persistence.sql.ddl;
 
+import database.DatabaseServer;
+import database.H2;
 import domain.PersonV1;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import jdbc.JdbcTemplate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import persistence.dialect.Dialect;
 import persistence.dialect.H2Dialect;
 import sources.AnnotationBinder;
@@ -10,13 +13,23 @@ import sources.MetaData;
 import sources.MetadataGenerator;
 import sources.MetadataGeneratorImpl;
 
+import java.sql.SQLException;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CreateTest {
+    DatabaseServer server;
+    final Dialect dialect = new H2Dialect();
+    final QueryBuilder queryBuilder = new QueryBuilder(dialect);
+    final AnnotationBinder annotationBinder = new AnnotationBinder();
+    final MetadataGenerator metadataGenerator = new MetadataGeneratorImpl(annotationBinder);
+    JdbcTemplate jdbcTemplate;
 
-    Dialect dialect = new H2Dialect();
-    QueryBuilder queryBuilder = new QueryBuilder(dialect);
-    AnnotationBinder annotationBinder = new AnnotationBinder();
-    MetadataGenerator metadataGenerator = new MetadataGeneratorImpl(annotationBinder);
-
+    @BeforeAll
+    void setUp() throws SQLException {
+        this.server = new H2();
+        server.start();
+        this.jdbcTemplate = new JdbcTemplate(server.getConnection());
+    }
 
     @Test
     @DisplayName("요구사항 1 - 아래 정보를 바탕으로 create 쿼리 만들어보기")
@@ -24,8 +37,14 @@ public class CreateTest {
         MetaData personv1 = metadataGenerator.generator(PersonV1.class);
         StringBuilder sb = new StringBuilder();
         StringBuilder createQuery = queryBuilder.create(personv1, sb);
-        System.out.println(createQuery);
+        jdbcTemplate.execute(String.valueOf(createQuery));
+        Assertions.assertThat("create table PersonV1 (id int not null auto_increment,  age varchar,  name varchar(100), primary key(id))")
+                .isEqualTo(String.valueOf(createQuery));
     }
 
+    @AfterAll
+    void close() {
+        server.stop();
+    }
 
 }
