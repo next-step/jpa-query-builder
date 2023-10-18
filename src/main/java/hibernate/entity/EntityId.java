f50c202a61da34f76adf01d8b57e9b6c573ba45c
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-19 00:17:19 +0900

    feat: EntityId가 GenerationType을 갖도록 추가 및 테스트

--- src/main/java/hibernate/entity/EntityId.java
+++ src/main/java/hibernate/entity/EntityId.java
@@ -1,5 +1,7 @@
 package hibernate.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.lang.reflect.Field;
@@ -7,11 +9,24 @@ import java.lang.reflect.Field;
 public class EntityId {
 
     private final EntityField entityField;
+    private final GenerationType generationType;
 
     public EntityId(final Field field) {
         if (!field.isAnnotationPresent(Id.class)) {
             throw new IllegalArgumentException("Id 어노테이션이 없습니다.");
         }
         this.entityField = new EntityField(field);
+        this.generationType = parseGenerationType(field);
+    }
+
+    private static GenerationType parseGenerationType(Field field) {
+        if (!field.isAnnotationPresent(GeneratedValue.class)) {
+            return GenerationType.AUTO;
+        }
+        return field.getAnnotation(GeneratedValue.class).strategy();
+    }
+
+    public GenerationType getGenerationType() {
+        return generationType;
     }
 }
--- src/test/java/hibernate/entity/EntityIdTest.java
+++ src/test/java/hibernate/entity/EntityIdTest.java
@@ -1,7 +1,11 @@
 package hibernate.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EntityIdTest {
@@ -13,7 +17,32 @@ class EntityIdTest {
                 .hasMessage("Id 어노테이션이 없습니다.");
     }
 
+    @Test
+    void GeneratedValue_어노테이션이_없는_경우_AUTO로_저장된다() throws NoSuchFieldException {
+        GenerationType expected = GenerationType.AUTO;
+        GenerationType actual = new EntityId(IdFieldNoGeneratedValue.class.getDeclaredField("id")).getGenerationType();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void GeneratedValue_어노테이션이_있는_경우_해당_값으로_저장된다() throws NoSuchFieldException {
+        GenerationType expected = GenerationType.IDENTITY;
+        GenerationType actual = new EntityId(IdField.class.getDeclaredField("id")).getGenerationType();
+        assertThat(actual).isEqualTo(expected);
+    }
+
     class NoIdField {
         private String field;
+    }
+
+    class IdFieldNoGeneratedValue {
+        @Id
+        private Long id;
+    }
+
+    class IdField {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
     }
 }
