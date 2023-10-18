ebe97c663f55b0d624beb9806966fb14900844d6
hhhhhwi <aab555586@gmail.com>
2023-10-18 15:19:23 +0900

    refactor: QueryBuilder 추상화
    - QueryBuilder가 다른 DDL도 수행할 수 있도록 공통적인 기능을 추상화
    - QueryBuilder를 상속받아 CreatQueryBuilder를 구현

--- src/main/java/persistence/sql/ddl/CreateQueryBuilder.java
+++ src/main/java/persistence/sql/ddl/CreateQueryBuilder.java
@@ -1,41 +1,24 @@
 package persistence.sql.ddl;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
-public class CreateQueryBuilder{
+public class CreateQueryBuilder extends QueryBuilder{
     private final static String CREATE_TABLE_COMMAND = "CREATE TABLE %s";
 
     public CreateQueryBuilder() {
     }
 
-    public String bulidQuery(Class<?> clazz) {
+    public String buildQuery(Class<?> clazz) {
         checkIsEntity(clazz);
 
         return format(CREATE_TABLE_COMMAND, findTableName(clazz)) +
                 "(" +
                 buildColumnList(convertClassToColumnList(clazz)) +
                 ");";
-    }
-
-    private void checkIsEntity(Class<?> clazz) {
-        if (!clazz.isAnnotationPresent(Entity.class)) {
-            throw new IllegalArgumentException("Entity 클래스가 아닙니다.");
-        }
-    }
-
-    private String findTableName(Class<?> clazz) {
-        if(clazz.isAnnotationPresent(Table.class)) {
-            return clazz.getDeclaredAnnotation(Table.class).name();
-        }
-
-        return clazz.getSimpleName();
     }
 
     private String buildColumnList(List<Column> columns) {
