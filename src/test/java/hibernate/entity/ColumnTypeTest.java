52245793b4c81f7ee5dd4f30e7b699a0a7749e86
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-19 02:23:38 +0900

    refactor: remove unused import

--- src/test/java/hibernate/entity/ColumnTypeTest.java
+++ src/test/java/hibernate/entity/ColumnTypeTest.java
@@ -1,6 +1,5 @@
 package hibernate.entity;
 
-import hibernate.entity.ColumnType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
