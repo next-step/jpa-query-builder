f293fe9dcd0372b5321eb09cfb5293a2445176c8
profoundsea25 <profoundsea25@gmail.com>
2023-10-18 21:28:33 +0900

    feat: 요구사항 4 drop 쿼리 만들어보기

--- src/main/java/persistence/sql/ddl/dialect/Dialect.java
+++ src/main/java/persistence/sql/ddl/dialect/Dialect.java
@@ -1,20 +1,25 @@
 package persistence.sql.ddl.dialect;
 
 import persistence.sql.ddl.entity.EntityColumn;
+import persistence.sql.ddl.entity.EntityData;
 
 /**
  * 각 DB 종류별 쿼리를 생성하는 클래스
  */
 public abstract class Dialect {
+
+    public final String STRING_FORMAT_SLOT = "%s";
     public final String SPACE = " ";
     public final String OPEN_PARENTHESIS = "(";
     public final String CLOSE_PARENTHESIS = ")";
 
     public final String COMMA = ",";
     public final String CREATE_QUERY;
+    public final String DROP_QUERY;
 
-    public Dialect(String create) {
-        this.CREATE_QUERY = create;
+    public Dialect(String createQuery, String dropQuery) {
+        this.CREATE_QUERY = createQuery;
+        this.DROP_QUERY = dropQuery;
     }
 
     /**
@@ -26,4 +31,7 @@ public abstract class Dialect {
      * Create 문의 Primary Key 부분 생성
      */
     public abstract String getPrimaryKeyInCreateQuery(EntityColumn primaryKey);
+
+    public abstract String getDropQuery(EntityData entityData);
+
 }
