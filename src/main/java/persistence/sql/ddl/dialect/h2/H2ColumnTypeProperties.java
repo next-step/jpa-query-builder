b00b40f7f7013989adeec4bb11b3448c16cae282
profoundsea25 <profoundsea25@gmail.com>
2023-10-19 19:53:12 +0900

    test: 테스트 추가 (H2ColumnTypeProperties)

--- src/main/java/persistence/sql/ddl/dialect/h2/H2ColumnTypeProperties.java
+++ src/main/java/persistence/sql/ddl/dialect/h2/H2ColumnTypeProperties.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Column;
 
 import java.lang.reflect.Field;
 
+import static persistence.sql.ddl.dialect.h2.H2Query.*;
+
 /**
  * 컬럼 프로퍼티 쿼리 생성을 위한 클래스
  */
 public class H2ColumnTypeProperties {
-
-    private static final String OPEN_PARENTHESIS = "(";
-    private static final String CLOSE_PARENTHESIS = ")";
-    private static final int VARCHAR_DEFAULT_LENGTH = 255;
 
     // Varchar인 경우, length 표시
     public static String getVarcharLength(Field entityField) {
