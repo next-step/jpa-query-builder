358031eef67534e2dc3f01a18c9592c06e9bc31a
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-19 12:59:05 +0900

    feat) DML 쿼리빌더에 Select 쿼리빌더 추가

--- src/main/java/persistence/sql/dialect/DbmsDmlQueryBuilder.java
+++ src/main/java/persistence/sql/dialect/DbmsDmlQueryBuilder.java
@@ -1,22 +1,26 @@
 package persistence.sql.dialect;
 
 import persistence.sql.dml.InsertQueryBuilder;
+import persistence.sql.dml.SelectQueryBuilder;
 import persistence.sql.dml.h2.H2InsertQueryBuilder;
+import persistence.sql.dml.h2.H2SelectQueryBuilder;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum DbmsDmlQueryBuilder {
 
-    H2("H2", H2InsertQueryBuilder.getInstance())
+    H2("H2", H2InsertQueryBuilder.getInstance(), H2SelectQueryBuilder.getInstance())
     ;
 
     private final String dbmsType;
     private final InsertQueryBuilder insertQueryBuilder;
+    private final SelectQueryBuilder selectQueryBuilder;
 
-    DbmsDmlQueryBuilder(String dbmsType, InsertQueryBuilder insertQueryBuilder) {
+    DbmsDmlQueryBuilder(String dbmsType, InsertQueryBuilder insertQueryBuilder, SelectQueryBuilder selectQueryBuilder) {
         this.dbmsType = dbmsType;
         this.insertQueryBuilder = insertQueryBuilder;
+        this.selectQueryBuilder = selectQueryBuilder;
     }
 
     public static DbmsDmlQueryBuilder findByDbmsType(String dbmsType) {
@@ -30,4 +34,7 @@ public enum DbmsDmlQueryBuilder {
         return insertQueryBuilder;
     }
 
+    public SelectQueryBuilder getSelectQueryBuilder() {
+        return selectQueryBuilder;
+    }
 }
