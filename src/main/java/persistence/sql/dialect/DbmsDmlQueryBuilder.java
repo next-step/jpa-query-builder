c6a6e14c978b1855aff5fffdc37b06d2d0affb1d
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-20 01:09:44 +0900

    feat) DBMS vendor enum에 DeleteQueryBuilder 추가

--- src/main/java/persistence/sql/dialect/DbmsDmlQueryBuilder.java
+++ src/main/java/persistence/sql/dialect/DbmsDmlQueryBuilder.java
@@ -1,7 +1,9 @@
 package persistence.sql.dialect;
 
+import persistence.sql.dml.DeleteQueryBuilder;
 import persistence.sql.dml.InsertQueryBuilder;
 import persistence.sql.dml.SelectQueryBuilder;
+import persistence.sql.dml.h2.H2DeleteQueryBuilder;
 import persistence.sql.dml.h2.H2InsertQueryBuilder;
 import persistence.sql.dml.h2.H2SelectQueryBuilder;
 
@@ -10,17 +12,19 @@ import java.util.NoSuchElementException;
 
 public enum DbmsDmlQueryBuilder {
 
-    H2("H2", H2InsertQueryBuilder.getInstance(), H2SelectQueryBuilder.getInstance())
+    H2("H2", H2InsertQueryBuilder.getInstance(), H2SelectQueryBuilder.getInstance(), H2DeleteQueryBuilder.getInstance())
     ;
 
     private final String dbmsType;
     private final InsertQueryBuilder insertQueryBuilder;
     private final SelectQueryBuilder selectQueryBuilder;
+    private final DeleteQueryBuilder deleteQueryBuilder;
 
-    DbmsDmlQueryBuilder(String dbmsType, InsertQueryBuilder insertQueryBuilder, SelectQueryBuilder selectQueryBuilder) {
+    DbmsDmlQueryBuilder(String dbmsType, InsertQueryBuilder insertQueryBuilder, SelectQueryBuilder selectQueryBuilder, DeleteQueryBuilder deleteQueryBuilder) {
         this.dbmsType = dbmsType;
         this.insertQueryBuilder = insertQueryBuilder;
         this.selectQueryBuilder = selectQueryBuilder;
+        this.deleteQueryBuilder = deleteQueryBuilder;
     }
 
     public static DbmsDmlQueryBuilder findByDbmsType(String dbmsType) {
@@ -36,5 +40,9 @@ public enum DbmsDmlQueryBuilder {
 
     public SelectQueryBuilder getSelectQueryBuilder() {
         return selectQueryBuilder;
+    }
+
+    public DeleteQueryBuilder getDeleteQueryBuilder() {
+        return deleteQueryBuilder;
     }
 }
