eea7e57842b3462374ba02bc4627cda78c17825d
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-20 00:14:59 +0900

    fix) 이메일 주소포맷 맞춤

--- src/main/java/fixture/PersonFixtureFactory.java
+++ src/main/java/fixture/PersonFixtureFactory.java
@@ -14,10 +14,10 @@ public class PersonFixtureFactory {
     public static List<Person> getFixtures() {
         return Arrays.asList(
                 new Person("테스트1", 21, "111@domain.com", 1),
-                new Person("테스트2", 22, "222domain.com", 2),
-                new Person("테스트3", 23, "333domain.com", 3),
-                new Person("테스트4", 24, "444domain.com", 4),
-                new Person("테스트5", 25, "555domain.com", 5)
+                new Person("테스트2", 22, "222@domain.com", 2),
+                new Person("테스트3", 23, "333@domain.com", 3),
+                new Person("테스트4", 24, "444@domain.com", 4),
+                new Person("테스트5", 25, "555@domain.com", 5)
         );
     }
 }
