de37ca1852c7eda02636158b134b5651713e7e57
whale.beom <whale.beom@woowahan.com>
2023-10-20 00:25:10 +0900

    refactor: 클래스 분리 및 코드 리팩터링

--- src/main/java/persistence/sql/ddl/CreateQueryGenerator.java
+++ src/main/java/persistence/sql/ddl/CreateQueryGenerator.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Table;
 public class CreateQueryGenerator<T> {
 
 	private static final String CREATE_TABLE = "CREATE TABLE %s (%s);";
-	private static final String DROP_TABLE = "DROP TABLE %s;";
 	private final EntityColumns entityColumns;
 	private final String tableName;
 
@@ -28,9 +27,5 @@ public class CreateQueryGenerator<T> {
 
 	private String getColumn() {
 		return entityColumns.getColumnQuery();
-	}
-
-	public String getDropQuery() {
-		return String.format(DROP_TABLE, tableName);
 	}
 }
