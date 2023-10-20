edda432dab4681931453b1241ef7ea951966354b
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-21 03:01:36 +0900

    feat: findAll 쿼리생성 추가 및 테스트

---
+++ src/main/java/hibernate/SelectAllQueryBuilder.java
@@ -0,0 +1,24 @@
+package hibernate;
+
+import hibernate.entity.EntityClass;
+import hibernate.entity.column.EntityColumn;
+
+import java.util.stream.Collectors;
+
+public class SelectAllQueryBuilder {
+
+    private static final String SELECT_ALL_QUERY = "select %s from %s;";
+
+    private static final String SELECT_AlL_QUERY_COLUMN_DELIMITER = ", ";
+
+    public String generateQuery(final EntityClass entityClass) {
+        return String.format(SELECT_ALL_QUERY, parseColumnQueries(entityClass), entityClass.tableName());
+    }
+
+    private String parseColumnQueries(EntityClass entityClass) {
+        return entityClass.getEntityColumns()
+                .stream()
+                .map(EntityColumn::getFieldName)
+                .collect(Collectors.joining(SELECT_AlL_QUERY_COLUMN_DELIMITER));
+    }
+}
---
+++ src/test/java/hibernate/SelectAllQueryBuilderTest.java
@@ -0,0 +1,48 @@
+package hibernate;
+
+import hibernate.entity.EntityClass;
+import jakarta.persistence.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SelectAllQueryBuilderTest {
+
+    private final SelectAllQueryBuilder selectAllQueryBuilder = new SelectAllQueryBuilder();
+
+    @Test
+    void select_all쿼리를_생성한다() {
+        // given
+        String expected = "select id, nick_name from test_entity;";
+
+        // when
+        String actual = selectAllQueryBuilder.generateQuery(new EntityClass(TestEntity.class))
+                .toLowerCase();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Entity
+    @Table(name = "test_entity")
+    static class TestEntity {
+
+        @Id
+        private Long id;
+
+        @Column(name = "nick_name")
+        private String name;
+
+        @Transient
+        private String email;
+
+        public TestEntity() {
+        }
+
+        public TestEntity(Long id, String name, String email) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
