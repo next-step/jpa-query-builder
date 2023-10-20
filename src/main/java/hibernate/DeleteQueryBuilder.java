5b1e3e8e92cc825fa06d2ab526147e03f9c50414
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-10-21 03:39:04 +0900

    feat: delete 쿼리 생성 추가 및 테스트

---
+++ src/main/java/hibernate/DeleteQueryBuilder.java
@@ -0,0 +1,13 @@
+package hibernate;
+
+public class DeleteQueryBuilder {
+
+    private static final String DELETE_QUERY = "delete from %s where %s = %s;";
+
+    public DeleteQueryBuilder() {
+    }
+
+    public String generateQuery(final EntityObject entityObject) {
+        return String.format(DELETE_QUERY, entityObject.getTableName(), entityObject.getEntityId().getFieldName(), entityObject.getEntityIdValue());
+    }
+}
