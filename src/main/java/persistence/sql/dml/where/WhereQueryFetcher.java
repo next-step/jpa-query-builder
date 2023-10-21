6aaa02c3e1e15f39e33f3b99ca68eec742213e42
hungrytech <xorals9448@gmail.com>
2023-10-21 20:19:23 +0900

    refactor: 사용하지 않는 import제거

--- src/main/java/persistence/sql/dml/where/WhereQueryFetcher.java
+++ src/main/java/persistence/sql/dml/where/WhereQueryFetcher.java
@@ -1,7 +1,5 @@
 package persistence.sql.dml.where;
 
-import persistence.sql.dml.where.FetchWhereQuery;
-
 public interface WhereQueryFetcher {
 
     String whereClause(String selectQuery, Class<?> entity, FetchWhereQuery fetchWhereQueries);
