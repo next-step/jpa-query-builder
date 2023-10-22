cae03918e11d87aa7e02f16d3d04704768cabf14
mj950425 <minjoon.lee@myrealtrip.com>
2023-10-22 19:49:01 +0900

    select쿼리 빌더 고도화

--- src/main/java/persistence/sql/dml/builder/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/builder/SelectQueryBuilder.java
@@ -1,7 +1,23 @@
 package persistence.sql.dml.builder;
 
 public class SelectQueryBuilder {
-    public String prepareStatement(Class<?> clazz, String id) {
-        return String.format("SELECT * FROM %s where id = %s", clazz.getSimpleName(), id);
+    private final String tableName;
+    private final WhereClause whereClause = new WhereClause();
+
+    private SelectQueryBuilder(Class<?> clazz) {
+        this.tableName = clazz.getSimpleName();
+    }
+
+    public static SelectQueryBuilder of(Class<?> clazz) {
+        return new SelectQueryBuilder(clazz);
+    }
+
+    public SelectQueryBuilder where(String fieldName, String value) {
+        whereClause.and(fieldName, value);
+        return this;
+    }
+
+    public String prepareStatement() {
+        return String.format("SELECT * FROM %s%s", tableName, whereClause);
     }
 }
--- src/test/java/persistence/sql/dml/builder/SelectInsertQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/builder/SelectInsertQueryBuilderTest.java
@@ -1,15 +1,30 @@
 package persistence.sql.dml.builder;
 
 import entity.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Nested
+@DisplayName("SelectInsertQueryBuilder 클래스의")
 public class SelectInsertQueryBuilderTest {
-    @Test
-    void test() {
-        SelectQueryBuilder selectQueryBuilder = new SelectQueryBuilder();
-        String dml = selectQueryBuilder.prepareStatement(Person.class, String.valueOf(1));
-        assertThat(dml).isEqualTo("SELECT * FROM Person where id = 1");
+
+    @Nested
+    @DisplayName("prepareStatement 메소드는")
+    public class prepareStatement {
+
+        @Nested
+        @DisplayName("where절이 주어지면")
+        public class withWhereClause {
+
+            @Test
+            @DisplayName("적절한 DML을 리턴한다.")
+            void returnDmlWithWhereClause() {
+                String dml = SelectQueryBuilder.of(Person.class).where("id", "1").prepareStatement();
+                assertThat(dml).isEqualTo("SELECT * FROM Person WHERE id = '1'");
+            }
+        }
     }
 }
