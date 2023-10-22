8f132daf78542c35698fda4fa2f1a17cdcf39adb
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-10-22 17:48:44 +0900

    refactor) SelectQueryBuilder 조건절 조립기능 WhereClauseBuilder로 추출

--- src/main/java/persistence/sql/dml/builder/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/builder/SelectQueryBuilder.java
@@ -1,71 +1,48 @@
 package persistence.sql.dml.builder;
 
-import persistence.sql.meta.ColumnMeta;
+import persistence.sql.meta.ColumnMetas;
 import persistence.sql.meta.EntityMeta;
-import persistence.sql.util.StringConstant;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public abstract class SelectQueryBuilder {
+public class SelectQueryBuilder {
 
     private static final String SELECT = "SELECT ";
     private static final String FROM = " FROM ";
-    private static final String WHERE = " WHERE ";
-    private static final String AND = " AND ";
 
-    public String getSelectAllQuery(Class<?> clazz) {
-        validateEntityAnnotation(clazz);
+    private final EntityMeta entityMeta;
+    private final WhereClauseBuilder whereClauseBuilder;
+
+    private SelectQueryBuilder(EntityMeta entityMeta) {
+        this.entityMeta = entityMeta;
+        this.whereClauseBuilder = WhereClauseBuilder.builder(entityMeta);
+    }
+
+    public static SelectQueryBuilder of(EntityMeta entityMeta) {
+        return new SelectQueryBuilder(entityMeta);
+    }
+
+    public String buildSelectAllQuery() {
         return new StringBuilder()
-                .append(getSelectHeaderQuery(clazz))
+                .append(getSelectHeaderQuery())
                 .append(";")
                 .toString();
     }
 
-    public String getSelectByPkQuery(Class<?> clazz, Object pkObject) {
-        validateEntityAnnotation(clazz);
+    public String buildSelectByPkQuery(Object pkObject) {
         return new StringBuilder()
-                .append(getSelectHeaderQuery(clazz))
-                .append(WHERE)
-                .append(getPkWhereClause(clazz.getDeclaredFields(), pkObject))
+                .append(getSelectHeaderQuery())
+                .append(whereClauseBuilder.buildPkClause(pkObject))
                 .append(";")
                 .toString();
     }
 
-    private void validateEntityAnnotation(Class<?> clazz) {
-        if (!EntityMeta.isEntity(clazz)) {
-            throw new IllegalArgumentException("Select Query 빌드 대상이 아닙니다.");
-        }
-    }
-
-    private String getSelectHeaderQuery(Class<?> clazz) {
+    private String getSelectHeaderQuery() {
+        ColumnMetas columnMetas = entityMeta.getColumnMetas();
         return new StringBuilder()
                 .append(SELECT)
-                .append(getColumnsClause(clazz.getDeclaredFields()))
+                .append(columnMetas.getColumnsClause())
                 .append(FROM)
-                .append(EntityMeta.getTableName(clazz))
+                .append(entityMeta.getTableName())
                 .toString();
     }
 
-    private String getColumnsClause(Field[] fields) {
-        List<String> columnNames = Arrays.stream(fields)
-                .filter(field -> !ColumnMeta.isTransient(field))
-                .map(ColumnMeta::getColumnName)
-                .collect(Collectors.toList());
-        return String.join(StringConstant.COLUMN_JOIN, columnNames);
-    }
-
-    private String getPkWhereClause(Field[] fields, Object pkObject) {
-        List<String> valueConditions = Arrays.stream(fields)
-                .filter(ColumnMeta::isId)
-                .map(field -> getValueCondition(field, pkObject))
-                .collect(Collectors.toList());
-        return String.join(AND, valueConditions);
-    }
-
-    private String getValueCondition(Field field, Object pkObject) {
-        return ColumnMeta.getColumnName(field) + StringConstant.EQUAL + ColumnMeta.parseColumnValue(pkObject);
-    }
 }
