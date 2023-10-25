1f6f1c24ce607ac8dfba6d4f8c6f5cc08d047c4c
Jivebread <spark@troy.edu>
2023-10-25 20:03:58 +0900

    feature(MetaDataColumn): @Transient 컬럼은 무시합니다.

--- src/main/java/persistence/meta/MetaDataColumns.java
+++ src/main/java/persistence/meta/MetaDataColumns.java
@@ -1,7 +1,9 @@
 package persistence.meta;
 
+import jakarta.persistence.Transient;
 import persistence.dialect.Dialect;
 
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,15 +11,16 @@ import java.util.stream.Collectors;
 
 public class MetaDataColumns {
   private final List<MetaDataColumn> columns = new ArrayList<>();
-
+  private static final String COMMA = ",";
   private MetaDataColumns(List<MetaDataColumn> metaColumns) {
     columns.addAll(metaColumns);
   }
 
   public static MetaDataColumns of(Class<?> clazz, Dialect dialect){
     List<MetaDataColumn> metaColumns = Arrays.stream(clazz.getDeclaredFields())
-                    .map(field -> MetaDataColumn.of(field, dialect.convertToColumn(field)))
-                    .collect(Collectors.toList());
+            .filter(field -> isNotTransient(List.of(field.getAnnotations())))
+            .map(field -> MetaDataColumn.of(field, dialect.convertToColumn(field)))
+            .collect(Collectors.toList());
 
     return new MetaDataColumns(metaColumns);
   }
@@ -27,7 +30,16 @@ public class MetaDataColumns {
     StringBuilder stringBuilder = new StringBuilder();
     for(MetaDataColumn dataColumn: columns){
       stringBuilder.append(dataColumn.getColumn());
+
+      if (!dataColumn.equals(columns.get(columns.size()-1))){
+        stringBuilder.append(COMMA);
+      }
     }
+
     return stringBuilder.toString();
+  }
+  private static boolean isNotTransient(List<Annotation> annotations) {
+    return !annotations.stream()
+            .anyMatch(annotation -> annotation.annotationType().equals(Transient.class));
   }
 }
