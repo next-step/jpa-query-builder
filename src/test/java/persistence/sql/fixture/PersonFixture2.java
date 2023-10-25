9b0b5909f1a19d47d860be0dd727b93c34d8149a
Jivebread <spark@troy.edu>
2023-10-25 20:04:49 +0900

    test(MetaData): MetaData 관련 객체들을 테스트합니다.

---
+++ src/test/java/persistence/sql/fixture/PersonFixture2.java
@@ -0,0 +1,24 @@
+package persistence.sql.fixture;
+
+import jakarta.persistence.*;
+
+@Entity
+public class PersonFixture2 {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "nick_name")
+  private String name;
+
+  @Column(name = "old")
+  private Integer age;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column
+  private Integer index;
+
+}
--- src/test/java/persistence/sql/meta/MetaDataTableTest.java
+++ src/test/java/persistence/sql/meta/MetaDataTableTest.java
@@ -4,10 +4,10 @@ import jakarta.persistence.Table;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import persistence.dialect.h2.H2Dialect;
 import persistence.meta.MetaDataTable;
 import persistence.sql.fixture.PersonFixture;
 import persistence.sql.fixture.PersonFixture2;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -21,27 +21,29 @@ public class MetaDataTableTest {
     person1 = PersonFixture.class;
     person2 = PersonFixture2.class;
   }
+
   @Test
   @DisplayName("1.2.1 Clazz의 클래스 명과 table의 명이 매칭 됩니다.")
   public void matchClazzNameandTableName() {
-    
+
     MetaDataTable table = MetaDataTable.of(person2);
 
-    assertAll( {
-      () -> assertThat(table.getTableName()).isEqualTo("PERSONFIXTURE2");
-    });
+    assertAll(
+            () -> assertThat(table.getName()).isEqualTo("PERSONFIXTURE2")
+    );
   }
 
   @Test
   @DisplayName("1.2.3 @Table에 name 값을 추가하였을 때, metaDataTable을 생성 할 수 있다.")
   public void requireEntityAnnotation() {
     Table tableAnnotation = person1.getAnnotation(Table.class);
-    MetaDataTable table = MetaDataTable.of(person2);
 
-    assertAll( {
-      () -> assertThat(tableAnnotation.name()).isEqualTo("users");
-      () -> assertThat(table.getTableName()).isEqualTo("USERS");
-    });
+    MetaDataTable table = MetaDataTable.of(person1);
+
+    assertAll(
+            () -> assertThat(tableAnnotation.name()).isEqualTo("users"),
+            () -> assertThat(table.getName()).isEqualTo("USERS")
+    );
   }
 
 }
