18358c057560d586d8f66f7d378d91592729a5c9
Jivebread <spark@troy.edu>
2023-10-25 20:24:06 +0900

    chore(project): 잘못들어간 커멘트 제거

--- src/main/java/persistence/meta/MetaData.java
+++ src/main/java/persistence/meta/MetaData.java
@@ -1,20 +1,6 @@
 package persistence.meta;
 
-import org.h2.table.MetaTable;
 import persistence.dialect.Dialect;
-
-import java.util.List;
-
-
-// 1) Columns 받고
-// 2) columns를 data type에 맞게 받고 sql로 넘겨주기 (여기서 mapper 가 dialect에서 가져오면될듯)
-//   a) nullable 값 받기 --> 값받아서  이거
-//   b) transient 도 -> annotation 읽고 db 만 안잡기
-// 3) value 값도 맵핑하기
-// 4) table 네임은 가볍게 가져오기
-// 5) annotation으로 id, column 값 가져오기 name(db 값 가져오기)
-//   a) generatedValue(Identity 관련해서 어떻게 해야할지 생각해보기)
-//   b)
 
 public class MetaData {
   private final MetaDataTable metaDataTable;
