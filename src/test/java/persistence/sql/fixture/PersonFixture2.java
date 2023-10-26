9b579d7fa4fa4424b6cc793c5c3efc0b7f8cd8d8
Jivebread <spark@troy.edu>
2023-10-26 19:17:56 +0900

    Chore(projectReformat): 리뷰 관련 코드 수정 및 패키지 이동

--- src/test/java/persistence/sql/fixture/PersonFixture2.java
+++ src/test/java/persistence/sql/fixture/PersonFixture2.java
@@ -4,20 +4,15 @@ import jakarta.persistence.*;
 
 @Entity
 public class PersonFixture2 {
-
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   @Column(name = "nick_name")
   private String name;
-
   @Column(name = "old")
   private Integer age;
-
   @Column(nullable = false)
   private String email;
-
   @Column
   private Integer index;
 
