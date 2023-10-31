ec799133092cbf933bb098bba90382dfaeaedccd
jiyeong-dt <imjee02@gmail.com>
2023-10-31 22:53:07 +0900

    feat(step3): SelectQueryBuilder에서 findAll 쿼리 생성

--- src/main/java/persistence/sql/dml/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/SelectQueryBuilder.java
@@ -1,9 +1,13 @@
 package persistence.sql.dml;
 
+import persistence.sql.ddl.EntityMetaData;
+
 public class SelectQueryBuilder {
     private static final String SELECT_QUERY_FORMAT = "select %s from %s";
 
 
-
+    public String findAll(final EntityMetaData entityMetaData) {
+        return String.format(SELECT_QUERY_FORMAT , "*" , entityMetaData.getTableName());
+    }
 
 }
