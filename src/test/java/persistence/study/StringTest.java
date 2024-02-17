0c14907d981d30f5b012ad59cc2aa99a2aed96e2
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-02-17 15:37:17 +0900

    feat : 123 이라는 숫자를 문자열로 반환

---
+++ src/test/java/persistence/study/StringTest.java
@@ -0,0 +1,12 @@
+package persistence.study;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+    @Test
+    void convert() {
+        String str = "123";
+        System.out.println(str);
+    }
+}
