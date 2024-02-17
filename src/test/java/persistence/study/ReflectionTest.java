501e5971f494cb73baad059171f6bd4af3fcb908
qwe5507 <apvmffkdls@gmail.com>
2024-02-17 16:35:42 +0900

    test: test로 시작하는 메소드 실행

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -18,5 +23,22 @@ public class ReflectionTest {
         Arrays.stream(carClass.getDeclaredFields()).forEach(System.out::println);
         Arrays.stream(carClass.getConstructors()).forEach(System.out::println);
         Arrays.stream(carClass.getDeclaredMethods()).forEach(System.out::println);
+    }
+
+    @DisplayName("test로 시작하는 메소드 실행한다.")
+    @Test
+    void testMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        String name = "simpson";
+        int age = 31;
+        final Class<Car> carClass = Car.class;
+        final Constructor<?>[] declaredConstructors = carClass.getDeclaredConstructors();
+        final Car car = carClass.getConstructor(String.class, int.class).newInstance(name, age);
+
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                final String result = (String) method.invoke(car);
+                logger.info(result);
+            }
+        }
     }
 }
