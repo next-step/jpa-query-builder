9aaf3dd12754070850e779d05457109cff1f02ad
djawnstj <90193598+djawnstj@users.noreply.github.com>
2024-02-17 15:42:39 +0900

    feat: TDD 실습 (#116)

---
+++ src/test/java/persistence/study/StringTest.java
@@ -0,0 +1,14 @@
+package persistence.study;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    public void convert() throws Exception {
+        int num = 123;
+
+        final String numString = String.valueOf(num);
+    }
+
+}
