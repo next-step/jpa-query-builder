32a31d03718fe58bbb30981b99b57f699d364c23
Seol Park <108073530+ParkSeolDev@users.noreply.github.com>
2024-02-17 15:43:58 +0900

    feat : step0 테스트 (#129)

---
+++ src/test/java/persistence/study/StringTest.java
@@ -0,0 +1,16 @@
+package persistence.study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+	@Test
+	void convert() {
+		// todo: 테스트 코드 만들어보기
+		int intValue = 123;
+		String strValue = Integer.toString(intValue);
+		assertThat(strValue.equals("123")).isTrue();
+	}
+}
