79c71529b209958979457b895cc66209f68ba17b
Jaeyoung You <ruew12@gmail.com>
2024-02-17 15:47:50 +0900

    🚀 0단계 - TDD 실습 (#132)
    * feat: 요구사항1
    
    * 123 이라는 숫자를 문자열로 반환
    
    * feat: 요구사항1
    
    * 123 이라는 숫자를 문자열로 반환

---
+++ src/test/java/persistence/study/StringTest.java
@@ -0,0 +1,11 @@
+package persistence.study;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void convert() {
+        Integer a = 123;
+        System.out.println(a);
+    }
+}
