046944fd01988bdc66d38dfac22feef3b81c1a7e
who-is-hu <dydgndlaek97@gmail.com>
2024-02-17 18:50:10 +0900

    test: step1 reflection 학습테스트

---
+++ src/test/java/persistence/study/Car.java
@@ -0,0 +1,34 @@
+package persistence.study;
+
+public class Car {
+    private String name;
+    private int price;
+
+    public Car() { }
+
+    public Car(final String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @PrintView
+    public void printView() {
+        System.out.println("자동차 정보를 출력합니다.");
+    }
+
+    public String testGetName() {
+        return "test: " + name;
+    }
+
+    public String testGetPrice() {
+        return "test: " + price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,90 @@
+package persistence.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    Class<Car> carClass = Car.class;
+
+    @Test
+    @DisplayName("car 객체 정보 가져오기")
+    void showClass() {
+        logger.debug(carClass.getName());
+    }
+
+
+    @Test
+    @DisplayName("car 객체의 이름이 test 로 시작하는 메서드만 실행하기")
+    void testMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance("test", 123);
+        final String targetMethodNamePrefix = "test";
+
+        Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith(targetMethodNamePrefix))
+                .forEach(method -> {
+                    try {
+                        String result = (String) method.invoke(car);
+                        assertThat(result).startsWith("test:");
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    @Test
+    @DisplayName("car 객체의 @PrintView 어노테이션이 붙은 메서드만 실행하기")
+    public void testAnnotationMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Car car = carClass.getDeclaredConstructor().newInstance();
+        Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .forEach(method -> {
+                    try {
+                        method.invoke(car);
+                        // print test ??...
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    @Test
+    @DisplayName("car 객체의 private 필드에 접근하기")
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        final String name = "testCar";
+        final int price = 10_000;
+        Car car = carClass.getDeclaredConstructor().newInstance();
+
+        Field nameField = carClass.getDeclaredField("name");
+        nameField.setAccessible(true);
+        nameField.set(car, name);
+
+        Field priceField = carClass.getDeclaredField("price");
+        priceField.setAccessible(true);
+        priceField.set(car, price);
+
+        assertThat(car.getName()).isEqualTo(name);
+        assertThat(car.getPrice()).isEqualTo(price);
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final String name = "testCar";
+        final int price = 10_000;
+        Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance(name, price);
+
+        assertThat(car).isNotNull();
+        assertThat(car.getName()).isEqualTo(name);
+        assertThat(car.getPrice()).isEqualTo(price);
+    }
+}
+
