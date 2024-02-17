f4c712aeda09a98b4d4cfb321e321af2fdf88628
Kiyeon Cho <qsky8520@gmail.com>
2024-02-17 15:44:46 +0900

    0단계 - TDD 실습 (#130)
    * docs(README.md): add feature list
    
    * feat(Converter): add 숫자를 문자열로 변경하는 기능

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jpa-query-builder
+
+## 구현할 기능 목록
+- Converter
+  - [x] 숫자를 문자열로 변경하는 기능
---
+++ src/test/java/persistence/study/Converter.java
@@ -0,0 +1,8 @@
+package persistence.study;
+
+public class Converter {
+
+  public static String numberToString(int num) {
+    return String.valueOf(num);
+  }
+}
