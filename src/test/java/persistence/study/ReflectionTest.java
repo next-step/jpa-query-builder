74888b51bd5bb65780e29c4cb055cf09d8265f32
who-is-hu <dydgndlaek97@gmail.com>
2024-02-18 20:59:09 +0900

    refactor: step1 test 피드백 반영
    refactor: 필드 값 바꾸는 부분 함수로 빼서 중복제거
    
    refactor: assertSoftly 적용

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,5 +1,6 @@
 package persistence.study;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,7 +13,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -27,16 +28,22 @@ public class ReflectionTest {
 
     @Test
     @DisplayName("car 객체의 이름이 test 로 시작하는 메서드만 실행하기")
-    void testMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    void testMethodRun() throws Exception {
         Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance("test", 123);
         final String targetMethodNamePrefix = "test";
 
+        assertSoftly(softly -> {
+            assertMethodNameStartWithPrefixRun(car, targetMethodNamePrefix, softly);
+        });
+    }
+
+    private void assertMethodNameStartWithPrefixRun(Car car, String targetMethodNamePrefix, SoftAssertions softly) {
         Arrays.stream(carClass.getDeclaredMethods())
                 .filter(method -> method.getName().startsWith(targetMethodNamePrefix))
                 .forEach(method -> {
                     try {
                         String result = (String) method.invoke(car);
-                        assertThat(result).startsWith("test:");
+                        softly.assertThat(result).startsWith("test:");
                     } catch (IllegalAccessException | InvocationTargetException e) {
                         throw new RuntimeException(e);
                     }
@@ -45,52 +52,65 @@ public class ReflectionTest {
 
     @Test
     @DisplayName("car 객체의 @PrintView 어노테이션이 붙은 메서드만 실행하기")
-    public void testAnnotationMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public void testAnnotationMethodRun() throws Exception {
         Car car = carClass.getDeclaredConstructor().newInstance();
 
+        assertSoftly(softly -> {
+            assertTestAnnotationMethodRun(car, softly);
+        });
+
+        System.setOut(System.out);
+    }
+
+    private void assertTestAnnotationMethodRun(Car car, SoftAssertions softly) {
         Arrays.stream(carClass.getDeclaredMethods())
                 .filter(method -> method.isAnnotationPresent(PrintView.class))
                 .forEach(method -> {
                     try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
                         System.setOut(new PrintStream(outputStream));
                         method.invoke(car);
-                        assertThat(outputStream.toString()).isEqualTo("자동차 정보를 출력합니다.\n");
+                        softly.assertThat(outputStream.toString()).isEqualTo("자동차 정보를 출력합니다.\n");
                     } catch (IllegalAccessException | InvocationTargetException | IOException e) {
                         throw new RuntimeException(e);
                     }
                 });
-        System.setOut(System.out);
     }
 
     @Test
     @DisplayName("car 객체의 private 필드에 접근하기")
-    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+    public void privateFieldAccess() throws Exception {
         final String name = "testCar";
         final int price = 10_000;
         Car car = carClass.getDeclaredConstructor().newInstance();
 
-        Field nameField = carClass.getDeclaredField("name");
-        nameField.setAccessible(true);
-        nameField.set(car, name);
+        setFiledValue(car, "name", name);
+        setFiledValue(car, "price", price);
 
-        Field priceField = carClass.getDeclaredField("price");
-        priceField.setAccessible(true);
-        priceField.set(car, price);
-
-        assertThat(car.getName()).isEqualTo(name);
-        assertThat(car.getPrice()).isEqualTo(price);
+        assertSoftly(softly -> {
+            softly.assertThat(car.getName()).isEqualTo(name);
+            softly.assertThat(car.getPrice()).isEqualTo(price);
+        });
     }
+
 
     @Test
     @DisplayName("인자를 가진 생성자의 인스턴스 생성")
-    void constructorWithArgs() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    void constructorWithArgs() throws Exception {
         final String name = "testCar";
         final int price = 10_000;
         Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance(name, price);
 
-        assertThat(car).isNotNull();
-        assertThat(car.getName()).isEqualTo(name);
-        assertThat(car.getPrice()).isEqualTo(price);
+        assertSoftly(softly -> {
+            softly.assertThat(car).isNotNull();
+            softly.assertThat(car.getName()).isEqualTo(name);
+            softly.assertThat(car.getPrice()).isEqualTo(price);
+        });
+    }
+
+    private void setFiledValue(Object targetObject, String fieldName, Object value) throws Exception {
+        Field field = carClass.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(targetObject, value);
     }
 }
 
