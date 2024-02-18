10ce4eb6979095f250a9a6f1a7429ebc0dde082b
junseo <djawnstj44@naver.com>
2024-02-18 23:36:21 +0900

    test: DefaultDdlQueryBuilderTest buildCreateQuery 메서드 예외 테스트 추가

--- src/test/java/persistence/sql/ddl/DefaultDdlQueryBuilderTest.java
+++ src/test/java/persistence/sql/ddl/DefaultDdlQueryBuilderTest.java
@@ -2,22 +2,23 @@ package persistence.sql.ddl;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import persistence.sql.QueryException;
 import persistence.sql.dialect.Dialect;
 import persistence.sql.dialect.H2Dialect;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class DefaultDdlQueryBuilderTest {
 
-    private Dialect dialect= new H2Dialect();
+    private final Dialect dialect= new H2Dialect();
+    private final DdlQueryBuilder<Object> queryBuilder = new DefaultDdlQueryBuilder<>(dialect);
 
     @DisplayName("PersonV1 Entity 정보로 create 쿼리 만들어보기")
     @Test
-
-    public void generateCreateQuery() throws Exception {
+    public void buildCreateQuery() throws Exception {
         // given
         final PersonV1 person = new PersonV1(1L, "name", 20);
-        final DdlQueryBuilder<Object> queryBuilder = new DefaultDdlQueryBuilder<>(dialect);
 
         final String ddl = """
                 CREATE TABLE PERSONV1 (
@@ -31,6 +32,18 @@ class DefaultDdlQueryBuilderTest {
 
         // then
         assertThat(createQuery).isEqualTo(ddl);
+    }
+
+    @DisplayName("")
+    @Test
+    public void throwExceptionWhenBuildNotEntityCreateQuery() throws Exception {
+        // given
+        final PersonV0 person = new PersonV0();
+
+        // when then
+        assertThatThrownBy(() -> queryBuilder.buildCreateQuery(person))
+                .isInstanceOf(QueryException.class)
+                .hasMessage("PersonV0 is not entity");
     }
 
 }
