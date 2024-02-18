94d2f9a2229c797403bac455d4d65e2ca47010dd
ckyeon <qsky8520@gmail.com>
2024-02-18 13:45:17 +0900

    feat(Reflection): add skeleton codes

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,10 @@
+package persistence.study;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReflectionTest {
+  private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+}
