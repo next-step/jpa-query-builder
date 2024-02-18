bcaff11ba84a96b5a61bcaeb1332981323953e07
ChangHoon Choi <69202388+hoon25@users.noreply.github.com>
2024-02-18 23:59:13 +0900

    🚀 1단계 - Reflection (#155)
    * docs: Step1-1 요구사항
    
    * test: Car 클래스 정보 출력
    
    * test: test로 시작하는 메소드 실행
    
    * feat: PrintView annotation 메소드 실행
    
    * test: Private Field에 값 할당
    
    * test: 인자를 가진 생성자의 인스턴스 생성
    
    * refactor(ReflectionTest): 테스트코드 리팩토링
    
    * docs: Step1 요구사항 정리
    
    * test(showClass): Class 정보 테스트 추가
    
    * test(printView): 출력값 테스트 추가
    
    * refactor(printViewTest): Mocking 테스트로 변경
    
    * test(testMethodRun): 테스트 추가
    
    * refactor(privateFieldAccess): for문 제거
    
    * remove(logger): 사용하지 않는 변수 제거
    
    * refactor(Class<Car>): 클래스 변수로 변경

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ### String 클래스의 학습 테스트 
 src>test>java>persistence>study 하위에 StringTest 클래스를 하나 생성한다
 - 123 이라는 숫자를 문자열로 반환
+
+## Step1 요구사항
+- 요구사항 1 - 클래스 정보 출력
+- 요구사항 2 - test로 시작하는 메소드 실행
+- 요구사항 3 - @PrintView 애노테이션 메소드 실행
+- 요구사항 4 - private field에 값 할당
+- 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,128 @@
+package persistence.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+public class ReflectionTest {
+    private static final Class<Car> carClass = Car.class;
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        // 패키지+클래스 이름
+        assertThat(carClass.getName()).isEqualTo("persistence.study.Car");
+
+        // 모든 필드 목록
+        List<String> fields = Arrays.stream(carClass.getDeclaredFields())
+                .map(Field::toString)
+                .toList();
+        assertThat(fields).contains(
+                "private java.lang.String persistence.study.Car.name",
+                "private int persistence.study.Car.price"
+        );
+
+        // 모든 생성자 반환
+        List<String> constructors = Arrays.stream(carClass.getDeclaredConstructors())
+                .map(Constructor::toString)
+                .toList();
+        assertThat(constructors).contains(
+                "public persistence.study.Car()",
+                "public persistence.study.Car(java.lang.String,int)"
+        );
+
+        // 모든 메서드 반환
+        List<String> methods = Arrays.stream(carClass.getDeclaredMethods())
+                .map(Method::toString)
+                .toList();
+        assertThat(methods).contains(
+                "public java.lang.String persistence.study.Car.getName()",
+                "public void persistence.study.Car.printView()",
+                "public int persistence.study.Car.getPrice()",
+                "public java.lang.String persistence.study.Car.testGetName()",
+                "public java.lang.String persistence.study.Car.testGetPrice()"
+        );
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드 실행")
+    void testMethodRun() throws InvocationTargetException, IllegalAccessException {
+        Car mockCar = mock(Car.class);
+
+        List<Method> startWithTestMethods = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+        for (final Method method : startWithTestMethods) {
+            method.invoke(mockCar);
+        }
+
+        assertAll(
+                () -> verify(mockCar, times(1)).testGetName(),
+                () -> verify(mockCar, times(1)).testGetPrice()
+        );
+    }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션 메소드 실행")
+    void testAnnotationMethodRun() throws InvocationTargetException, IllegalAccessException {
+        Car mockCar = mock(Car.class);
+
+        List<Method> annotationMethods = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .toList();
+        for (final Method method : annotationMethods) {
+            method.invoke(mockCar);
+        }
+
+        verify(mockCar, times(1)).printView();
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    void privateFieldAccess() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        final Class<Car> carClass = Car.class;
+        final Car car = carClass.getConstructor().newInstance();
+        final String name = "Sonata";
+        final int price = 1000;
+
+        Field nameField = carClass.getDeclaredField("name");
+        nameField.setAccessible(true);
+        nameField.set(car, name);
+        Field priceField = carClass.getDeclaredField("price");
+        priceField.setAccessible(true);
+        priceField.set(car, price);
+
+        assertAll(
+                () -> assertThat(car.getName()).isEqualTo(name),
+                () -> assertThat(car.getPrice()).isEqualTo(price)
+        );
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final String name = "Sonata";
+        final int price = 1000;
+
+        final Car car = carClass.getConstructor(String.class, int.class).newInstance(name, price);
+
+        assertAll(
+                () -> assertThat(car).isInstanceOf(Car.class),
+                () -> assertThat(car.getName()).isEqualTo(name),
+                () -> assertThat(car.getPrice()).isEqualTo(price)
+        );
+    }
+}
