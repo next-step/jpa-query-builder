c0131c884fd20d4abd31cb4b76ee8102a2769b6d
jinny <48121233+EunjinJwa@users.noreply.github.com>
2024-02-18 15:13:16 +0900

    1단계 - Reflection (#157)
    * 요구사항 1 - 클래스 정보 출력
    
    * Car 클래스의 정보 출력
    
    * test로 시작하는 메소드 실행
    
    * test로 시작하는 메소드 실행: 메서드 역할 재정의
    
    * @PrintView 애노테이션 메소드 실행
    
    * 인자를 가진 생성자의 인스턴스 생성

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,103 @@
+package persistence.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+
+        // Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.
+        for (Method method : carClass.getDeclaredMethods()) {
+            logger.debug(method.getName());
+        }
+        for (Field field : carClass.getFields()) {
+            logger.debug(field.getName());
+        }
+        for (Constructor<?> constructor : carClass.getConstructors()) {
+            logger.debug(constructor.getName());
+        }
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드 실행")
+    public void testMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+        Car car = Car.class.getConstructor().newInstance();
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (isTestMethod(method)) {
+                methodRun(car, method);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("PrintView 어노테이션 메소드 실행")
+    public void printViewMethodRun() throws Exception {
+        Class<Car> carClass = Car.class;
+        Car car = Car.class.getConstructor().newInstance();
+        for (Method method : carClass.getDeclaredMethods()){
+            if (method.isAnnotationPresent(PrintView.class)) {
+                methodRun(car, method);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    public void privateFieldAccess() throws Exception{
+        final String name = "타이칸";
+        final int price = 10000;
+
+        Class<Car> carClass = Car.class;
+        Car car = Car.class.getConstructor().newInstance();
+
+        setFieldValue(car, carClass.getDeclaredField("name"), name);
+        setFieldValue(car, carClass.getDeclaredField("price"), price);
+
+        Assertions.assertThat(name).isEqualTo(car.getName());
+        Assertions.assertThat(price).isEqualTo(car.getPrice());
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    public void constructorWithArgs() throws Exception {
+        final String name = "타이칸";
+        final int price = 10000;
+
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance(name, price);
+
+        Assertions.assertThat(name).isEqualTo(car.getName());
+        Assertions.assertThat(price).isEqualTo(car.getPrice());
+    }
+
+
+
+    private void methodRun(Object instance, Method method) throws Exception {
+
+        method.setAccessible(true);
+        method.invoke(instance);
+    }
+
+    private boolean isTestMethod(Method method) {
+        return method.getName().startsWith("test");
+    }
+
+    private void setFieldValue(Object instance, Field field, Object value) throws IllegalAccessException {
+        field.setAccessible(true);
+        field.set(instance, value);
+    }
+}
