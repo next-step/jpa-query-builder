15c567329b560fe017d0ab5fc701996a0bfebe51
qwe5507 <apvmffkdls@gmail.com>
2024-02-18 19:17:56 +0900

    docs: 요구사항 3 작성

---
+++ src/main/java/persistence/domain/Person3.java
@@ -0,0 +1,18 @@
+package persistence.domain;
+
+
+@Table(name = "users")
+@Entity
+public class Person3 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "old")
+    private Integer age;
+    @Column(nullable = false)
+    private String email;
+    @Column(name = "nick_name")
+    private String name;
+    @Transient
+    private Integer index;
+}
