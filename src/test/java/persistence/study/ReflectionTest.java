01c9383410dd2bee2be14efedfbb5be5c329717e
YooInKeun <keun0390@naver.com>
2024-02-18 11:23:22 +0900

    refactor: method invoke 하는 부분, 메소드로 분리

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -16,6 +23,7 @@ public class ReflectionTest {
     @DisplayName("Car 객체 정보 가져오기")
     void showClass() {
         Class<Car> carClass = Car.class;
+
         logger.debug(carClass.getName());
         logger.debug(Arrays.toString(carClass.getDeclaredFields()));
         logger.debug(Arrays.toString(carClass.getDeclaredConstructors()));
@@ -25,41 +33,42 @@ public class ReflectionTest {
     @Test
     @DisplayName("test 로 시작하는 메소드 실행하기")
     void testMethodRun() {
+        // given
         Class<Car> carClass = Car.class;
-        Arrays.stream(carClass.getDeclaredMethods())
+
+        // when
+        List<Object> result = Arrays.stream(carClass.getDeclaredMethods())
                 .filter(method -> method.getName().startsWith("test"))
-                .forEach(testMethod -> {
-                    try {
-                        Object result = testMethod.invoke(carClass.newInstance());
-                        logger.debug((String) result);
-                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                        logger.error(e.getMessage());
-                    }
-                });
+                .map(testMethod -> invokeMethod(testMethod, carClass))
+                .collect(Collectors.toList());
+
+        // then
+        assertAll(
+                () -> assertThat(result.size()).isEqualTo(2),
+                () -> assertThat(result).containsAll(List.of("test : null", "test : 0"))
+        );
     }
 
     @Test
     @DisplayName("@PrintView 애노테이션 메소드 실행")
     void testAnnotationMethodRun() {
+        // given
         Class<Car> carClass = Car.class;
+
+        // when
         Arrays.stream(carClass.getDeclaredMethods())
                 .filter(method -> method.isAnnotationPresent(PrintView.class))
-                .forEach(method -> {
-                    try {
-                        Object result = method.invoke(carClass.newInstance());
-                        logger.debug((String) result);
-                    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                        logger.error(e.getMessage());
-                    }
-                });
+                .forEach(method -> invokeMethod(method, carClass));
     }
 
     @Test
     @DisplayName("private field 에 값 할당")
     void privateFieldAccess() throws InstantiationException, IllegalAccessException, NoSuchFieldException {
+        // given
         Class<Car> carClass = Car.class;
         Car car = carClass.newInstance();
 
+        // when
         Field name = carClass.getDeclaredField("name");
         Field price = carClass.getDeclaredField("price");
 
@@ -69,24 +78,39 @@ public class ReflectionTest {
         name.set(car, "소나타");
         price.set(car, 100000);
 
-        logger.debug(car.testGetName());
-        logger.debug(car.testGetPrice());
+        // then
+        assertAll(
+                () -> assertThat(car.testGetName()).isEqualTo("test : 소나타"),
+                () -> assertThat(car.testGetPrice()).isEqualTo("test : 100000")
+        );
     }
 
     @Test
     @DisplayName("인자를 가진 생성자의 인스턴스 생성")
-    void constructorWithArgs() {
+    void constructorWithArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        // given
         Class<Car> carClass = Car.class;
-        Arrays.stream(carClass.getDeclaredConstructors())
+
+        // when
+        Constructor<?> constructorWithArgs = Arrays.stream(carClass.getDeclaredConstructors())
                 .filter(constructor -> constructor.getParameterCount() > 0)
-                .forEach(constructor -> {
-                    try {
-                        Car car = (Car) constructor.newInstance("소나타", 10000);
-                        logger.debug(car.testGetName());
-                        logger.debug(car.testGetPrice());
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                        logger.error(e.getMessage());
-                    }
-                });
+                .findFirst().get();
+
+        Car car = (Car) constructorWithArgs.newInstance("소나타", 100000);
+
+        // then
+        assertAll(
+                () -> assertThat(car.testGetName()).isEqualTo("test : 소나타"),
+                () -> assertThat(car.testGetPrice()).isEqualTo("test : 100000")
+        );
+    }
+
+    private Object invokeMethod(Method method, Class<Car> car) {
+        try {
+            return method.invoke(car.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            logger.error(e.getMessage());
+            return null;
+        }
     }
 }
