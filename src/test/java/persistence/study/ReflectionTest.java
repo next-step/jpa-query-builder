0dd3bbd2dcf0799224c7331181ab4a0e6e407ed4
ckyeon <qsky8520@gmail.com>
2024-02-18 15:35:55 +0900

    feat(Reflection): 요구사항 1 구현

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,10 +1,19 @@
 package persistence.study;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReflectionTest {
   private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+  private final Class<Car> carClass = Car.class;
 
+  @Test
+  @DisplayName("Car 객체 정보 가져오기")
+  void showClass() {
+    String carClassName = carClass.getName();
+    logger.debug(carClassName);
+  }
 }
