980f937d92e94876fca51cef031e740c99a24c34
joydaheecha <daheecha0612@gmail.com>
2024-02-18 15:18:00 +0900

    (test) 클래스 이름을 출력하는 테스트를 추가하라 (요구사항1)

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,19 @@
+package persistence.study;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+    }
+}
