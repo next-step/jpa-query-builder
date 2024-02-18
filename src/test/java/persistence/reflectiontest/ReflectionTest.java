c579865e5ffb485374577125b03d58cb677b2b0c
jaesungahn91 <jaesungahn91@gmail.com>
2024-02-18 18:20:38 +0900

    refactor: 요구사항 1. 리팩토링
    - 검증 코드 추가

--- src/test/java/persistence/reflectiontest/ReflectionTest.java
+++ src/test/java/persistence/reflectiontest/ReflectionTest.java
@@ -5,31 +5,57 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class ReflectionTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ReflectionTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
     @DisplayName("Car 객체 정보 가져오기")
     void showClass() {
-        // Car 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력
+        // given
+        String expectedClassName = "persistence.reflectiontest.Car";
+        List<String> expectedFieldNames = List.of("name", "price");
+        List<String> expectedConstructorNames = List.of("persistence.reflectiontest.Car", "persistence.reflectiontest.Car");
+        List<String> expectedMethodNames = List.of("printView", "testGetName", "testGetPrice");
+
+        // when
         Class<Car> carClass = Car.class;
+        String className = carClass.getName();
 
-        // fields
-        Arrays.stream(carClass.getDeclaredFields())
-                .forEach(field -> logger.debug(field.getName()));
+        List<String> fieldNames = Arrays.stream(carClass.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.toList());
 
-        // constructors
-        Arrays.stream(carClass.getConstructors())
-                .forEach(constructor -> logger.debug(constructor.getName()));
 
-        // methods
-        Arrays.stream(carClass.getMethods())
-                .forEach(method -> logger.debug(method.getName()));
+        List<String> constructorNames = Arrays.stream(carClass.getDeclaredConstructors())
+                .map(Constructor::getName)
+                .collect(Collectors.toList());
+
+
+        List<String> methodNames = Arrays.stream(carClass.getDeclaredMethods())
+                .map(Method::getName)
+                .collect(Collectors.toList());
+
+        // then
+        logger.debug(className);
+        fieldNames.forEach(logger::debug);
+        constructorNames.forEach(logger::debug);
+        methodNames.forEach(logger::debug);
+        assertAll(
+                () -> assertThat(className).isEqualTo(expectedClassName),
+                () -> assertThat(fieldNames).containsExactlyInAnyOrderElementsOf(expectedFieldNames),
+                () -> assertThat(constructorNames).containsExactlyInAnyOrderElementsOf(expectedConstructorNames),
+                () -> assertThat(methodNames).containsExactlyInAnyOrderElementsOf(expectedMethodNames)
+        );
     }
-
-
-
 }
