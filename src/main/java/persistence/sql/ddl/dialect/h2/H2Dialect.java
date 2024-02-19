5695ce35bb67bf2d832d17bf991941a606686fb4
Shin seongsu <tlstjdtn321@naver.com>
2024-02-20 01:56:30 +0900

    feat: Dialect 설정하는 인터페이스 설정

---
+++ src/main/java/persistence/sql/ddl/dialect/h2/H2Dialect.java
@@ -0,0 +1,22 @@
+package persistence.sql.ddl.dialect.h2;
+
+import persistence.sql.ddl.dialect.Dialect;
+import persistence.sql.ddl.dialect.database.TypeMapper;
+import persistence.sql.ddl.query.EntityMappingTable;
+import persistence.sql.ddl.query.builder.CreateQueryBuilder;
+
+public class H2Dialect extends Dialect {
+    private final TypeMapper typeMapper;
+
+    public H2Dialect() {
+        this.typeMapper = H2TypeMapper.newInstance();
+    }
+
+    @Override
+    public String createTable(Class<?> clazz) {
+        final EntityMappingTable entityMappingTable = EntityMappingTable.from(clazz);
+
+        final CreateQueryBuilder queryBuilder = new CreateQueryBuilder(entityMappingTable);
+        return queryBuilder.toSql(typeMapper);
+    }
+}
