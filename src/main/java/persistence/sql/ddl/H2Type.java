a4e9be314131dcb0f5da22db68dba2f73fe649c0
juhyun <zzang9ha@naver.com>
2024-02-20 00:31:07 +0900

    refactor: length 필드 추가

--- src/main/java/persistence/sql/ddl/H2Type.java
+++ src/main/java/persistence/sql/ddl/H2Type.java
@@ -4,25 +4,39 @@ import java.util.Arrays;
 
 public enum H2Type {
 
-    INTEGER(Integer.class, "INTEGER"),
+    INTEGER(Integer.class, "INTEGER", 0),
 
-    LONG(Long.class, "BIGINT"),
+    LONG(Long.class, "BIGINT", 0),
 
-    STRING(String.class, "VARCHAR(255)");
+    STRING(String.class, "VARCHAR", 255);
 
     private final Class<?> classType;
     private final String columnType;
+    private final Integer length;
 
-    H2Type(Class<?> classType, String columnType) {
+    H2Type(Class<?> classType, String columnType, Integer length) {
         this.classType = classType;
         this.columnType = columnType;
+        this.length = length;
     }
 
     public static String converter(Class<?> type) {
         return Arrays.stream(values())
                 .filter(h2 -> h2.classType.equals(type))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(type.getName() + " is not exist!"))
-                .columnType;
+                .map(H2Type::converterLength)
+                .orElseThrow(() -> new IllegalArgumentException(type.getName() + " is not exist!"));
+    }
+
+    private static String converterLength(H2Type h2Type) {
+        if (isDefaultLength(h2Type)) {
+            return h2Type.columnType;
+        }
+
+        return h2Type.columnType + "(" + h2Type.length + ")";
+    }
+
+    private static boolean isDefaultLength(H2Type h2Type) {
+        return h2Type.length == 0;
     }
 }
