a1e03120cfc2ca8761d428a94ae0c047655ab539
JUNSEO <djawnstj44@naver.com>
2024-02-19 16:50:35 +0900

    feat: Dialect 수정
    Dialect 객체는 각 DB 문법에 맞는 쿼리 문자열만 반환하도록 변경하였다.

--- src/main/java/persistence/sql/dialect/Dialect.java
+++ src/main/java/persistence/sql/dialect/Dialect.java
@@ -1,9 +1,11 @@
 package persistence.sql.dialect;
 
+import persistence.sql.query.Column;
+
 public interface Dialect {
 
-    String createColumnQuery(final String type);
+    String convertColumnType(final int type, final int length);
 
-    String getPk();
+    String toDialectKeywords(final Column column);
 
 }
--- src/main/java/persistence/sql/dialect/H2Dialect.java
+++ src/main/java/persistence/sql/dialect/H2Dialect.java
@@ -1,30 +1,48 @@
 package persistence.sql.dialect;
 
+import jakarta.persistence.GenerationType;
+import persistence.sql.query.Column;
+
 import java.sql.Types;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class H2Dialect implements Dialect {
 
-    private final Map<String, Integer> columnTypes = Map.of(
-            "STRING", Types.VARCHAR,
-            "INTEGER", Types.INTEGER,
-            "LONG", Types.BIGINT);
+    private final String notNull = "NOT NULL";
+
+    private final String autoIncrement = "AUTO_INCREMENT";
+
+    private final String primaryKey = "PRIMARY KEY";
+
+    private final String unique = "UNIQUE";
 
     @Override
-    public String createColumnQuery(final String type) {
-        final Integer columnType = columnTypes.getOrDefault(type.toUpperCase(), Types.VARCHAR);
-
-        return switch (columnType) {
-            case Types.VARCHAR -> "VARCHAR(255)";
+    public String convertColumnType(final int type, final int length) {
+        return switch (type) {
+            case Types.VARCHAR -> "VARCHAR(" + length + ")";
             case Types.INTEGER -> "INTEGER";
             case Types.BIGINT -> "BIGINT";
-            default -> throw new DialectException("NotFount ColumnType for " + type + "in H2Dialect");
+            default -> throw new DialectException("NotFount ColumnType for " + type + " in H2Dialect");
         };
     }
 
     @Override
-    public String getPk() {
-        return "PRIMARY KEY";
+    public String toDialectKeywords(final Column column) {
+        List<String> keywords = new ArrayList<>();
+        if (!column.isNullable()) {
+            keywords.add(notNull);
+        }
+        if (column.isPk()) {
+            if (column.getPkStrategy() == GenerationType.IDENTITY) {
+                keywords.add(autoIncrement);
+            }
+            keywords.add(primaryKey);
+        } else if (column.isUnique()) {
+            keywords.add(unique);
+        }
+
+        return String.join(" ", keywords);
     }
 
 }
