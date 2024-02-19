a494fe9c11c67eeeadf114fe5fe7c7d0482403c9
hoon25 <cchoon95@hanmail.net>
2024-02-19 23:09:05 +0900

    feat(step2-2): Person객체 변경

--- src/main/java/persistence/sql/ddl/Person.java
+++ src/main/java/persistence/sql/ddl/Person.java
@@ -1,12 +1,23 @@
 package persistence.sql.ddl;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Person {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nick_name")
     private String name;
+
+    @Column(name = "old")
     private Integer age;
+
+    @Column(nullable = false)
+    private String email;
 }
