3a8fd4701bc59afe53f92bd0f7d72974aaf0aab1
daheecha0612 <dahee.cha@gmail.com>
2024-02-19 22:58:12 +0900

    (test) test로 시작하는 메서드를 실행하라(요구사항2)

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -6,14 +6,30 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
-    @DisplayName("Car 객체 정보 가져오기")
+    @DisplayName("Car 객체 정보를 출력하라")
     void showClass() {
         Class<Car> carClass = Car.class;
         logger.debug(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드를 실행하라")
+    void pickMethodStartsWithTest() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Class<Car> carClass = Car.class;
+
+        for (Method declaredMethod : carClass.getDeclaredMethods()) {
+            if (declaredMethod.getName().startsWith("test")){
+                declaredMethod.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
+            }
+        }
     }
 }
