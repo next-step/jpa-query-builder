9e896767793038f8dd5f815266afa74afb7fe204
Shin seongsu <tlstjdtn321@naver.com>
2024-02-20 01:58:49 +0900

    feat: 요구사항 1 만족하는 enitty 추가

---
+++ src/main/java/persistence/entity/Person1.java
@@ -0,0 +1,16 @@
+package persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person1 {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+}
