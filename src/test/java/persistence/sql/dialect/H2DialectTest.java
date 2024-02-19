ca3e3229d05c16771696e438d626e161ee515bbf
JUNSEO <djawnstj44@naver.com>
2024-02-19 17:01:08 +0900

    test: H2DialectTest 테스트 작성
    Dialect 의 역할을 변경함에 따라 기존 테스트코드를 수정하였다.
    추가로 Column 정보를 통해 H2 DB 에 알맞은 예약어 쿼리로 변환하는 메서드를 테스트하였다.

--- src/test/java/persistence/sql/dialect/H2DialectTest.java
+++ src/test/java/persistence/sql/dialect/H2DialectTest.java
@@ -1,27 +1,32 @@
 package persistence.sql.dialect;
 
+import jakarta.persistence.GenerationType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import persistence.sql.query.Column;
+
+import java.sql.Types;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class H2DialectTest {
 
     private final Dialect dialect = new H2Dialect();
 
-    @DisplayName("필드의 Type Name 을 받아 H2 DB 의 column type 을 반환한다")
+    @DisplayName("Type 을 받아 H2 DB 의 column type 을 반환한다")
     @Test
     public void createColumnType() throws Exception {
         // given
-        final String stringName = String.class.getSimpleName();
-        final String longName = Long.class.getSimpleName();
-        final String integerName = Integer.class.getSimpleName();
+        final int stringType = Types.VARCHAR;
+        final int longType = Types.BIGINT;
+        final int integerType = Types.INTEGER;
 
         // when
-        final String stringColumnType = dialect.createColumnQuery(stringName);
-        final String longColumnType = dialect.createColumnQuery(longName);
-        final String integerColumnType = dialect.createColumnQuery(integerName);
+        final String stringColumnType = dialect.convertColumnType(stringType, 255);
+        final String longColumnType = dialect.convertColumnType(longType, 255);
+        final String integerColumnType = dialect.convertColumnType(integerType, 255);
 
         // then
         assertAll(
@@ -31,27 +36,41 @@ class H2DialectTest {
         );
     }
 
-    @DisplayName("필드의 Type Name 으로 H2 DB 의 column type 을 반환할 때 적절한 type 을 찾지 못하면 VARCHAR 를 반환한다")
+    @DisplayName("Type 으로 H2 DB 의 column type 을 반환할 때 적절한 type 을 찾지 못하면 예외를 를 반환한다")
     @Test
     public void createNotfoundColumnType() throws Exception {
         // given
-        final String doubleName = Double.class.getSimpleName();
+        final int doubleType = Types.DOUBLE;
 
-        // when
-        final String doubleColumnType = dialect.createColumnQuery(doubleName);
-
-        // then
-        assertThat(doubleColumnType).isEqualTo("VARCHAR(255)");
+        // when then
+        assertThatThrownBy(() -> dialect.convertColumnType(doubleType, 255))
+                .isInstanceOf(DialectException.class)
+                .hasMessage("NotFount ColumnType for 8 in H2Dialect");
     }
 
-    @DisplayName("H2 DB 문법에 맞는 PK 예약어를 반환한다")
+    @DisplayName("Column 객체를 받아 H2 DB 에 맞는 예약어로 변환한다")
     @Test
-    public void getH2Pk() throws Exception {
+    public void convertToKeywords() throws Exception {
+        // given
+        final Column notNullAndUnique = new Column.Builder()
+                .setNullable(false)
+                .setUnique(true)
+                .build();
+
+        final Column pkAndIdentityAndUnique = new Column.Builder()
+                .setPk(true)
+                .setPkStrategy(GenerationType.IDENTITY)
+                .setUnique(true)
+                .build();
+
         // when
-        final String h2Pk = dialect.getPk();
+        final String result1 = dialect.toDialectKeywords(notNullAndUnique);
+        final String result2 = dialect.toDialectKeywords(pkAndIdentityAndUnique);
 
         // then
-        assertThat(h2Pk).isEqualTo("PRIMARY KEY");
+        assertAll(
+                () -> assertThat(result1).isEqualTo("NOT NULL UNIQUE"),
+                () -> assertThat(result2).isEqualTo("AUTO_INCREMENT PRIMARY KEY")
+        );
     }
-
 }
