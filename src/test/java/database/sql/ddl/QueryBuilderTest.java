9d9c1dc0cd99daecafb9dd757b316c104c3f9033
Dongyoon Lee <nine.cow@gmail.com>
2024-02-19 12:54:08 +0900

    test: QueryBuilderTest 에 ParameterizedTest 도입

--- src/test/java/database/sql/ddl/QueryBuilderTest.java
+++ src/test/java/database/sql/ddl/QueryBuilderTest.java
@@ -1,37 +1,31 @@
 package database.sql.ddl;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class QueryBuilderTest {
-
-    @Test
-    void buildCreateQuery() {
-        assertAll(
-                () -> assertCreateQuery(OldPerson1.class, "CREATE TABLE OldPerson1 (id BIGINT PRIMARY KEY, name VARCHAR(255) NULL, age INT NULL)"),
-                () -> assertCreateQuery(OldPerson2.class, "CREATE TABLE OldPerson2 (id BIGINT AUTO_INCREMENT PRIMARY KEY, nick_name VARCHAR(255) NULL, old INT NULL, email VARCHAR(255) NOT NULL)"),
-                () -> assertCreateQuery(Person.class, "CREATE TABLE users (id BIGINT AUTO_INCREMENT PRIMARY KEY, nick_name VARCHAR(255) NULL, old INT NULL, email VARCHAR(255) NOT NULL)")
-        );
-    }
-
-    private void assertCreateQuery(Class<?> entityClass, String expected) {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "database.sql.ddl.OldPerson1:CREATE TABLE OldPerson1 (id BIGINT PRIMARY KEY, name VARCHAR(255) NULL, age INT NULL)",
+            "database.sql.ddl.OldPerson2:CREATE TABLE OldPerson2 (id BIGINT AUTO_INCREMENT PRIMARY KEY, nick_name VARCHAR(255) NULL, old INT NULL, email VARCHAR(255) NOT NULL)",
+            "database.sql.ddl.Person:CREATE TABLE users (id BIGINT AUTO_INCREMENT PRIMARY KEY, nick_name VARCHAR(255) NULL, old INT NULL, email VARCHAR(255) NOT NULL)"
+    }, delimiter = ':')
+    void buildCreateQuery(String entityClassCanonicalName, String expected) throws ClassNotFoundException {
+        Class<?> entityClass = Class.forName(entityClassCanonicalName);
         QueryBuilder queryBuilder = new QueryBuilder();
         String actual = queryBuilder.buildCreateQuery(entityClass);
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void buildDeleteQuery() {
-        assertAll(
-                () -> assertDropQuery(OldPerson1.class, "DROP TABLE OldPerson1"),
-                () -> assertDropQuery(OldPerson2.class, "DROP TABLE OldPerson2"),
-                () -> assertDropQuery(Person.class, "DROP TABLE users")
-        );
-    }
-
-    private void assertDropQuery(Class<?> entityClass, String expected) {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "database.sql.ddl.OldPerson1:DROP TABLE OldPerson1",
+            "database.sql.ddl.OldPerson2:DROP TABLE OldPerson2",
+            "database.sql.ddl.Person:DROP TABLE users"
+    }, delimiter = ':')
+    void buildDeleteQuery(Class<?> entityClass, String expected) {
         QueryBuilder queryBuilder = new QueryBuilder();
         String actual = queryBuilder.buildDeleteQuery(entityClass);
         assertThat(actual).isEqualTo(expected);
