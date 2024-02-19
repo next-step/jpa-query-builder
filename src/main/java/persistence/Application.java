e637edded6362d7182b806418ac461d786448524
juhyun <zzang9ha@naver.com>
2024-02-20 00:33:07 +0900

    feat: 애플리케이션 main 실행 코드 추가

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -2,9 +2,14 @@ package persistence;
 
 import database.DatabaseServer;
 import database.H2;
+import domain.Person;
 import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import persistence.sql.ddl.CreateDDLQueryBuilder;
+import persistence.sql.ddl.DDLQueryBuilder;
+import persistence.sql.ddl.H2TypeConverter;
+import persistence.sql.ddl.TypeConverter;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -15,7 +20,13 @@ public class Application {
             final DatabaseServer server = new H2();
             server.start();
 
+            final TypeConverter typeConverter = new H2TypeConverter();
+            final DDLQueryBuilder queryBuilder = new CreateDDLQueryBuilder(typeConverter);
+            final String createQuery = queryBuilder.query(Person.class);
+
             final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
+
+            jdbcTemplate.execute(createQuery);
 
             server.stop();
         } catch (Exception e) {
