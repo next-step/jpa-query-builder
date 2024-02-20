354bb67c7b5909df378d6129ba701bb20d0c4087
ParkSeolDev <parkseol.dev@gmail.com>
2024-02-20 22:33:37 +0900

    feat : Car class 생성

--- src/test/java/persistence/study/StringTest.java
+++ src/test/java/persistence/study/StringTest.java
@@ -2,7 +2,6 @@ package persistence.study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
