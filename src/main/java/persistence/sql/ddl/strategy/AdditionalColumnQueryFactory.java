59b76245b9e309c73d49270f05ada8ec9c55307a
Hyunta <mohai2618@gmail.com>
2024-02-20 12:40:21 +0900

    refactor: Reflections 외부 라이브러리 제거

--- src/main/java/persistence/sql/ddl/strategy/AdditionalColumnQueryFactory.java
+++ src/main/java/persistence/sql/ddl/strategy/AdditionalColumnQueryFactory.java
@@ -1,12 +1,7 @@
 package persistence.sql.ddl.strategy;
 
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
-
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AdditionalColumnQueryFactory {
@@ -15,22 +10,13 @@ public class AdditionalColumnQueryFactory {
     }
 
     public static List<AdditionalColumQueryStrategy> getStrategies(Field field) {
-        Reflections reflections = new Reflections("persistence.sql.ddl.strategy", new SubTypesScanner(false));
-
-        Set<Class<? extends AdditionalColumQueryStrategy>> classes = reflections.getSubTypesOf(AdditionalColumQueryStrategy.class);
-
-        return classes.stream()
-                .filter(clazz -> !Modifier.isAbstract(clazz.getModifiers()))
-                .map(AdditionalColumnQueryFactory::instantiateStrategy)
+        List<AdditionalColumQueryStrategy> targetStrategies = List.of(
+                new AutoIncrementColumnStrategy(),
+                new NullableFalseColumnStrategy(),
+                new PrimaryKeyColumnStrategy()
+        );
+        return targetStrategies.stream()
                 .filter(strategy -> strategy.isRequired(field))
                 .collect(Collectors.toList());
-    }
-
-    private static AdditionalColumQueryStrategy instantiateStrategy(Class<? extends AdditionalColumQueryStrategy> clazz) {
-        try {
-            return clazz.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
     }
 }
