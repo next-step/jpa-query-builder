a813130a19f60e4436eef6518d47bb935b4e8819
정종은 | Brian | JJ <with.beth@gmail.com>
2024-02-21 00:56:42 +0900

    refactor(ReflectionTest): 자주 사용되는 코드 별도 메서드 및 필드로 extract

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -1,12 +1,19 @@
 package persistence.study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -14,69 +21,92 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    private static final Class<Car> carClass = Car.class;
+    private static final Class<Car> CAR_CLASS = Car.class;
+    private static final String CAR_NAME = "렉서스";
+    private static final int CAR_PRICE = 999;
+
+    private Car car;
+
+    @BeforeEach
+    void setup() {
+        try {
+            // Note : Integer.class NG, int.class OK
+            car = CAR_CLASS.getDeclaredConstructor(String.class, int.class).newInstance(CAR_NAME, CAR_PRICE);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object invokeCarMethod(Method method) {
+        try {
+            return method.invoke(car);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
-    @DisplayName("Car 객체 정보 가져오기")
+    @DisplayName("Reflection을 이용해 Car 객체 정보를 가져올수 있어야 한다")
     void showClass() {
         assertAll(
-                () -> assertThat(carClass.getSimpleName()).isEqualTo("Car"),
-                () -> assertThat(carClass.getDeclaredFields()).hasSize(2),
-                () -> assertThat(carClass.getConstructors()).hasSize(2),
-                () -> assertThat(carClass.getDeclaredMethods()).hasSize(3)
+                () -> assertThat(CAR_CLASS.getSimpleName()).isEqualTo("Car"),
+                () -> assertThat(Arrays.stream(CAR_CLASS.getDeclaredFields())
+                        .map(Field::getName)
+                        .collect(Collectors.toUnmodifiableList()))
+                        .containsAll(List.of("name", "price")),
+                () -> assertThat(CAR_CLASS.getConstructors()).hasSize(2),
+                () -> assertThat(Arrays.stream(CAR_CLASS.getDeclaredMethods())
+                        .map(Method::getName)
+                        .collect(Collectors.toUnmodifiableList()))
+                        .containsAll(List.of("printView", "testGetName", "testGetPrice"))
         );
     }
 
     @Test
-    @DisplayName("test로 시작하는 메소드 실행")
+    @DisplayName("Reflection을 이용해 test로 시작하는 메소드 실행할 수 있어야 한다")
     void testMethodRun() {
-
-        // get Car instance first
-        Car carFromDefaultConstructor;
-        try {
-            carFromDefaultConstructor = carClass.getDeclaredConstructor().newInstance();
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Car 객체의 메소드 중 `test로 시작하는 메소드를 자동으로 실행`
-        Arrays.stream(carClass.getDeclaredMethods())
+        final List<String> testMethodsInvokeResults = Arrays.stream(CAR_CLASS.getDeclaredMethods())
                 .filter(method -> method.getName().startsWith("test"))
-                .forEach(method -> {
-                    try {
-                        logger.debug((String) method.invoke(carFromDefaultConstructor));
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                .map(method -> (String) invokeCarMethod(method))
+                .collect(Collectors.toUnmodifiableList());
+
+        assertThat(testMethodsInvokeResults).containsAll(List.of("test : " + CAR_NAME, "test : " + CAR_PRICE));
     }
 
+
     @Test
-    @DisplayName("@PrintView 애노테이션 메소드 실행")
+    @DisplayName("Reflection을 이용해 @PrintView 애노테이션 메소드를 실행할 수 있어야 한다")
     void testAnnotationMethodRun() {
+        // @PrintView 메서드 output 캡처를 위해 일시적으로 System.out을 변경
+        PrintStream originOut = System.out;
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
 
-        // get Car instance first
-        Car carFromDefaultConstructor;
-        try {
-            carFromDefaultConstructor = carClass.getDeclaredConstructor().newInstance();
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
-
-        // `@PrintView` 애노테이션으로 설정되어 있는 메소드만 Java Reflection을 활용해 실행하도록 구현.
-        Arrays.stream(carClass.getDeclaredMethods())
+        Arrays.stream(CAR_CLASS.getDeclaredMethods())
                 .filter(method -> method.isAnnotationPresent(PrintView.class))
-                .forEach(method -> {
-                    try {
-                        method.invoke(carFromDefaultConstructor);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                .forEach(this::invokeCarMethod);
+        // restore origin System.out
+        System.setOut(originOut);
+
+        assertThat(outContent.toString().trim()).isEqualTo("자동차 정보를 출력 합니다.");
     }
 
     @Test
-    @DisplayName("private field에 값 할당")
-    void privateFieldAccess() {
+    @DisplayName("Reflection을 이용해 private field에 값 할당할 수 있어야 한다")
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+
+        // given
+        Field nameField = CAR_CLASS.getDeclaredField("name");
+        Field priceField = CAR_CLASS.getDeclaredField("price");
+        nameField.setAccessible(true);
+        priceField.setAccessible(true);
+
+        // when
+        nameField.set(car, "테슬라");
+        priceField.set(car, 1);
+
+        // then
+        assertThat(car.testGetName()).isEqualTo("test : 테슬라");
+        assertThat(car.testGetPrice()).isEqualTo("test : 1");
     }
 }
