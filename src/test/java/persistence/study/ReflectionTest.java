64b8657d2d56fcf764bca838860d1df0cf59172c
차다희 Dahee Joy Cha <daheecha0612@gmail.com>
2024-02-20 11:10:38 +0900

    Step1(Reflection) 과제 제출합니다. (#170)
    * (docs) readme 추가하라
    
    * (feature) Car 클래스, PrintView 애노테이션 추가하라
    
    * (test) 클래스 이름을 출력하는 테스트를 추가하라 (요구사항1)
    
    * (test) test로 시작하는 메서드를 실행하라(요구사항2)
    
    * (test) @PrintView 애노테이션 메소드를 실행하라(요구사항3)
    
    * (test) private field에 값 할당하라
    (요구사항4)
    
    * (test) 인자를 가진 생성자의 인스턴스 생성
    (요구사항5)
    
    ---------
    
    Co-authored-by: daheecha0612 <dahee.cha@gmail.com>

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,73 @@
+package persistence.study;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.logging.LoggingPermission;
+
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보를 출력하라")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드를 실행하라")
+    void testMethodRun() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Class<Car> carClass = Car.class;
+
+        for (Method declaredMethod : carClass.getDeclaredMethods()) {
+            if (declaredMethod.getName().startsWith("test")){
+                declaredMethod.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션이 붙은 메소드를 실행하라")
+    void testAnnotationMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Car> carClass = Car.class;
+
+        for (Method declaredMethod : carClass.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(PrintView.class)){
+                declaredMethod.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("private field에 값을 할당하라")
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Car car = new Car();
+
+        Field name = car.getClass().getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(car, "기아자동차");
+
+        Field price = car.getClass().getDeclaredField("price");
+        price.setAccessible(true);
+        price.set(car, 1000);
+
+        logger.debug(car.getName());
+        logger.debug(String.valueOf(car.getPrice()));
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성하라")
+    void constructorWithArgs() throws NoSuchFieldException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        Car.class.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 10000);
+    }
+}
