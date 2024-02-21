39ba13e666e8efe78563e193c7c3b4d38e481896
신성수 <tlstjdtn321@naver.com>
2024-02-21 15:18:44 +0900

    fix: 변환된 클래스 실제로 사용하는 로직 수정

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -5,10 +5,13 @@ import database.H2;
 import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import persistence.entity.Person1;
+import persistence.entity.LegacyPerson;
 import persistence.sql.ddl.StandardDialectResolver;
 import persistence.sql.ddl.dialect.Dialect;
 import persistence.sql.ddl.dialect.DialectResolutionInfo;
+import persistence.sql.ddl.query.EntityMappingTable;
+import persistence.sql.ddl.query.builder.CreateQueryBuilder;
+import persistence.sql.ddl.query.builder.DropQueryBuilder;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -21,11 +24,14 @@ public class Application {
 
             final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
 
+            // DB 정보
             DialectResolutionInfo dialectResolutionInfo = new DialectResolutionInfo(server.getConnection().getMetaData());
+            // 방언 설정
             Dialect dialect = StandardDialectResolver.resolveDialect(dialectResolutionInfo);
 
-            String createTableSql = dialect.createTable(Person1.class);
-            String dropTableSql = dialect.dropTable(Person1.class);
+            final EntityMappingTable entityMappingTable = EntityMappingTable.from(LegacyPerson.class);
+            String createTableSql = CreateQueryBuilder.of(entityMappingTable, dialect.getTypeMapper(), dialect.getConstantTypeMapper()).toSql();
+            String dropTableSql = new DropQueryBuilder(entityMappingTable).toSql();
 
             logger.debug(createTableSql);
             logger.debug(dropTableSql);
