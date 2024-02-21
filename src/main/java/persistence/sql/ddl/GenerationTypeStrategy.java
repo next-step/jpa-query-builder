0669fb508fc608199bcce3514155c2196af407f4
jurlring <give.yoon@woowahan.com>
2024-02-21 22:30:42 +0900

    feature: GenerationType Strategy 별로 ddl 반환하는 enum 개발

---
+++ src/main/java/persistence/sql/ddl/GenerationTypeStrategy.java
@@ -0,0 +1,35 @@
+package persistence.sql.ddl;
+
+import jakarta.persistence.GeneratedValue;
+
+import java.util.Arrays;
+
+public enum GenerationTypeStrategy {
+
+    TABLE(""),
+    SEQUENCE("seq.NEXTVAL"),
+    IDENTITY("AUTO_INCREMENT"),
+    UUID(""),
+    AUTO("AUTO_INCREMENT");
+
+    private final String mySqlStrategyDDL;
+
+    GenerationTypeStrategy(String mySqlStrategyDDL) {
+        this.mySqlStrategyDDL = mySqlStrategyDDL;
+    }
+
+    public static GenerationTypeStrategy from(GeneratedValue generatedValue) {
+        if (generatedValue == null) {
+            return null;
+        }
+
+        return Arrays.stream(values())
+                .filter(generationTypeStrategy -> generationTypeStrategy.name().equals(generatedValue.strategy().name()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public String getMySqlStrategyDDL() {
+        return mySqlStrategyDDL;
+    }
+}
---
+++ src/test/java/persistence/sql/ddl/GenerationTypeStrategyTest.java
@@ -0,0 +1,20 @@
+package persistence.sql.ddl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class GenerationTypeStrategyTest {
+
+    @Test
+    @DisplayName("GenerationTypeStrategy 을 찾을 때 generationValue 가 null 인 경우 null 을 반환한다.")
+    void fromByNull() {
+        // given & when
+        GenerationTypeStrategy result = GenerationTypeStrategy.from(null);
+
+        // then
+        assertThat(result).isNull();
+    }
+
+}
