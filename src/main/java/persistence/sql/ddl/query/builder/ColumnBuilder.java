832b0154d114d521ce91328a2cfb12146bd77d4c
신성수 <tlstjdtn321@naver.com>
2024-02-21 15:15:27 +0900

    fix: 디비별 제약조건을 받도록 수정

--- src/main/java/persistence/sql/ddl/query/builder/ColumnBuilder.java
+++ src/main/java/persistence/sql/ddl/query/builder/ColumnBuilder.java
@@ -4,8 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import persistence.sql.ddl.dialect.database.ConstraintsMapper;
 import persistence.sql.ddl.dialect.database.TypeMapper;
-import persistence.sql.ddl.query.model.ConstantType;
+import persistence.sql.ddl.query.model.Constraints;
 import persistence.sql.ddl.query.model.DomainType;
 
 public class ColumnBuilder {
@@ -15,11 +16,14 @@ public class ColumnBuilder {
 
     private final DomainType domainType;
     private final TypeMapper typeMapper;
+    private final ConstraintsMapper constantTypeMapper;
 
     public ColumnBuilder(final DomainType domainType,
-                         final TypeMapper typeMapper) {
+                         final TypeMapper typeMapper,
+                         final ConstraintsMapper constantTypeMapper) {
         this.domainType = domainType;
         this.typeMapper = typeMapper;
+        this.constantTypeMapper = constantTypeMapper;
     }
 
     public String build() {
@@ -44,25 +48,25 @@ public class ColumnBuilder {
     }
 
     private String getPkConstantType() {
-        if(!domainType.isAnnotation(Id.class)) {
+        if (!domainType.isAnnotation(Id.class)) {
             return EMPTY;
         }
 
-        if(!domainType.isAnnotation(GeneratedValue.class)) {
-            return ConstantType.PRIMARY_KEY.getType();
+        if (!domainType.isAnnotation(GeneratedValue.class)) {
+            return constantTypeMapper.getConstantType(Constraints.PRIMARY_KEY);
         }
 
-        if(domainType.getAnnotation(GeneratedValue.class).strategy() != GenerationType.IDENTITY) {
-            return ConstantType.PRIMARY_KEY.getType();
+        if (domainType.getAnnotation(GeneratedValue.class).strategy() != GenerationType.IDENTITY) {
+            return constantTypeMapper.getConstantType(Constraints.PRIMARY_KEY);
         }
 
-        return ConstantType.PK.getType();
+        return constantTypeMapper.getConstantType(Constraints.PK);
     }
 
     private String getConstantType() {
-        if(domainType.isAnnotation(Column.class)) {
+        if (domainType.isAnnotation(Column.class)) {
             Column column = domainType.getAnnotation(Column.class);
-            return column.nullable() ? EMPTY : ConstantType.NOT_NULL.getType();
+            return column.nullable() ? EMPTY : constantTypeMapper.getConstantType(Constraints.NOT_NULL);
         }
 
         return EMPTY;
