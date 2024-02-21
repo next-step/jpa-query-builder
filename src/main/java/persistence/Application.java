9edcb16a734de258a76c33cbd18f8403862b66f4
juhyun <zzang9ha@naver.com>
2024-02-22 00:30:10 +0900

    refactor: Application 실행 코드 추가

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -6,10 +6,14 @@ import domain.Person;
 import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import persistence.sql.ddl.CreateDDLQueryBuilder;
-import persistence.sql.ddl.DDLQueryBuilder;
-import persistence.sql.ddl.H2TypeConverter;
-import persistence.sql.ddl.TypeConverter;
+import persistence.sql.ddl.model.Column;
+import persistence.sql.ddl.mapping.DDLQueryBuilder;
+import persistence.sql.ddl.mapping.H2PrimaryKeyGenerationType;
+import persistence.sql.ddl.mapping.PrimaryKeyGenerationType;
+import persistence.sql.ddl.mapping.QueryBuilder;
+import persistence.sql.ddl.converter.H2TypeConverter;
+import persistence.sql.ddl.model.Table;
+import persistence.sql.ddl.converter.TypeConverter;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -21,12 +25,18 @@ public class Application {
             server.start();
 
             final TypeConverter typeConverter = new H2TypeConverter();
-            final DDLQueryBuilder queryBuilder = new CreateDDLQueryBuilder(typeConverter);
-            final String createQuery = queryBuilder.query(Person.class);
+            final PrimaryKeyGenerationType generationType = new H2PrimaryKeyGenerationType();
+            final QueryBuilder queryBuilder = new DDLQueryBuilder(
+                    new Table(),
+                    new Column(typeConverter, generationType)
+            );
+            final String createQuery = queryBuilder.create(Person.class);
+            final String dropQuery = queryBuilder.drop(Person.class);
 
             final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
 
             jdbcTemplate.execute(createQuery);
+            jdbcTemplate.execute(dropQuery);
 
             server.stop();
         } catch (Exception e) {
