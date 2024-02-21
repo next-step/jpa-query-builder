24c6c80351f642ff1dff9b7059f760692e2af2be
Seol Park <108073530+ParkSeolDev@users.noreply.github.com>
2024-02-21 12:06:28 +0900

    1단계 - Reflection (#178)
    * feat : Car class 생성
    
    * test : 요구사항 1 - 클래스 정보 출력
    
    * test : 요구사항 2 - test로 시작하는 메소드 실행
    
    * test : 요구사항 3 - @PrintView 애노테이션 메소드 실행
    
    * test : 요구사항 3 - @PrintView 애노테이션 메소드 실행
    
    * test : 요구사항 4 - private field에 값 할당
    
    * test : 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,109 @@
+package persistence.study;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+	private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+	@Test
+	@DisplayName("Car 객체 정보 가져오기")
+	void showCarClass() {
+		Class<Car> carClass = Car.class;
+		logger.debug(carClass.getName());
+
+		Field[] fields = carClass.getDeclaredFields();
+		System.out.println("필드:");
+		for (Field field : fields) {
+			System.out.println(field);
+		}
+
+		Constructor<?>[] constructors = carClass.getDeclaredConstructors();
+		System.out.println("생성자:");
+		for (Constructor<?> constructor : constructors) {
+			System.out.println(constructor);
+		}
+
+		Method[] methods = carClass.getDeclaredMethods();
+		System.out.println("메서드");
+		for (Method method : methods) {
+			System.out.println(method);
+		}
+	}
+
+	@Test
+	void test_로_시작하는_메소드_실행() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<Car> carClass = Car.class;
+		Car car = carClass.getConstructor().newInstance();
+		Arrays.stream(carClass.getDeclaredMethods())
+			.filter(x -> x.getName().contains("test"))
+			.forEach(x -> {
+				try {
+					x.invoke(car);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			});
+	}
+
+	@Test
+	void PrintView_어노테이션_메소드_실행() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<Car> carClass = Car.class;
+		Car car = carClass.getConstructor().newInstance();
+		Arrays.stream(carClass.getDeclaredMethods())
+			.filter( x -> x.isAnnotationPresent(PrintView.class))
+			.forEach(x -> {
+				try {
+					x.invoke(car);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			});
+	}
+
+	@Test
+	public void privateField_값_할당() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+		Class<Car> clazz = Car.class;
+		logger.debug(clazz.getName());
+
+		String name = "소나타";
+		int price = 1000;
+
+		Car car = clazz.getConstructor().newInstance();
+		Field nameField = clazz.getDeclaredField("name");
+		nameField.setAccessible(true);
+		nameField.set(car, name);
+
+		Field priceField = clazz.getDeclaredField("price");
+		priceField.setAccessible(true);
+		priceField.set(car, price);
+
+		assertThat(car.testGetName()).isEqualTo("test : " + name);
+		assertThat(car.testGetPrice()).isEqualTo("test : " + price);
+	}
+
+	@Test
+	void 인자를_가진_생성자의_인스턴스_생성() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<Car> clazz = Car.class;
+		String name = "모닝";
+		int price = 2000;
+
+		Constructor<?> constructor = Arrays.stream(clazz.getDeclaredConstructors())
+			.filter(x -> x.getParameterCount() > 0)
+			.findFirst().orElseThrow();
+		Car car = (Car) constructor.newInstance(name, price);
+
+		assertThat(car.testGetName()).isEqualTo("test : " + name);
+		assertThat(car.testGetPrice()).isEqualTo("test : " + price);
+	}
+}
