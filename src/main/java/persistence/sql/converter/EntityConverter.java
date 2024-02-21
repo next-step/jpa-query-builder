e772aee637d66ff5000413937c882284925675fa
SeokHo-Ham <forkyy.dev@gmail.com>
2024-02-22 01:09:09 +0900

    feat: EntityConvert 클래스 작성 및 테스트 통과

---
+++ src/main/java/persistence/sql/converter/EntityConverter.java
@@ -0,0 +1,54 @@
+package persistence.sql.converter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import persistence.sql.dialect.Dialect;
+import persistence.sql.model.Column;
+import persistence.sql.model.NotEntityException;
+import persistence.sql.model.Table;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EntityConverter {
+
+    private final Dialect dialect;
+
+    public EntityConverter(Dialect dialect) {
+        this.dialect = dialect;
+    }
+
+    public Table convertEntityToTable(Class<?> clazz) {
+
+        if (!clazz.isAnnotationPresent(Entity.class)) {
+            throw new NotEntityException();
+        }
+
+        String tableName = clazz.getSimpleName();
+        Field[] fields = clazz.getDeclaredFields();
+        List<Column> columns = convertFieldsToColumn(fields);
+
+        return Table.create(tableName, columns);
+    }
+
+
+    private List<Column> convertFieldsToColumn(Field[] fields) {
+        return Arrays.stream(fields)
+            .filter(field -> !field.isAnnotationPresent(Transient.class))
+            .map(this::convertFieldToColumn)
+            .collect(Collectors.toList());
+    }
+
+    private Column convertFieldToColumn(Field field) {
+
+        if (field.isAnnotationPresent(Id.class)) {
+            return Column.create(field.getName(), dialect.convertDataType(field.getType()), false, true);
+        }
+
+        return Column.create(field.getName(), dialect.convertDataType(field.getType()), true, false);
+    }
+
+}
---
+++ src/test/java/persistence/sql/converter/EntityConverterTest.java
@@ -0,0 +1,64 @@
+package persistence.sql.converter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.sql.dialect.H2Dialect;
+import persistence.sql.entity.Person;
+import persistence.sql.model.Column;
+import persistence.sql.converter.EntityConverter;
+import persistence.sql.model.NotEntityException;
+import persistence.sql.model.Table;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("[Unit] Entity Converter Test")
+class EntityConverterTest {
+
+    private final EntityConverter entityConverter = new EntityConverter(new H2Dialect());
+
+    @DisplayName("Entity 어노테이션이 적용된 클래스가 전달될 경우 해당 클래스명을 name으로 가진 Table 타입의 객체를 반환한다.")
+    @Test
+    void convert_success() {
+
+        Table table = entityConverter.convertEntityToTable(Person.class);
+
+        assertThat(table.getName()).isEqualTo("PERSON");
+    }
+
+    @DisplayName("Entity 어노테이션이 적용된 클래스가 전달될 경우 해당 클래스의 필드를 column으로 가진 Table 타입의 객체를 반환한다.")
+    @Test
+    void convert_success_check_column() {
+
+        List<String> declaredFieldNames = Arrays.stream(Person.class.getDeclaredFields())
+            .map(Field::getName)
+            .collect(Collectors.toList());
+
+        Table table = entityConverter.convertEntityToTable(Person.class);
+        List<Column> columns = table.getColumns();
+
+        assertAll(() -> {
+            assertThat(columns.size()).isEqualTo(declaredFieldNames.size());
+            columns.forEach((column) -> {
+                assertThat(declaredFieldNames.contains(column.getName())).isTrue();
+            });
+        });
+    }
+
+    @DisplayName("Entity 어노테이션이 적용되지 않은 클래스가 전달될 경우 예외를 반환한다.")
+    @Test
+    void fail_convert_by_parameter_is_not_entity() {
+        assertThatThrownBy(() -> entityConverter.convertEntityToTable(String.class))
+            .isInstanceOf(NotEntityException.class)
+            .hasMessage("해당 클래스는 Entity가 아닙니다.");
+
+    }
+
+
+}
