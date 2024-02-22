e37f4f2c9430b22e1113161dd91946d319501ee3
SeokHo-Ham <forkyy.dev@gmail.com>
2024-02-22 12:40:26 +0900

    feat: 요구사항2에 맞게 Person 클래스 수정

--- src/main/java/persistence/sql/entity/Person.java
+++ src/main/java/persistence/sql/entity/Person.java
@@ -1,16 +1,21 @@
 package persistence.sql.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nick_name")
     private String name;
 
+    @Column(name = "old")
     private Integer age;
+
+    @Column(nullable = false)
+    private String email;
 
 }
