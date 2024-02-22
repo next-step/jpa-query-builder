8ca3c545d6d52156baca0411c70534bb6585ac43
SeokHo-Ham <forkyy.dev@gmail.com>
2024-02-22 16:12:42 +0900

    test: drop query 관련 테스트 작성 및 통과

--- src/test/java/persistence/sql/database/IntegrationTest.java
+++ src/test/java/persistence/sql/database/IntegrationTest.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import persistence.sql.converter.EntityConverter;
+import persistence.sql.converter.TypeMapper;
 import persistence.sql.ddl.QueryBuilder;
 import persistence.sql.dialect.H2Dialect;
 import persistence.sql.entity.Person;
+import persistence.sql.model.Table;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,6 +25,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("[Integration] Execute Query Integration Test")
 class IntegrationTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
     DatabaseServer databaseServer;
 
     @BeforeEach
@@ -41,19 +48,59 @@ class IntegrationTest {
         Connection connection = databaseServer.getConnection();
         final JdbcTemplate jdbcTemplate = new JdbcTemplate(connection);
 
-        String sql = queryBuilder.buildCreateQuery(Person.class);
+        EntityConverter entityConverter = new EntityConverter(new TypeMapper());
+        Table personTable = entityConverter.convertEntityToTable(Person.class);
+
+        String sql = queryBuilder.buildCreateQuery(personTable);
         jdbcTemplate.execute(sql);
+
+        String tableName = Person.class.isAnnotationPresent(jakarta.persistence.Table.class) ? Person.class.getDeclaredAnnotation(jakarta.persistence.Table.class).name() : "PERSON";
 
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) "
             + "FROM information_schema.tables "
-            + "WHERE table_name = ?"
+            + "WHERE table_name = ? and TABLE_SCHEMA = ? "
             + "LIMIT 1;");
-        preparedStatement.setString(1, "PERSON");
+        preparedStatement.setString(1, tableName.toUpperCase());
+        preparedStatement.setString(2, "PUBLIC");
 
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
 
         assertThat(resultSet.getInt(1)).isNotZero();
     }
+
+
+    @DisplayName("@Entity 어노테이션이 적용된 클래스를 바탕으로 DB에 테이블을 제거한다.")
+    @Test
+    void drop_table_success() throws SQLException {
+        QueryBuilder queryBuilder = new QueryBuilder(new H2Dialect());
+
+        Connection connection = databaseServer.getConnection();
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(connection);
+
+        EntityConverter entityConverter = new EntityConverter(new TypeMapper());
+        Table personTable = entityConverter.convertEntityToTable(Person.class);
+
+        String createQuery = queryBuilder.buildCreateQuery(personTable);
+        jdbcTemplate.execute(createQuery);
+
+        String dropQuery = queryBuilder.buildDropQuery(personTable);
+        jdbcTemplate.execute(dropQuery);
+
+        String tableName = Person.class.isAnnotationPresent(jakarta.persistence.Table.class) ? Person.class.getDeclaredAnnotation(jakarta.persistence.Table.class).name() : "PERSON";
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) "
+            + "FROM information_schema.TABLES "
+            + "WHERE TABLE_NAME = ?  and TABLE_SCHEMA = ? "
+            + "LIMIT 1;");
+        preparedStatement.setString(1, tableName.toUpperCase());
+        preparedStatement.setString(2, "PUBLIC");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        assertThat(resultSet.getInt(1)).isZero();
+    }
+
 
 }
--- src/test/java/persistence/sql/ddl/QueryBuilderTest.java
+++ src/test/java/persistence/sql/ddl/QueryBuilderTest.java
@@ -2,9 +2,12 @@ package persistence.sql.ddl;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import persistence.sql.converter.EntityConverter;
+import persistence.sql.converter.TypeMapper;
 import persistence.sql.dialect.H2Dialect;
 import persistence.sql.entity.Person;
 import persistence.sql.model.NotEntityException;
+import persistence.sql.model.Table;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,24 +15,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("[Unit] Query Builder Test")
 class QueryBuilderTest {
 
+    private final EntityConverter entityConverter = new EntityConverter(new TypeMapper());
     private final QueryBuilder queryBuilder = new QueryBuilder(new H2Dialect());
 
-    @DisplayName("@Entity 어노테이션이 추가된 클래스 타입을 전달할 경우 해당 테이블을 생성하는 쿼리를 반환한다.")
+    @DisplayName("Table 타입의 인스턴스를 전달하면 해당 인스터슨의 정보를 바탕으로 테이블을 생성하는 쿼리를 반환한다.")
     @Test
     void build_create_query_success() {
-        String query = queryBuilder.buildCreateQuery(Person.class);
+        Table table = entityConverter.convertEntityToTable(Person.class);
 
-        assertThat(query.startsWith("CREATE TABLE")).isTrue();
+        String query = queryBuilder.buildCreateQuery(table);
+
+        assertThat(query.startsWith("CREATE TABLE " + table.getName())).isTrue();
     }
 
-    @DisplayName("@Entity 어노테이션이 적용되지 않은 클래스 타입을 전달할 경우 예외를 반환한다.")
+    @DisplayName("Table 타입의 인스턴스를 전달하면 해당 인스터슨의 정보를 바탕으로 테이블을 제거하는 쿼리를 반환한다.")
     @Test
-    void build_create_query_fail_by_not_entity() {
+    void build_drop_query_success() {
+        Table table = entityConverter.convertEntityToTable(Person.class);
 
-        assertThatThrownBy(() -> queryBuilder.buildCreateQuery(String.class))
-            .isInstanceOf(NotEntityException.class)
-            .hasMessage("해당 클래스는 Entity가 아닙니다.");
+        String query = queryBuilder.buildDropQuery(table);
 
+        assertThat(query.equals("DROP TABLE " + table.getName())).isTrue();
     }
 
 }
