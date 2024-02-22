6a58b425dc775ec5ad3ac8793c00a06e218193a6
daheecha0612 <dahee.cha@gmail.com>
2024-02-22 18:31:30 +0900

    (adjust) 한 메서드에 한번의 들여쓰기만 한다. assertion을 사용한다

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -14,10 +16,12 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.LoggingPermission;
+import java.util.stream.Stream;
 
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @Test
     @DisplayName("Car 객체 정보를 출력하라")
@@ -29,24 +33,41 @@ public class ReflectionTest {
     @Test
     @DisplayName("test로 시작하는 메소드를 실행하라")
     void testMethodRun() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        // given
         Class<Car> carClass = Car.class;
+        List<String> expected = Stream.of("test : 기아자동차", "test : 1000").sorted().toList();
 
-        for (Method declaredMethod : carClass.getDeclaredMethods()) {
-            if (declaredMethod.getName().startsWith("test")){
-                declaredMethod.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
-            }
+        // when
+        List<Method> methodsToExecute = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+
+        List<String> resultMessages = new java.util.ArrayList<>(List.of());
+        for (Method method : methodsToExecute) {
+            String result = (String) method.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
+            resultMessages.add(result);
         }
+
+        // then
+        Assertions.assertIterableEquals(resultMessages.stream().sorted().toList(), expected);
+
     }
 
     @Test
     @DisplayName("@PrintView 애노테이션이 붙은 메소드를 실행하라")
     void testAnnotationMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        // given
         Class<Car> carClass = Car.class;
+        System.setOut(new PrintStream(outputStreamCaptor));
 
-        for (Method declaredMethod : carClass.getDeclaredMethods()) {
-            if (declaredMethod.isAnnotationPresent(PrintView.class)){
-                declaredMethod.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
-            }
+        // when
+        List<Method> methodsToExecute = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .toList();
+
+        for (Method method : methodsToExecute) {
+            method.invoke(carClass.getDeclaredConstructor(String.class, int.class).newInstance("기아자동차", 1000));
+            Assertions.assertEquals("자동차 정보를 출력합니다.", outputStreamCaptor.toString().trim());
         }
     }
 
