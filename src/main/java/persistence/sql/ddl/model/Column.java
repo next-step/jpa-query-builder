8a53b293a25103494746b6ae99177a3cc07c1cce
juhyun <zzang9ha@naver.com>
2024-02-24 01:17:08 +0900

    refactor: 네이밍 변경

--- src/main/java/persistence/sql/ddl/model/Column.java
+++ src/main/java/persistence/sql/ddl/model/Column.java
@@ -2,8 +2,8 @@ package persistence.sql.ddl.model;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
-import persistence.sql.ddl.mapping.PrimaryKeyGenerationType;
 import persistence.sql.ddl.converter.TypeConverter;
+import persistence.sql.ddl.mapping.PrimaryKeyGenerationType;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -17,8 +17,7 @@ public class Column {
     private static final String SPACE = " ";
     private static final String PK_QUERY = " NOT NULL PRIMARY KEY";
     private static final String NOT_NULL = " NOT NULL";
-    private static final String NOTHING = "";
-
+    private static final String EMPTY = "";
     private final TypeConverter converter;
     private final PrimaryKeyGenerationType generationType;
 
@@ -72,13 +71,13 @@ public class Column {
     private String properties(Field field) {
         final jakarta.persistence.Column columnAnnotation = field.getAnnotation(jakarta.persistence.Column.class);
 
-        if (existColumnNullableProperty(columnAnnotation)) {
+        if (isTrueColumnNullableProperty(columnAnnotation)) {
             return NOT_NULL;
         }
-        return NOTHING;
+        return EMPTY;
     }
 
-    private static boolean existColumnNullableProperty(jakarta.persistence.Column columnAnnotation) {
+    private static boolean isTrueColumnNullableProperty(jakarta.persistence.Column columnAnnotation) {
         return columnAnnotation != null && !columnAnnotation.nullable();
     }
 
@@ -86,7 +85,7 @@ public class Column {
         if (hasPrimaryKeyAnnotation(field)) {
             return PK_QUERY;
         }
-        return NOTHING;
+        return EMPTY;
     }
 
     private static boolean hasPrimaryKeyAnnotation(Field field) {
