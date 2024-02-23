56fe24b278af42303bc716d04f6e45c618035186
ckyeon <qsky8520@gmail.com>
2024-02-23 22:26:22 +0900

    refactor(H2DialectTest): 중복되는 코드 제거

--- src/test/java/persistence/sql/dialect/H2DialectTest.java
+++ src/test/java/persistence/sql/dialect/H2DialectTest.java
@@ -2,26 +2,34 @@ package persistence.sql.dialect;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import persistence.sql.model.SqlType;
 
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class H2DialectTest {
 
     private final Dialect dialect = new H2Dialect();
 
-    @Test
     @DisplayName("H2 방언 확인하기")
-    void getType() {
-        String varchar = dialect.getType(SqlType.VARCHAR);
-        String integer = dialect.getType(SqlType.INTEGER);
-        String bigint = dialect.getType(SqlType.BIGINT);
+    @ParameterizedTest
+    @MethodSource
+    void getType(SqlType type, String jdbcType) {
+        String result = dialect.getType(type);
 
-        assertSoftly(softly -> {
-           softly.assertThat(varchar).isEqualTo("varchar");
-           softly.assertThat(integer).isEqualTo("integer");
-           softly.assertThat(bigint).isEqualTo("bigint");
-        });
+        assertThat(result).isEqualTo(jdbcType);
+    }
+
+    private static Stream<Arguments> getType() {
+        return Stream.of(
+                Arguments.arguments(SqlType.VARCHAR, "varchar"),
+                Arguments.arguments(SqlType.VARCHAR, "varchar"),
+                Arguments.arguments(SqlType.VARCHAR, "varchar")
+        );
     }
 
     @Test
