e075eb59d4f7433b64adfe29c23677946dda197a
jinwoo.oh <jinwoo.oh@yanolja.com>
2024-02-24 23:07:28 +0900

    refect : class명 적절하게 변경

--- src/main/java/jdbc/ResultSetColumnReader.java
+++ src/main/java/jdbc/ResultSetColumnReader.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
-public enum FieldType {
+public enum ResultSetColumnReader {
     STRING(String.class, (rs, columnName) -> {
         try {
             return rs.getString(columnName);
@@ -33,22 +33,22 @@ public enum FieldType {
     private final Class<?> clazz;
     private final BiFunction<ResultSet, String, ?> mapper;
 
-    private static final Map<Class<?>, FieldType> TYPE_MAP =
-        Arrays.stream(FieldType.values())
+    private static final Map<Class<?>, ResultSetColumnReader> TYPE_MAP =
+        Arrays.stream(ResultSetColumnReader.values())
             .collect(Collectors.toMap(type -> type.clazz, type -> type));
 
 
-    FieldType(Class<?> clazz, BiFunction<ResultSet, String, ?> mapper) {
+    ResultSetColumnReader(Class<?> clazz, BiFunction<ResultSet, String, ?> mapper) {
         this.clazz = clazz;
         this.mapper = mapper;
     }
 
     public static Object map(ResultSet resultSet, String columnName, Class<?> clazz) {
-        FieldType fieldType = TYPE_MAP.get(clazz);
-        if (fieldType == null) {
+        ResultSetColumnReader reader = TYPE_MAP.get(clazz);
+        if (reader == null) {
             throw new IllegalArgumentException("존재 하지 않는 타입의 필드 입니다.");
         }
-        return fieldType.map(resultSet, columnName);
+        return reader.map(resultSet, columnName);
     }
 
     public Object map(ResultSet resultSet, String columnName) {
