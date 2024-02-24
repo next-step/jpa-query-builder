d600b1177c7105b6c5e43b6cac7286226b641571
Shin seongsu <tlstjdtn321@naver.com>
2024-02-25 00:29:17 +0900

    refactor: 패키지 위치 리팩토링

--- src/test/java/persistence/sql/dialect/database/DatabaseTest.java
+++ src/test/java/persistence/sql/dialect/database/DatabaseTest.java
@@ -1,4 +1,4 @@
-package persistence.sql.ddl.dialect.database;
+package persistence.sql.dialect.database;
 
 import database.DatabaseServer;
 import database.H2;
--- src/test/java/persistence/sql/dialect/database/TypeMapperTest.java
+++ src/test/java/persistence/sql/dialect/database/TypeMapperTest.java
@@ -1,4 +1,4 @@
-package persistence.sql.ddl.dialect.database;
+package persistence.sql.dialect.database;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
