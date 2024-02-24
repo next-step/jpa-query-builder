da0758179ac31249b3d3935f16340242eb3e911c
joydaheecha <daheecha0612@gmail.com>
2024-02-24 16:01:52 +0900

    (refactor) 줄 바꿈하라

--- src/test/java/persistence/sql/ddl/QueryBuilderTest.java
+++ src/test/java/persistence/sql/ddl/QueryBuilderTest.java
@@ -51,7 +51,8 @@ class QueryBuilderTest {
     @DisplayName("[요구사항 1] @Column 애노테이션이 없는 Person 엔티티를 이용하여 create 쿼리 만든다.")
     void 요구사항1_test() throws SQLException {
         //given
-        String expectedQuery = "CREATE TABLE IF NOT EXISTS Person (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(30) NULL,age INT)";
+        String expectedQuery = "CREATE TABLE IF NOT EXISTS Person " +
+                "(id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(30) NULL,age INT)";
         // when
         String actualQuery = new QueryBuilder(persistence.entity.basic.Person.class).getCreateQuery();
 
@@ -63,7 +64,8 @@ class QueryBuilderTest {
     @DisplayName("[요구사항 2] @Column 애노테이션이 있는 Person 엔티티를 이용하여 create 쿼리 만든다.")
     void 요구사항2_test() throws SQLException {
         //given
-        String expectedQuery = "CREATE TABLE IF NOT EXISTS Person (id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
+        String expectedQuery = "CREATE TABLE IF NOT EXISTS Person " +
+                "(id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
 
         // when
         String actualQuery = new QueryBuilder(Person.class).getCreateQueryUsingAnnotation();
@@ -76,7 +78,8 @@ class QueryBuilderTest {
     @DisplayName("[요구사항 3] 3.1 @Table 애노테이션이 붙은 필드의 name을 테이블명으로 가지는 create 쿼리 만들")
     void 요구사항3_1_test() throws SQLException {
         //given
-        String expectedQuery = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
+        String expectedQuery = "CREATE TABLE IF NOT EXISTS users " +
+                "(id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
 
         // when
         String actualQuery = new QueryBuilder(persistence.entity.notcolumn.Person.class).getCreateQueryUsingAnnotation();
@@ -89,7 +92,8 @@ class QueryBuilderTest {
     @DisplayName("[요구사항 3.2] @Transient 애노테이션이 붙은 필드는 제하고 create 쿼리 만든다.")
     void 요구사항3_2_test() throws SQLException {
         //given
-        String expectedQuery = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
+        String expectedQuery = "CREATE TABLE IF NOT EXISTS users " +
+                "(id INT AUTO_INCREMENT PRIMARY KEY,nick_name VARCHAR(30) NULL,old INT,email VARCHAR(30) NOT NULL)";
 
         // when
         String actualQuery = new QueryBuilder(persistence.entity.notcolumn.Person.class).getCreateQueryUsingAnnotation();
