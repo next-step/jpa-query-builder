fb93c193820d8f0e46a62826b44aa7ecd83797ca
Shin seongsu <tlstjdtn321@naver.com>
2024-02-25 03:18:54 +0900

    feat: 조회문 만드는 클래스추가

--- src/main/java/persistence/sql/dml/query/builder/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/query/builder/SelectQueryBuilder.java
@@ -1,28 +1,45 @@
 package persistence.sql.dml.query.builder;
 
 import persistence.sql.dml.query.clause.ColumnClause;
+import persistence.sql.dml.query.clause.WhereClause;
 import persistence.sql.entity.EntityMappingTable;
+import persistence.sql.entity.model.DomainType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelectQueryBuilder {
-    private static final String FORMAT = "SELECT %s FROM %s";
+    private static final String FORMAT = "SELECT %s FROM %s %s";
 
     private final String tableName;
     private final ColumnClause columnClause;
+    private final WhereClause whereClause;
 
     private SelectQueryBuilder(final String tableName,
-                               final ColumnClause columnClause) {
+                               final ColumnClause columnClause,
+                               final WhereClause whereClause) {
         this.tableName = tableName;
         this.columnClause = columnClause;
+        this.whereClause = whereClause;
     }
 
     public String toSql() {
-        return String.format(FORMAT, columnClause.toSql(), tableName);
+        return String.format(FORMAT, columnClause.toSql(), tableName, whereClause.toSql());
+    }
+
+    public static SelectQueryBuilder of(EntityMappingTable entityMappingTable, Map<DomainType, String> whereClause) {
+        return new SelectQueryBuilder(
+                entityMappingTable.getTableName(),
+                ColumnClause.from(entityMappingTable.getDomainTypes()),
+                new WhereClause(whereClause)
+        );
     }
 
     public static SelectQueryBuilder from(EntityMappingTable entityMappingTable) {
         return new SelectQueryBuilder(
                 entityMappingTable.getTableName(),
-                ColumnClause.from(entityMappingTable.getDomainTypes())
+                ColumnClause.from(entityMappingTable.getDomainTypes()),
+                new WhereClause(new HashMap())
         );
     }
 
