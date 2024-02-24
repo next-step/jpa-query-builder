c488444438777da98831ba7f34d64b87d3a673c3
Shin seongsu <tlstjdtn321@naver.com>
2024-02-25 00:37:01 +0900

    test: Value 만드는 빌더 테스트 추가

---
+++ src/test/java/persistence/sql/dml/query/clause/ValueClauseTest.java
@@ -0,0 +1,55 @@
+package persistence.sql.dml.query.clause;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.entity.Person;
+import persistence.entity.User;
+import persistence.sql.dml.query.exception.IllegalFieldValueException;
+import persistence.sql.dml.query.exception.NotFoundFieldNameException;
+import persistence.sql.entity.EntityMappingTable;
+import persistence.sql.entity.model.DomainTypes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class ValueClauseTest {
+
+    private DomainTypes domainTypes;
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        this.domainTypes = EntityMappingTable.from(Person.class).getDomainTypes();
+        this.person = new Person(1L, "신성수", 20, "tlstjdtn@nextstep.com");
+    }
+
+    @DisplayName("쿼리문에 쓰일 값들을 반환한다.")
+    @Test
+    void valueSql() {
+        ValueClause valueClause = ValueClause.from(person, domainTypes);
+
+        assertThat(valueClause.toSql()).isEqualTo("1,'신성수',20,'tlstjdtn@nextstep.com'");
+    }
+
+    @DisplayName("필드이름을 찾지 못할떄, 에러를 발생시킨다.")
+    @Test
+    void notFoundFieldNameException() {
+        DomainTypes userDomainTypes = EntityMappingTable.from(User.class).getDomainTypes();
+
+        assertThatThrownBy(() ->ValueClause.from(person, userDomainTypes))
+                .isInstanceOf(NotFoundFieldNameException.class)
+                .hasMessage("필드이름을 찾지 못했습니다.");
+    }
+
+    @DisplayName("값에 널값이 있으면 에러를 발생시킨다.")
+    @Test
+    void IllegalFieldValueException() {
+        Person existsNullperson = new Person();
+
+        assertThatThrownBy(() ->ValueClause.from(existsNullperson, domainTypes))
+                .isInstanceOf(IllegalFieldValueException.class)
+                .hasMessage("적절하지 않은 필드 값 입니다.");
+    }
+
+}
