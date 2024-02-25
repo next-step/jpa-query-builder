0719f108518f664b2f0ae9ac8981b1a6f83893e7
juhyun <zzang9ha@naver.com>
2024-02-25 16:56:10 +0900

    refactor: 테이블 생성 쿼리 수정

--- src/test/java/persistence/sql/ddl/model/TableTest.java
+++ src/test/java/persistence/sql/ddl/model/TableTest.java
@@ -10,14 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TableTest {
 
-    private final persistence.sql.ddl.model.Table table = new persistence.sql.ddl.model.Table();
+    private final persistence.sql.Table table = new persistence.sql.Table();
 
     @Test
     @DisplayName("@Table 어노테이션이 존재하지 않으면 소문자의 클래스명을 생성한다.")
     void tableCreateTest_withoutTable() {
-        final var expected = "CREATE TABLE fixture";
+        final var expected = "fixture";
 
-        final var actual = table.create(Fixture.class);
+        final var actual = table.name(Fixture.class);
 
         assertThat(actual).isEqualTo(expected);
     }
@@ -25,9 +25,9 @@ class TableTest {
     @Test
     @DisplayName("@Table 어노테이션이 존재하지만, name property 가 존재하지 않으면 소문자의 클래스명을 생성한다.")
     void tableCreateTest_withoutName() {
-        final var expected = "CREATE TABLE fixturewithoutname";
+        final var expected = "fixturewithoutname";
 
-        final var actual = table.create(FixtureWithoutName.class);
+        final var actual = table.name(FixtureWithoutName.class);
 
         assertThat(actual).isEqualTo(expected);
     }
@@ -35,9 +35,9 @@ class TableTest {
     @Test
     @DisplayName("@Table 어노테이션과 name property 가 존재하면, name 값으로 생성한다.")
     void tableCreateTest_withName() {
-        final var expected = "CREATE TABLE fixtures";
+        final var expected = "fixtures";
 
-        final var actual = table.create(FixtureWithName.class);
+        final var actual = table.name(FixtureWithName.class);
 
         assertThat(actual).isEqualTo(expected);
     }
