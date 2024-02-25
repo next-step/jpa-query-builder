45782db10d2a29ebce2c50754f0311a0f144aaa7
Shin seongsu <tlstjdtn321@naver.com>
2024-02-25 15:30:49 +0900

    refactor: Repository abstract에서 interface로 수정

--- src/main/java/persistence/sql/dml/repository/Repository.java
+++ src/main/java/persistence/sql/dml/repository/Repository.java
@@ -1,89 +1,19 @@
 package persistence.sql.dml.repository;
 
-import jdbc.JdbcTemplate;
-import persistence.sql.dml.exception.*;
-import persistence.sql.entity.EntityMappingTable;
-import persistence.sql.entity.model.DomainType;
-
-import java.lang.reflect.Field;
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class Repository<T> {
+public interface Repository<T> {
 
-    protected final JdbcTemplate jdbcTemplate;
-    protected final Class<T> clazz;
-    protected final EntityMappingTable entityMappingTable;
-    protected final DomainType pkDomainType;
+    List<T> findAll();
 
-    public Repository(final JdbcTemplate jdbcTemplate,
-                      final Class<T> clazz) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.clazz = clazz;
-        this.entityMappingTable = EntityMappingTable.from(clazz);
-        this.pkDomainType = entityMappingTable.getDomainTypeList()
-                .stream()
-                .filter(DomainType::isExistsId)
-                .findFirst()
-                .orElseThrow(NotFoundIdException::new);
-    }
+    Optional<T> findById(Long id);
 
-    abstract List<T> findAll();
+    T save(T t);
 
-    abstract Optional<T> findById(Long id);
+    void deleteAll();
 
-    abstract T save(T t);
-
-    abstract void deleteAll();
-
-    abstract void deleteById(Long id);
-
-
-    public T mapper(ResultSet resultSet) {
-        EntityMappingTable entityMappingTable = EntityMappingTable.from(clazz);
-        T instance = createInstance();
-
-        entityMappingTable.getDomainTypeList()
-                .forEach(domainType -> {
-                    Field field = getField(clazz, domainType.getName());
-                    setField(instance, field, getValue(resultSet, domainType.getColumnName()));
-                });
-
-        return instance;
-    }
-
-    private T createInstance() {
-        try {
-            return clazz.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new InstanceException();
-        }
-    }
-
-    private Field getField(Class<T> clazz, String name) {
-        try {
-            return clazz.getDeclaredField(name);
-        } catch (Exception e) {
-            throw new NotFoundFieldException();
-        }
-    }
-
-    private Object getValue(ResultSet resultSet, String columnName) {
-        try {
-            return resultSet.getObject(columnName);
-        } catch (Exception e) {
-            throw new InvalidFieldValueException();
-        }
-    }
-
-    private void setField(Object instance, Field field, Object value) {
-        try {
-            field.setAccessible(true);
-            field.set(instance, value);
-        } catch (Exception e) {
-            throw new FieldSetValueException();
-        }
-    }
+    void deleteById(Long id);
+    
 
 }
--- src/main/java/persistence/sql/dml/repository/RepositoryImpl.java
+++ src/main/java/persistence/sql/dml/repository/RepositoryImpl.java
@@ -1,51 +1,67 @@
 package persistence.sql.dml.repository;
 
 import jdbc.JdbcTemplate;
+import persistence.sql.dml.exception.NotFoundIdException;
 import persistence.sql.dml.query.builder.DeleteQueryBuilder;
 import persistence.sql.dml.query.builder.InsertQueryBuilder;
 import persistence.sql.dml.query.builder.SelectQueryBuilder;
+import persistence.sql.entity.EntityMappingTable;
 import persistence.sql.entity.model.DomainType;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class RepositoryImpl<T> extends Repository<T> {
+public class RepositoryImpl<T> implements Repository<T> {
 
-    public RepositoryImpl(JdbcTemplate jdbcTemplate, Class<T> clazz) {
-        super(jdbcTemplate, clazz);
+    private final JdbcTemplate jdbcTemplate;
+    private final EntityMappingTable entityMappingTable;
+    private RepositoryMapper<T> repositoryMapper;
+    private final DomainType pkDomainType;
+
+
+    public RepositoryImpl(final JdbcTemplate jdbcTemplate,
+                          final Class<T> clazz) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.entityMappingTable = EntityMappingTable.from(clazz);
+        this.repositoryMapper = new RepositoryMapper<>(clazz);
+        this.pkDomainType = entityMappingTable.getDomainTypeList()
+                .stream()
+                .filter(DomainType::isExistsId)
+                .findFirst()
+                .orElseThrow(NotFoundIdException::new);
     }
 
     @Override
     public List<T> findAll() {
         SelectQueryBuilder selectQueryBuilder = SelectQueryBuilder.from(entityMappingTable);
-        return super.jdbcTemplate.query(selectQueryBuilder.toSql(), this::mapper);
+        return jdbcTemplate.query(selectQueryBuilder.toSql(), repositoryMapper::mapper);
     }
 
     @Override
-    Optional<T> findById(Long id) {
+    public Optional<T> findById(Long id) {
         Map<DomainType, String> where = Map.of(pkDomainType, id.toString());
 
         SelectQueryBuilder selectQueryBuilder = SelectQueryBuilder.of(entityMappingTable, where);
-        return Optional.ofNullable(super.jdbcTemplate.queryForObject(selectQueryBuilder.toSql(), this::mapper));
+        return Optional.ofNullable(jdbcTemplate.queryForObject(selectQueryBuilder.toSql(), repositoryMapper::mapper));
     }
 
     @Override
-    T save(T t) {
+    public T save(T t) {
         InsertQueryBuilder insertQueryBuilder = InsertQueryBuilder.from(t);
         jdbcTemplate.execute(insertQueryBuilder.toSql());
         return t;
     }
 
     @Override
-    void deleteAll() {
+    public void deleteAll() {
         DeleteQueryBuilder deleteQueryBuilder = DeleteQueryBuilder.from(entityMappingTable.getTableName());
 
         jdbcTemplate.execute(deleteQueryBuilder.toSql());
     }
 
     @Override
-    void deleteById(Long id) {
+    public void deleteById(Long id) {
         Map<DomainType, String> where = Map.of(pkDomainType, id.toString());
 
         DeleteQueryBuilder deleteQueryBuilder = DeleteQueryBuilder.of(entityMappingTable.getTableName(), where);
