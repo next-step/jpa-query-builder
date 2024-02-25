6361c84af581d3291d0b26eaee6bf76d011cfdf4
Shin seongsu <tlstjdtn321@naver.com>
2024-02-25 15:54:40 +0900

    test: Repository Mapper 테스트 코드 추가

--- src/test/java/persistence/sql/dml/repository/RepositoryImplTest.java
+++ src/test/java/persistence/sql/dml/repository/RepositoryImplTest.java
@@ -1,62 +1,32 @@
 package persistence.sql.dml.repository;
 
-import database.DatabaseServer;
-import database.H2;
-import jdbc.JdbcTemplate;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.entity.Person;
-import persistence.sql.StandardDialectResolver;
-import persistence.sql.ddl.query.builder.CreateQueryBuilder;
-import persistence.sql.dialect.Dialect;
-import persistence.sql.dialect.DialectResolutionInfo;
-import persistence.sql.entity.EntityMappingTable;
+import persistence.sql.db.H2Database;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class RepositoryImplTest {
+class RepositoryImplTest extends H2Database {
 
-    private static DatabaseServer server;
-    private static JdbcTemplate jdbcTemplate;
-    private static EntityMappingTable entityMappingTable;
+    private Person person1;
+    private Person person2;
 
-
-    private static Person person1;
-    private static Person person2;
-
-    @BeforeAll
-    static void setUpAll() throws SQLException {
-        server = new H2();
-        jdbcTemplate = new JdbcTemplate(server.getConnection());
-
-        person1 = new Person(1L, "박재성", 10, "jason");
-        person2 = new Person(2L, "이동규", 11, "cu");
-
-        createTable();
-    }
 
     @BeforeEach
     void setUp() {
         Repository<Person> personRepository = new RepositoryImpl<>(jdbcTemplate, Person.class);
         personRepository.deleteAll();
 
+        person1 = new Person(1L, "박재성", 10, "jason");
+        person2 = new Person(2L, "이동규", 11, "cu");
+
         personRepository.save(person1);
         personRepository.save(person2);
-    }
-
-    private static void createTable() throws SQLException {
-        DialectResolutionInfo dialectResolutionInfo = new DialectResolutionInfo(server.getConnection().getMetaData());
-        Dialect dialect = StandardDialectResolver.resolveDialect(dialectResolutionInfo);
-        entityMappingTable = EntityMappingTable.from(Person.class);
-        CreateQueryBuilder createQueryBuilder = CreateQueryBuilder.of(entityMappingTable, dialect.getTypeMapper(), dialect.getConstantTypeMapper());
-
-        jdbcTemplate.execute(createQueryBuilder.toSql());
     }
 
     @DisplayName("전체 목록을 반환한다.")
---
+++ src/test/java/persistence/sql/dml/repository/RepositoryMapperTest.java
@@ -0,0 +1,63 @@
+package persistence.sql.dml.repository;
+
+import jdbc.JdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.entity.Person;
+import persistence.sql.db.H2Database;
+import persistence.sql.dml.query.builder.SelectQueryBuilder;
+import persistence.sql.entity.EntityMappingTable;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RepositoryMapperTest extends H2Database {
+    private RepositoryMapper<Person> repositoryMapper;
+    private Connection connection;
+    private SelectQueryBuilder selectQueryBuilder;
+    private Repository<Person> personRepository;
+
+    private Person person1;
+    private Person person2;
+
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        this.personRepository = new RepositoryImpl<>(jdbcTemplate, Person.class);
+        this.repositoryMapper = new RepositoryMapper(Person.class);
+        this.connection = server.getConnection();
+        this.selectQueryBuilder = SelectQueryBuilder.from(entityMappingTable);
+
+        personRepository.deleteAll();
+
+        person1 = new Person(1L, "박재성", 10, "jason");
+
+        personRepository.save(person1);
+    }
+
+
+    @DisplayName("디비에서 조회된 컬럼들이 자동으로 객체에 매핑이 된다.")
+    @Test
+    void mapperTest() throws SQLException {
+        String sql = selectQueryBuilder.toSql();
+
+        Person person = executeQuery(sql);
+
+        assertThat(person).isEqualTo(person1);
+    }
+
+    private Person executeQuery(final String sql) {
+        try (final ResultSet resultSet = connection.prepareStatement(sql).executeQuery()) {
+            resultSet.next();
+            return repositoryMapper.mapper(resultSet);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("SQL 에러가 발생하였습니다.");
+        }
+    }
+
+}
