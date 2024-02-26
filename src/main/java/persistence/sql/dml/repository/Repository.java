5b694347ff8e6c4b8fd1c5da2fa7851372cc23e1
Shin seongsu <tlstjdtn321@naver.com>
2024-02-26 23:39:11 +0900

    feat: 전체 조회, 삭제 추가

--- src/main/java/persistence/sql/dml/repository/Repository.java
+++ src/main/java/persistence/sql/dml/repository/Repository.java
@@ -3,17 +3,17 @@ package persistence.sql.dml.repository;
 import java.util.List;
 import java.util.Optional;
 
-public interface Repository<T> {
+public interface Repository<T, K> {
 
     List<T> findAll();
 
-    Optional<T> findById(Long id);
+    Optional<T> findById(K id);
 
     T save(T t);
 
     void deleteAll();
 
-    void deleteById(Long id);
+    void deleteById(K id);
     
 
 }
