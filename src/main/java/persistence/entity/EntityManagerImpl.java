82c0cf0c7435751de2720efc5c829e5210ed0785
juhyun <zzang9ha@naver.com>
2024-03-03 01:44:37 +0900

    feat: EntityManager find, persist, delete 추가

---
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -0,0 +1,64 @@
+package persistence.entity;
+
+import jdbc.JdbcTemplate;
+import jdbc.RowMapperImpl;
+import persistence.sql.model.Table;
+import persistence.sql.dml.builder.DeleteQueryBuilder;
+import persistence.sql.dml.builder.InsertQueryBuilder;
+import persistence.sql.dml.builder.SelectQueryBuilder;
+import persistence.sql.dml.model.DMLColumn;
+
+public class EntityManagerImpl implements EntityManager {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public EntityManagerImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public <T> T find(Class<T> clazz, Long id) {
+        final String query = findQuery(clazz, id);
+
+        return jdbcTemplate.queryForObject(query, new RowMapperImpl<>(clazz));
+    }
+
+    private <T> String findQuery(Class<T> clazz, Long id) {
+        final SelectQueryBuilder queryBuilder = new SelectQueryBuilder(
+                new Table(), new DMLColumn(clazz)
+        );
+
+        return queryBuilder.findById(clazz, id);
+    }
+
+    @Override
+    public void persist(Object entity) {
+        final String query = persistQuery(entity);
+
+        jdbcTemplate.execute(query);
+    }
+
+    private String persistQuery(Object entity) {
+        final InsertQueryBuilder queryBuilder = new InsertQueryBuilder(
+                new Table(), new DMLColumn(entity)
+        );
+
+        return queryBuilder.query(entity);
+    }
+
+    @Override
+    public void remove(Object entity) {
+        final String query = removeQuery(entity);
+
+        jdbcTemplate.execute(query);
+    }
+
+    private String removeQuery(Object entity) {
+        final DeleteQueryBuilder queryBuilder = new DeleteQueryBuilder(
+                new Table(), new DMLColumn(entity)
+        );
+
+        return queryBuilder.query(entity);
+    }
+
+}
