d19c172710231e0fb9879098cb27afccf3593a8d
jurlring <give.yoon@woowahan.com>
2024-03-02 16:52:11 +0900

    feature: TODO 추가

--- src/main/java/persistence/EntityManager.java
+++ src/main/java/persistence/EntityManager.java
@@ -4,7 +4,7 @@ import domain.Person;
 
 public interface EntityManager {
 
-    // <T> T find(Class<T> clazz, Long Id); 제네릭을 사용해보셔도 됩니다.
+    // TODO: ObjectMapper 부터 제네릭으로 수정하고 <T> T find(Class<T> clazz, Long Id); 으로 바꾸기
     Person find(Class<Person> clazz, Long id);
 
     Object persist(Object entity);
