2889590f9565576faa6305a26cdd618ed9df209d
joydaheecha <daheecha0612@gmail.com>
2024-03-03 15:28:32 +0900

    (feat) EntityManager 인터페이스 추가

---
+++ src/main/java/persistence/entity/EntityManager.java
@@ -0,0 +1,26 @@
+package persistence.entity;
+
+import org.h2.mvstore.type.ObjectDataType;
+
+public interface EntityManager<T> {
+    /**
+     * 조회
+     * @param clazz 클래스 타입
+     * @param id 식별자
+     * @return 조회 결과
+     */
+    T find(Class<T> clazz, Long id);
+
+    /**
+     * 저장
+     * @param entity 저장 대상
+     * @return 저장 결과
+     */
+    Object persist(Object entity);
+
+    /**
+     * 제거
+     * @param entity 삭제 대상
+     */
+    void remove(Object entity);
+}
