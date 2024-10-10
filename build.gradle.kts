0730edbb7ffd8b9f2a7ad3323048dce0f0105bdb
wotjd243 <wotjd243@naver.com>
2024-10-10 15:00:01 +0900

    build: upgrade to java 21 and enable kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "camp.nextstep.edu"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation("com.h2database:h2:2.1.214")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
