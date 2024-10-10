fbd6c656c2753848ac50993b8dd7b083ef001d40
sooragenius <soora_genius@naver.com>
2024-10-10 23:24:58 +0900

    step1: Reflection (#230)
    * test: Car ReflectionTest 작성
    
    * refect: 마지막 개행 추가
    
    ---------
    
    Co-authored-by: Soora Oh <soora.oh@betalabs.kr>

---
+++ src/test/java/study/ReflectionTest.java
@@ -0,0 +1,91 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+
+        assertThat(carClass.getName()).isEqualTo("study.Car");
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메소드 실행")
+    void testMethodRun() {
+        Car car = new Car("자동차", 10000);
+        Class<Car> carClass = Car.class;
+        List<Method> testMethods = Arrays.stream(carClass.getDeclaredMethods()).filter(it -> it.getName().startsWith("test"))
+                .toList();
+
+        List<Object> results = testMethods.stream().map(it -> invoke(it, car)).toList();
+
+        assertThat(results).contains("test : 자동차", "test : 10000");
+    }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션 메소드 실행")
+    void testAnnotationMethodRun() {
+        Class<Car> carClass = Car.class;
+        Car car = new Car("자동차", 10000);
+        List<Method> printViewMethods = Arrays.stream(carClass.getDeclaredMethods()).filter(it -> hasAnnotation(it, PrintView.class))
+                .toList();
+
+        printViewMethods.forEach(it -> invoke(it, car));
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
+        Class<Car> carClass = Car.class;
+        Car car = new Car("자동차", 10000);
+
+        Field nameField = carClass.getDeclaredField("name");
+        Field priceField = carClass.getDeclaredField("price");
+        nameField.setAccessible(true);
+        priceField.setAccessible(true);
+
+        nameField.set(car, "붕붕이");
+        priceField.set(car, 20000);
+
+        assertThat(car.getName()).isEqualTo("붕붕이");
+        assertThat(car.getPrice()).isEqualTo(20000);
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Car> carClass = Car.class;
+        Constructor<?> constructor = Arrays.stream(carClass.getDeclaredConstructors()).filter(it -> it.getParameterCount() > 0)
+                .findFirst().get();
+
+        Car car = (Car) constructor.newInstance("자동차", 10000);
+
+        assertThat(car.getName()).isEqualTo("자동차");
+        assertThat(car.getPrice()).isEqualTo(10000);
+    }
+
+    private boolean hasAnnotation(Method method, Class<? extends Annotation> annotation) {
+        return method.getAnnotation(annotation) != null;
+    }
+
+    private Object invoke(Method method, Car car) {
+        try {
+            return method.invoke(car);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
