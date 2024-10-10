74602ed8e5536190f0425410c810ac1a133929c3
yangseung-in <rhfpdk92@naver.com>
2024-10-10 23:49:23 +0900

    test: Car객체의 모든 필드정보 출력 테스트 추가

---
+++ src/test/java/study/ReflectionTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체의 모든 필드정보 출력하기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        Field[] declaredFields = carClass.getDeclaredFields();
+        List<String> fieldNames = Arrays.stream(declaredFields)
+                .map(Field::getName)
+                .toList();
+        assertThat(fieldNames).contains("name", "price");
+    }
+}
