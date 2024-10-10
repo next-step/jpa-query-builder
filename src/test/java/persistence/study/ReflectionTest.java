c0abfd434fa1861aa5a348a5b2d8c4c4a4d9f430
sang5c <skah321@gmail.com>
2024-10-11 00:01:25 +0900

    🚀 1단계 - Reflection (#231)
    * docs: 1단계 요구사항 정리
    
    * test: Car 클래스의 모든 필드 정보를 출력한다
    
    * test: Car 클래스의 모든 생성자 정보를 출력한다
    
    * test: Car 클래스의 모든 메서드 정보를 출력한다
    
    * docs: 1단계 요구사항 추가 정리
    
    * test: test로 시작하는 메서드를 실행한다
    
    * test: @PrintView 애노테이션 메서드를 실행한다
    
    * test: private field에 값을 할당한다
    
    * test: 인자를 가진 생성자를 사용하여 인스턴스를 생성한다
    
    * fix: 필드, 생성자, 메서드 순서 상관 없이 검증하도록 변경

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,138 @@
+package persistence.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReflectionTest {
+
+    @DisplayName("getSimpleName() 메서드는 클래스의 이름을 반환한다")
+    @Test
+    void getSimpleName() {
+        Class<Car> carClass = Car.class;
+
+        assertThat(carClass.getSimpleName()).isEqualTo("Car");
+    }
+
+    @DisplayName("getDeclaredFields() 메서드는 클래스에 선언된 필드를 반환한다")
+    @Test
+    void getDeclaredFields() {
+        Class<Car> carClass = Car.class;
+        List<String> fieldNames = Arrays.stream(carClass.getDeclaredFields())
+                .map(Field::getName)
+                .toList();
+
+        assertThat(fieldNames).containsExactlyInAnyOrder("name", "price");
+    }
+
+    @DisplayName("getDeclaredConstructor() 메서드는 클래스에 선언된 생성자를 반환한다")
+    @Test
+    void getDeclaredConstructor() {
+        Class<Car> carClass = Car.class;
+        List<String> constructorNames = Arrays.stream(carClass.getDeclaredConstructors())
+                .map(Constructor::toString)
+                .toList();
+
+        assertThat(constructorNames).containsExactlyInAnyOrder(
+                "public persistence.study.Car()",
+                "public persistence.study.Car(java.lang.String,int)"
+        );
+    }
+
+    @DisplayName("getDeclaredMethods() 메서드는 클래스에 선언된 메서드를 반환한다")
+    @Test
+    void getDeclaredMethods() {
+        Class<Car> carClass = Car.class;
+        List<String> methodNames = Arrays.stream(carClass.getDeclaredMethods())
+                .map(Method::toString)
+                .toList();
+
+        assertThat(methodNames).containsExactlyInAnyOrder(
+                "public void persistence.study.Car.printView()",
+                "public java.lang.String persistence.study.Car.testGetName()",
+                "public java.lang.String persistence.study.Car.testGetPrice()"
+        );
+    }
+
+    @DisplayName("test로 시작하는 메서드를 실행한다")
+    @Test
+    void invokeTestPrefixMethods() throws Exception {
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getDeclaredConstructor().newInstance();
+
+        List<String> invokeResults = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .map(method -> invokeMethod(method, car))
+                .toList();
+
+        assertThat(invokeResults).hasSize(2)
+                .allMatch(result -> result.startsWith("test :"));
+    }
+
+    private String invokeMethod(Method method, Car car) {
+        try {
+            return (String) method.invoke(car);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DisplayName("@PrintView 애노테이션 메서드를 실행한다")
+    @Test
+    void invokePrintViewAnnotatedMethod() throws Exception {
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getDeclaredConstructor().newInstance();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Method annotatedMethod = Arrays.stream(carClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(PrintView.class))
+                .findAny()
+                .orElseThrow();
+        annotatedMethod.invoke(car);
+
+        assertThat(outContent).hasToString("자동차 정보를 출력 합니다.\n");
+    }
+
+    @DisplayName("private field에 값을 할당한다")
+    @Test
+    void setPrivateField() throws Exception {
+        String expectedName = "테슬라";
+        int expectedPrice = 100_000_000;
+
+        Car car = Car.class.getDeclaredConstructor().newInstance();
+        setFieldValue(car, "name", expectedName);
+        setFieldValue(car, "price", expectedPrice);
+
+        assertThat(car.testGetName()).endsWith(expectedName);
+        assertThat(car.testGetPrice()).endsWith(String.valueOf(expectedPrice));
+    }
+
+    private void setFieldValue(Car car, String targetField, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field field = car.getClass().getDeclaredField(targetField);
+        field.setAccessible(true);
+        field.set(car, value);
+    }
+
+    @DisplayName("인자를 가진 생성자를 사용하여 인스턴스를 생성한다")
+    @Test
+    void createInstanceWithConstructor() throws Exception {
+        String expectedName = "테슬라";
+        int expectedPrice = 100_000_000;
+
+        Car car = Car.class.getDeclaredConstructor(String.class, int.class)
+                .newInstance(expectedName, expectedPrice);
+
+        assertThat(car.testGetName()).endsWith(expectedName);
+        assertThat(car.testGetPrice()).endsWith(String.valueOf(expectedPrice));
+    }
+}
