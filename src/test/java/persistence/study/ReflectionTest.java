876851d4b7df0d9c52d5f1f34f99d90791f228f8
SightStudio <sdm3285@gmail.com>
2024-10-12 08:40:44 +0900

    test: 요구 사항 2 - test로 시작하는 메소드 실행

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -14,5 +16,22 @@ public class ReflectionTest {
     void showClass() {
         Class<Car> carClass = Car.class;
         logger.debug(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("Car 객체의 메서드 중 test로 시작하는 메소드 실행한다.")
+    void testMethodRun() throws Exception {
+        // given
+        Class<Car> carClass = Car.class;
+        Method[] declaredMethods = carClass.getDeclaredMethods();
+        var car = carClass.getDeclaredConstructors()[1].newInstance("BMW", 100_000);
+
+        // when
+        for (Method method : declaredMethods) {
+            if (method.getName().startsWith("test")) {
+                Object result = method.invoke(car);
+                logger.debug("method name : {}, result :  {}", method.getName(), result);
+            }
+        }
     }
 }
