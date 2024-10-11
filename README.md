1b19cc8405a7ba0c883e03b4493daea2287dc1ba
Jay Kang <jihoi.kang@gmail.com>
2024-10-11 13:34:54 +0900

    [step1] reflection test (#232)
    * docs: add requirements
    
    * feat: add car class
    
    * feat: add show class information
    
    * feat: execute method start with test
    
    * feat: execute method with annotation @PrintView
    
    * feat: add set private field
    
    * feat: create car instance with args constructor
    
    * refactor: clean up code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jpa-query-builder
+
+## step1
+### requirements
+- [x] 클래스 정보 출력
+- [x] test로 시작하는 메소드 실행
+- [x] @PrintView Annotation method 실행
+- [x] private field에 값 할당
+- [x] 인자를 가진 생성자의 인스턴스 생성
---
+++ src/test/java/study/ReflectionTest.java
@@ -0,0 +1,123 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.info("[클래스 정보 출력]");
+        logger.debug("name=" + carClass.getName());
+        logger.debug("modifiers=" + carClass.getModifiers());
+
+        // 모든 필드 정보 출력
+        logger.info("[모든 필드 정보 출력]");
+        logger.info("[필드] 접근 가능한 public 필드 목록");
+        for (Field field : carClass.getFields()) {
+            logger.debug("name=" + field.getName() + " modifiers=" + field.getModifiers());
+        }
+        logger.info("[필드] 모든 필드 목록");
+        for (Field field : carClass.getDeclaredFields()) {
+            logger.debug("name=" + field.getName() + " modifiers=" + field.getModifiers());
+        }
+
+        // 생성자 정보 출력
+        logger.info("[생성자] 접근 가능한 public 생성자 목록");
+        for (Constructor<?> constructor : carClass.getConstructors()) {
+            logger.debug(constructor.getName() + ": modifiers=" + constructor.getModifiers() + " parameterTypes=" + Arrays.toString(
+                constructor.getParameterTypes()));
+        }
+        logger.info("[생성자] 모든 생성자 목록");
+        for (Constructor<?> constructor : carClass.getDeclaredConstructors()) {
+            logger.debug(constructor.getName() + ": modifiers=" + constructor.getModifiers() + " parameterTypes=" + Arrays.toString(
+                constructor.getParameterTypes()));
+        }
+
+        // 메서드 정보 출력
+        logger.info("[메서드] 부모 클래스, 자신 클래스의 접근 가능한 public 메서드 목록");
+        for (Method method : carClass.getMethods()) {
+            logger.debug(method.getName() + ": modifiers=" + method.getModifiers() + " parameterTypes=" + Arrays.toString(
+                method.getParameterTypes()));
+        }
+
+        logger.info("[메서드] 모든 메서드 목록");
+        for (Method method : carClass.getDeclaredMethods()) {
+            logger.debug(method.getName() + ": modifiers=" + method.getModifiers() + " parameterTypes=" + Arrays.toString(
+                method.getParameterTypes()));
+        }
+    }
+
+    @Test
+    @DisplayName("test로 시작하는 메서드 실행")
+    void testMethodRun() throws Exception {
+//        Class<? extends Car> carClass = Car.class;
+
+        Car car = new Car("tesla", 199999999);
+        Class<? extends Car> carClass = car.getClass();
+
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                Object result = method.invoke(car);
+                logger.debug("" + result);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("@PrintView 애노테이션 메소드 실행")
+    void testAnnotationMethodRun() throws Exception {
+        Class<? extends Car> carClass = Car.class;
+
+        for (Method method : carClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(PrintView.class)) {
+                method.invoke(carClass.newInstance());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    void privateFieldAccess() throws Exception {
+        // given
+        String carName = "소나타";
+        Car car = new Car();
+        Class<? extends Car> carClass = car.getClass();
+        Field field = carClass.getDeclaredField("name");
+
+        // when
+        field.setAccessible(true);
+        field.set(car, carName);
+
+        // then
+        Assertions.assertThat(car.getName()).isEqualTo(carName);
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws Exception {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+
+        for (Constructor<?> constructor : carClass.getDeclaredConstructors()) {
+            if (constructor.getParameterCount() == 2) {
+                Car car = (Car) constructor.newInstance("name", 100000);
+                logger.debug("name=" + car.getName());
+                logger.debug("price=" + car.getPrice());
+            }
+        }
+    }
+
+}
