8226953fa7c100e1f919d5d50d61ba4e33b28c9e
weno <35131529+wenodev@users.noreply.github.com>
2024-10-11 17:52:08 +0900

    test: print all constructors

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -31,5 +35,24 @@ class ReflectionTest {
                     field.getType().getSimpleName(),
                     field.getName());
         }
+    }
+
+    @Test
+    @DisplayName("Car 클래스의 모든 생성자 정보를 출력한다")
+    void printAllConstructors() {
+        final Class<Car> carClass = Car.class;
+        final Constructor<?>[] constructors = carClass.getDeclaredConstructors();
+        for (final Constructor<?> constructor : constructors) {
+            final String modifiers = Modifier.toString(constructor.getModifiers());
+            final String name = carClass.getSimpleName();
+            final String parameters = getParameters(constructor.getParameters());
+            logger.info("Constructor: {} {} ({})", modifiers, name, parameters);
+        }
+    }
+
+    private String getParameters(final Parameter[] parameters) {
+        return Arrays.stream(parameters)
+                .map(param -> param.getType().getSimpleName())
+                .collect(Collectors.joining(", "));
     }
 }
