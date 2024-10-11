9ea9420390397a1ca062e93434d65c0036f2a276
yangseung-in <rhfpdk92@naver.com>
2024-10-11 23:38:39 +0900

    feat: 요구사항4 - drop table query 만들기

--- src/main/java/persistence/sql/ddl/DropQueryBuilder.java
+++ src/main/java/persistence/sql/ddl/DropQueryBuilder.java
@@ -1,7 +1,11 @@
 package persistence.sql.ddl;
 
+import jakarta.persistence.Table;
+
 public class DropQueryBuilder {
     public String dropTableQuery(Class<?> clazz) {
-        return "drop table users";
+        Table annotation = clazz.getAnnotation(Table.class);
+        String tableName = annotation != null && !annotation.name().isEmpty() ? annotation.name() : clazz.getSimpleName().toLowerCase();
+        return String.format("drop table %s", tableName);
     }
 }
