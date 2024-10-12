ecd4350611a60817d832b0396b278e3026a40b0d
lhs0610 <lhs0610@midasin.com>
2024-10-12 23:27:38 +0900

    fix: 피드백 반영
    - GeneratedValue 조건문 메서드 분리

--- src/main/java/persistence/sql/ddl/impl/ColumnGeneratedValueSupplier.java
+++ src/main/java/persistence/sql/ddl/impl/ColumnGeneratedValueSupplier.java
@@ -5,10 +5,16 @@ import jakarta.persistence.GenerationType;
 import persistence.sql.ddl.QueryColumnSupplier;
 import persistence.sql.ddl.node.FieldNode;
 
+import java.util.List;
+
 /**
  * 컬럼 생성 시 @GeneratedValue 어노테이션을 처리하는 컬럼 쿼리 제공자
  */
 public record ColumnGeneratedValueSupplier(short priority) implements QueryColumnSupplier {
+
+    private static final List<GenerationType> ALLOW_AUTO_INCREMENT_STRATEGY = List.of(
+            GenerationType.IDENTITY, GenerationType.AUTO
+    );
 
     @Override
     public boolean supported(FieldNode fieldNode) {
@@ -18,10 +24,14 @@ public record ColumnGeneratedValueSupplier(short priority) implements QueryColum
     @Override
     public String supply(FieldNode fieldNode) {
         GeneratedValue anno = fieldNode.getAnnotation(GeneratedValue.class);
-        if (anno == null || anno.strategy() != GenerationType.IDENTITY && anno.strategy() != GenerationType.AUTO) {
-            return "";
+        if (appendableAutoIncrement(anno)) {
+            return "AUTO_INCREMENT";
         }
 
-        return "AUTO_INCREMENT";
+        return "";
+    }
+
+    private static boolean appendableAutoIncrement(GeneratedValue anno) {
+        return anno != null && ALLOW_AUTO_INCREMENT_STRATEGY.contains(anno.strategy());
     }
 }
