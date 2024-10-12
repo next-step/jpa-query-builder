53a88ccf1a2481eac0152163203a63c2da72745c
jeongdalma <jeongdalma@gmail.com>
2024-10-12 18:05:57 +0900

    chore: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("ch.qos.logback:logback-classic:1.4.7")
     implementation("com.h2database:h2:2.1.214")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
