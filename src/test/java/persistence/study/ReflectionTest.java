52218ee2d2bd1d4421d22c644e0f48e471b7ca75
Dong Min Seol <sdm3285@gmail.com>
2024-10-12 09:51:01 +0900

    1단계 - Reflection (#247)
    * Docs: README.md에 미션 요구사항 정리
    
    * feat: Car 클래스 추가
    
    * test: 요구사항 1. 클래스 정보 출력를 위한 ReflectionTest 추가
    
    * test: 요구 사항 2 - test로 시작하는 메소드 실행
    
    * test: 요구 사항 3 - @PrintView 애노테이션 메소드 실행
    
    * test: 요구 사항 4 - private field에 값 할당
    
    * test: 요구 사항 5 - 인자를 가진 생성자의 인스턴스 생성
    
    ---------
    
    Co-authored-by: dino.dd <dino.dd@kakaocorp.com>

---
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -0,0 +1,100 @@
+package persistence.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+    }
+
+    @Test
+    @DisplayName("Car 객체의 메서드 중 test로 시작하는 메소드 실행한다.")
+    void testMethodRun() throws Exception {
+        // given
+        Class<Car> carClass = Car.class;
+        Method[] declaredMethods = carClass.getDeclaredMethods();
+        var car = carClass.getDeclaredConstructors()[1].newInstance("BMW", 100_000);
+
+        // when
+        for (Method method : declaredMethods) {
+            if (method.getName().startsWith("test")) {
+                Object result = method.invoke(car);
+                logger.debug("method name : {}, result :  {}", method.getName(), result);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Car 객체의 메서드 중 PrintView 어노테이션이 있는 메소드 실행한다.")
+    public void printAnnotatedMethod() throws Exception {
+
+        // given
+        Class<Car> carClass = Car.class;
+        logger.debug(carClass.getName());
+        var car = carClass.getDeclaredConstructors()[0].newInstance();
+
+        // when
+        for (Method declaredMethod : carClass.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(PrintView.class)) {
+                declaredMethod.invoke(car);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("private field에 값 할당")
+    public void privateFieldAccess() throws Exception {
+
+        // given
+        Class<Car> carClass = Car.class;
+        var car = carClass.getDeclaredConstructors()[0].newInstance();
+
+        // when
+        Field field = carClass.getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(car, "소나타");
+
+        // then
+        logger.debug(carClass.getName());
+        assertThat(car)
+                .extracting("name").asString().isEqualTo("소나타");
+    }
+
+    @Test
+    @DisplayName("인자를 가진 생성자의 인스턴스 생성")
+    void constructorWithArgs() throws Exception {
+
+        // given
+        Class<Car> clazz = Car.class;
+        Car car = null;
+
+        // when
+        for (Constructor<?> declaredConstructor : clazz.getDeclaredConstructors()) {
+            int parameterCount = declaredConstructor.getParameterCount();
+            if (parameterCount == 2) {
+                car = (Car) declaredConstructor.newInstance("BMW", 100_000);
+                logger.debug("car : {}", car);
+            }
+        }
+
+        // then
+        assertThat(car).isNotNull();
+    }
+}
