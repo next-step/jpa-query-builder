9db7436f6595f834981a3198d060cf8ae2be7555
hyeongyeong <kohy0329@naver.com>
2024-10-13 21:43:51 +0900

    feat : @GeneratedValue, @Column을 추가하여 Person class 의 create query 생성

---
+++ src/main/java/persistence/sql/ddl/ColumnInfo.java
@@ -0,0 +1,69 @@
+package persistence.sql.ddl;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ColumnInfo {
+
+    private String name;
+    private ColumnType columnType;
+    private boolean primary;
+    private List<String> options;
+
+    public ColumnInfo(String name, ColumnType columnType, boolean primary, List<String> options) {
+        this.name = name;
+        this.columnType = columnType;
+        this.primary = primary;
+        this.options = options;
+    }
+
+    public static ColumnInfo extract(Field field) {
+        return new ColumnInfo(
+                extractColumnName(field),
+                ColumnType.of(field.getType()),
+                field.isAnnotationPresent(Id.class),
+                extractOptions(field)
+        );
+    }
+
+    private static List<String> extractOptions(Field field){
+        List<String> options = new ArrayList<>();
+        final var columnAnotation = field.getAnnotation(Column.class);
+
+        if(field.isAnnotationPresent(Id.class)) {options.add("not null");}
+
+        if(!Objects.isNull(columnAnotation) && !columnAnotation.nullable()) {options.add("not null");}
+
+        final var generatedValue = field.getAnnotation(GeneratedValue.class);
+        if(!Objects.isNull(generatedValue) && generatedValue.strategy().equals(GenerationType.IDENTITY)){
+            options.add("auto_increment");
+        }
+
+        return options;
+    }
+
+    private static String extractColumnName(Field field){
+        final var columnAnotation = field.getAnnotation(Column.class);
+        return Objects.isNull(columnAnotation) || columnAnotation.name().isBlank() ? field.getName() : columnAnotation.name();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public ColumnType getColumnType() {
+        return columnType;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
+    public List<String> getOptions() {
+        return options.stream().distinct().collect(Collectors.toList());
+    }
+}
