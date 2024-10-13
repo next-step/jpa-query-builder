ecabbc834903b0eeca5fb520b5ee2c3ebfe90150
yangseung-in <rhfpdk92@naver.com>
2024-10-13 14:02:55 +0900

    feat: insert query 구현

--- src/main/java/persistence/sql/dml/InsertQueryBuilder.java
+++ src/main/java/persistence/sql/dml/InsertQueryBuilder.java
@@ -1,9 +1,79 @@
 package persistence.sql.dml;
 
-import persistence.sql.domain.Person;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InsertQueryBuilder {
-    public String getInsertQuery(Person person) {
-        return "insert into users (nick_name, old, email) VALUES ('양승인', 33, 'rhfpdk92@naver.com')";
+    public String getInsertQuery(Object object) {
+        Class<?> clazz = object.getClass();
+        String tableName = getTableName(clazz);
+        String tableColumns = columnsClause(clazz);
+        String tableValues = valueClause(object);
+        return String.format("insert into %s (%s) VALUES (%s)", tableName, tableColumns, tableValues);
+    }
+
+    private static String getTableName(Class<?> clazz) {
+        Table annotation = clazz.getAnnotation(Table.class);
+        if (annotation == null) {
+            return clazz.getSimpleName().toLowerCase();
+        }
+
+        if (!annotation.name().isEmpty()) {
+            return annotation.name();
+        }
+
+        return clazz.getSimpleName().toLowerCase();
+    }
+
+
+    private String columnsClause(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> !field.isAnnotationPresent(GeneratedValue.class))
+                .filter(field -> !field.isAnnotationPresent(Transient.class))
+                .map(this::getTableColumn)
+                .collect(Collectors.joining(", "));
+    }
+
+    private String getTableColumn(Field field) {
+        Column annotation = field.getAnnotation(Column.class);
+        if (annotation == null) {
+            return field.getName();
+        }
+        if (!annotation.name().isEmpty()) {
+            return annotation.name();
+        }
+
+        return field.getName();
+    }
+
+    private String valueClause(Object object) {
+        return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(field -> !field.isAnnotationPresent(GeneratedValue.class))
+                .filter(field -> !field.isAnnotationPresent(Transient.class))
+                .map(field -> getTableValue(object, field))
+                .collect(Collectors.joining(", "));
+    }
+
+    private String getTableValue(Object object, Field field) {
+        field.setAccessible(true);
+        try {
+            Object fieldValue = field.get(object);
+            if (fieldValue == null) {
+                return "NULL";
+            }
+            if (fieldValue instanceof String) {
+                return String.format("'%s'", fieldValue);
+            }
+            return fieldValue.toString();
+
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
