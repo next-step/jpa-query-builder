4576933c22f064be4acf35bad3207eae6ca9c960
Jeongsu Yang <mycat83@gmail.com>
2024-10-14 01:10:34 +0900

    refactor: H2ConnectionFactory 클래스 분리

--- src/main/java/persistence/entity/EntityManagerImpl.java
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -1,34 +1,19 @@
 package persistence.entity;
 
-import database.H2;
 import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import persistence.sql.dml.SelectQueryBuilder;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public class EntityManagerImpl<T> implements EntityManager<T> {
     private static final Logger logger = LoggerFactory.getLogger(EntityManagerImpl.class);
-    private static final String SERVER_CREATION_FAILED_MESSAGE = "서버 생성에 실패하였습니다.";
-    private static final String CONNECTION_FAILED_MESSAGE = "서버 연결에 실패하였습니다.";
 
     private final Connection connection;
 
     public EntityManagerImpl() {
-        final H2 server;
-        try {
-            server = new H2();
-        } catch (SQLException e) {
-            throw new IllegalStateException(SERVER_CREATION_FAILED_MESSAGE, e);
-        }
-        
-        try {
-            this.connection = server.getConnection();
-        } catch (SQLException e) {
-            throw new IllegalStateException(CONNECTION_FAILED_MESSAGE, e);
-        }
+        connection = H2ConnectionFactory.getConnection();
     }
 
     @Override
---
+++ src/main/java/persistence/entity/H2ConnectionFactory.java
@@ -0,0 +1,38 @@
+package persistence.entity;
+
+import database.H2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class H2ConnectionFactory {
+    private static final String SERVER_CREATION_FAILED_MESSAGE = "서버 생성에 실패하였습니다.";
+    private static final String CONNECTION_FAILED_MESSAGE = "서버 연결에 실패하였습니다.";
+
+    private H2ConnectionFactory() {
+        throw new AssertionError();
+    }
+
+    public static Connection newConnection() {
+        final H2 server = getServer();
+        return newConnection(server);
+    }
+
+    private static H2 getServer() {
+        final H2 server;
+        try {
+            server = new H2();
+        } catch (SQLException e) {
+            throw new IllegalStateException(SERVER_CREATION_FAILED_MESSAGE, e);
+        }
+        return server;
+    }
+
+    private static Connection newConnection(H2 server) {
+        try {
+            return server.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(CONNECTION_FAILED_MESSAGE, e);
+        }
+    }
+}
