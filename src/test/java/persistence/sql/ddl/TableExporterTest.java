f02fc4ec6e84df8afa6d0ecc08aed7c7992e7964
hellonayeon <hellonykwon@gmail.com>
2024-10-14 03:35:01 +0900

    test: @Entity 애노테이션 없는 경우 예외처리 검증

--- src/test/java/persistence/sql/ddl/TableExporterTest.java
+++ src/test/java/persistence/sql/ddl/TableExporterTest.java
@@ -1,8 +1,11 @@
 package persistence.sql.ddl;
 
+import jakarta.persistence.Entity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TableExporterTest {
 
@@ -19,4 +22,17 @@ public class TableExporterTest {
         Assertions.assertEquals(exporter.getSqlCreateQueryString(Person.class), expectedQuery);
     }
 
+    @Test
+    @DisplayName("[실패] @Entity 애노테이션이 없는 클래스에 대한 create 쿼리 생성 시도 시, RuntimeException 발생")
+    void createQueryWithoutEntityAnnotation() {
+        TableExporter exporter = new TableExporter();
+        assertThatThrownBy(() -> exporter.getSqlCreateQueryString(NotEntityPerson.class))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("@Entity not exist. class = " + NotEntityPerson.class.getName());
+
+    }
+
+    static class NotEntityPerson {
+
+    }
 }
