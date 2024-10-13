904f51eb23777a0acbcac8a56dee2dedad369bb6
hellonayeon <hellonykwon@gmail.com>
2024-10-14 03:47:07 +0900

    style: 미사용 import 제거 (Optimize imports 설정 활성화)

--- src/test/java/persistence/sql/ddl/TableExporterTest.java
+++ src/test/java/persistence/sql/ddl/TableExporterTest.java
@@ -1,6 +1,5 @@
 package persistence.sql.ddl;
 
-import jakarta.persistence.Entity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
