46f2d9c37c817a15d94c87d9562157b847c91466
hellonayeon <hellonykwon@gmail.com>
2024-10-14 03:24:43 +0900

    feat: Java Type -> SQL Type 변환을 위한 Enum 정의

---
+++ src/main/java/persistence/sql/ddl/type/TypeReference.java
@@ -0,0 +1,36 @@
+package persistence.sql.ddl.type;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public enum TypeReference {
+
+    LONG(Long.class, "bigint"),
+    STRING(String.class, "varchar"),
+    INTEGER(Integer.class, "integer")
+    ;
+
+    private final Class<?> javaType;
+    private final String sqlType;
+    private static final Map<Class<?>, String> sqlTypes;
+
+    static {
+        sqlTypes = Arrays.stream(values())
+                .collect(Collectors.toConcurrentMap(reference -> reference.javaType, reference -> reference.sqlType));
+    }
+
+    TypeReference(Class<?> javaType, String sqlType) {
+        this.javaType = javaType;
+        this.sqlType = sqlType;
+    }
+
+    public static String getSqlType(Class<?> clazz) {
+        try {
+            return sqlTypes.get(clazz);
+        } catch (NullPointerException  ex) {
+            throw new RuntimeException("class '" + clazz.getName() + "' sql type not exist.");
+        }
+    }
+
+}
