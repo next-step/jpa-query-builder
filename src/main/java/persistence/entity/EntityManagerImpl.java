9ddfa97d949dfbf2caa685aa94888178dd0aed0b
Jeongsu Yang <mycat83@gmail.com>
2024-10-14 01:27:21 +0900

    refactor: 인스턴스 생성 시점에 jdbcTemplate 생성

--- src/main/java/persistence/entity/EntityManagerImpl.java
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -6,22 +6,19 @@ import org.slf4j.LoggerFactory;
 import persistence.sql.dml.InsertQueryBuilder;
 import persistence.sql.dml.SelectQueryBuilder;
 
-import java.sql.Connection;
-
 public class EntityManagerImpl<T> implements EntityManager<T> {
     private static final Logger logger = LoggerFactory.getLogger(EntityManagerImpl.class);
 
-    private final Connection connection;
+    private final JdbcTemplate jdbcTemplate;
 
     public EntityManagerImpl() {
-        connection = H2ConnectionFactory.newConnection();
+        this.jdbcTemplate = new JdbcTemplate(H2ConnectionFactory.newConnection());
     }
 
     @Override
     public T find(Class<T> entityClass, Long id) {
         final SelectQueryBuilder selectQueryBuilder = new SelectQueryBuilder(entityClass);
         final String sql = selectQueryBuilder.findById(id);
-        final JdbcTemplate jdbcTemplate = new JdbcTemplate(connection);
         return jdbcTemplate.queryForObject(sql, new CustomRowMapper<>(entityClass));
     }
 
@@ -29,7 +26,6 @@ public class EntityManagerImpl<T> implements EntityManager<T> {
     public void persist(Object entity) {
         final InsertQueryBuilder insertQueryBuilder = new InsertQueryBuilder(entity);
         final String sql = insertQueryBuilder.insert();
-        final JdbcTemplate jdbcTemplate = new JdbcTemplate(connection);
         jdbcTemplate.execute(sql);
     }
 }
