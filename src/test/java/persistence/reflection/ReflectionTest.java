a72744826f1019284eb84f9679e1a5d1bd25fdbf
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-10-13 20:15:32 +0900

    update: 요구사항 3 - Assertion.asserThat 사용하기

--- src/test/java/persistence/reflection/ReflectionTest.java
+++ src/test/java/persistence/reflection/ReflectionTest.java
@@ -1,6 +1,5 @@
 package persistence.reflection;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.study.Car;
