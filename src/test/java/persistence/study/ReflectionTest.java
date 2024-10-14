69d7835cc74b8b7d8f733d40a5aa822613aaa003
Park <qwer9w@gmail.com>
2024-10-15 00:05:59 +0900

    feat: 테스트코드 추가 (요구 사항 1 - 클래스 정보 출력)

--- src/test/java/persistence/study/ReflectionTest.java
+++ src/test/java/persistence/study/ReflectionTest.java
@@ -2,11 +2,48 @@ package persistence.study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReflectionTest {
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("클래스의 모든 필드 출력")
+    void showClassFields() {
+        Class<Car> carClass = Car.class;
+        Field[] declaredFields = carClass.getDeclaredFields();
+
+        Arrays.stream(declaredFields).forEach(field -> logger.info("field name: {}, type: {}", field.getName(), field.getType()));
+    }
+
+    @Test
+    @DisplayName("클래스의 모든 생성자 출력")
+    void showClassConstructors() {
+        Class<Car> carClass = Car.class;
+        Constructor<?>[] constructors = carClass.getConstructors();
+
+        Arrays.stream(constructors).forEach(constructor -> logger.info("constructor name: {}, parameterCount: {}", constructor.getName(), constructor.getParameterCount()));
+    }
+
+    @Test
+    @DisplayName("클래스의 모든 메소드 출력")
+    void showClassMethods() {
+        Class<Car> carClass = Car.class;
+        Method[] methods = carClass.getDeclaredMethods();
+
+        Arrays.stream(methods).forEach(method -> logger.info("method name: {}, modifiers: {}, parameterType: {}", method.getName(), method.getModifiers(), method.getParameterTypes()));
+    }
+
+
     @Test
     @DisplayName("클래스 이름 검증")
     void testReflectionAPI() {
