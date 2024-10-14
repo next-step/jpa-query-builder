c2bf31d1016c670a4f56d101ae7b6083ab8d3c45
weno <35131529+wenodev@users.noreply.github.com>
2024-10-15 00:31:52 +0900

    test: add for main method

---
+++ src/test/java/persistence/ApplicationTest.java
@@ -0,0 +1,15 @@
+package persistence;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class ApplicationTest {
+    @Test
+    @DisplayName("Application.main 메서드는 예외를 던지지 않아야 한다")
+    void mainMethodDoesNotThrowAnyException() {
+        assertThatCode(() -> Application.main(new String[]{}))
+                .doesNotThrowAnyException();
+    }
+}
