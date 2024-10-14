9c1ebfdf3a4a87d5d8f99eceed938a8d47b2535c
hellonayeon <hellonykwon@gmail.com>
2024-10-14 19:14:11 +0900

    test: 요구사항 1~3 충족 확인을 위한 Fixture 클래스 추가

---
+++ src/test/java/persistence/sql/ddl/fixture/PersonWithGeneratedValueColumnFixture.java
@@ -0,0 +1,21 @@
+package persistence.sql.ddl.fixture;
+
+import jakarta.persistence.*;
+
+@Entity
+public class PersonWithGeneratedValueColumnFixture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name")
+    private String name;
+
+    @Column(name = "old")
+    private Integer age;
+
+    @Column(nullable = false)
+    private String email;
+
+}
