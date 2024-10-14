3eaaaafada6892f768cfbe8147dfb6533fb33703
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-14 23:41:47 +0900

    fix: 패키지분리

--- src/main/java/builder/h2/ddl/H2QueryBuilderDDL.java
+++ src/main/java/builder/h2/ddl/H2QueryBuilderDDL.java
@@ -1,5 +1,6 @@
-package builder.h2;
+package builder.h2.ddl;
 
+import builder.QueryBuilder;
 import builder.QueryBuilderDDL;
 import jakarta.persistence.*;
 
@@ -8,9 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class H2QueryBuilderDDL implements QueryBuilderDDL {
+public class H2QueryBuilderDDL extends QueryBuilder implements QueryBuilderDDL {
 
-    private final static String NOT_EXIST_ENTITY_ANNOTATION = "@Entity 어노테이션이 존재하지 않습니다.";
     private final static String ID_ANNOTATION_OVER_ONE = "@Id 어노테이션은 한개를 초과할수 없습니다.";
     private final static String CREATE_QUERY = "CREATE TABLE {tableName} ({columnDefinitions});";
     private final static String DROP_QUERY = "DROP TABLE {tableName};";
@@ -26,7 +26,7 @@ public class H2QueryBuilderDDL implements QueryBuilderDDL {
     @Override
     public String buildCreateQuery(Class<?> entityClass) {
         confirmEntityAnnotation(entityClass);
-        return createTableQuery(getTableName(entityClass), getColumnData(entityClass));
+        return createTableQuery(getTableName(entityClass), getDDLColumnData(entityClass));
     }
 
     //drop 쿼리를 생성한다.
@@ -37,11 +37,11 @@ public class H2QueryBuilderDDL implements QueryBuilderDDL {
     }
 
     //create 쿼리를 생성한다.
-    public String createTableQuery(String tableName, List<ColumnData> columns) {
+    public String createTableQuery(String tableName, List<DDLColumnData> columns) {
         // 테이블 열 정의 생성
         String columnDefinitions = columns.stream()
                 .map(column -> {
-                    String definition = column.getColumnName() + BLANK + column.getColumnDataType();
+                    String definition = column.columnName() + BLANK + column.columnDataType();
                     // primary key인 경우 "PRIMARY KEY" 추가
                     if (column.isNotNull()) definition += NOT_NULL; //false면 NOT_NULL 조건 추가
                     if (column.isAutoIncrement()) definition += AUTO_INCREMENT; //true면 AutoIncrement 추가
@@ -60,50 +60,34 @@ public class H2QueryBuilderDDL implements QueryBuilderDDL {
         return DROP_QUERY.replace(TABLE_NAME, tableName);
     }
 
-    //Entity 어노테이션 여부를 확인한다.
-    private void confirmEntityAnnotation(Class<?> entityClass) {
-        if (!entityClass.isAnnotationPresent(Entity.class)) {
-            throw new IllegalArgumentException(NOT_EXIST_ENTITY_ANNOTATION);
-        }
-    }
-
-    //Table 어노테이션 여부를 확인한다.
-    private String getTableName(Class<?> entityClass) {
-        if (entityClass.isAnnotationPresent(Table.class)) {
-            Table table = entityClass.getAnnotation(Table.class);
-            return table.name();
-        }
-        return entityClass.getSimpleName();
-    }
-
     //변수들의 정보를 가져온다.
-    private List<ColumnData> getColumnData(Class<?> entityClass) {
+    private List<DDLColumnData> getDDLColumnData(Class<?> entityClass) {
         Field[] fields = entityClass.getDeclaredFields();
-        List<ColumnData> columnDataList = new ArrayList<>();
+        List<DDLColumnData> DDLColumnDataList = new ArrayList<>();
 
         for (Field field : fields) {
-            createTableColumnData(columnDataList, field);
+            createTableDDLColumnData(DDLColumnDataList, field);
         }
 
-        return columnDataList;
+        return DDLColumnDataList;
     }
 
     //테이블에 생성될 필드(컬럼)들을 생성한다.
-    private void createTableColumnData(List<ColumnData> columnDataList, Field field) {
-        getPrimaryKey(columnDataList, field);
-        getColumnAnnotationData(columnDataList, field);
+    private void createTableDDLColumnData(List<DDLColumnData> DDLColumnDataList, Field field) {
+        getPrimaryKey(DDLColumnDataList, field);
+        getColumnAnnotationData(DDLColumnDataList, field);
     }
 
     //Id 어노테이션을 primarykey로 가져온다.
-    private void getPrimaryKey(List<ColumnData> columnDataList, Field field) {
+    private void getPrimaryKey(List<DDLColumnData> DDLColumnDataList, Field field) {
         if (field.isAnnotationPresent(Id.class)) {
-            confirmIdAnnotationOverTwo(columnDataList);
-            columnDataList.add(ColumnData.createPk(field.getName(), field.getType(), confirmGeneratedValueAnnotation(field)));
+            confirmIdAnnotationOverTwo(DDLColumnDataList);
+            DDLColumnDataList.add(DDLColumnData.createPk(field.getName(), field.getType(), confirmGeneratedValueAnnotation(field)));
         }
     }
 
     //Column 어노테이션 여부를 확인하여 변수의 컬럼타입을 가져온다.
-    private void getColumnAnnotationData(List<ColumnData> columnDataList, Field field) {
+    private void getColumnAnnotationData(List<DDLColumnData> DDLColumnDataList, Field field) {
         if (field.isAnnotationPresent(Transient.class) || field.isAnnotationPresent(Id.class))
             return; // Transient 어노테이션이 있거나 @Id인 경우 검증하지 않음
 
@@ -116,13 +100,13 @@ public class H2QueryBuilderDDL implements QueryBuilderDDL {
             isNullable = column.nullable();
         }
 
-        columnDataList.add(ColumnData.createColumn(columnName, field.getType(), !isNullable));
+        DDLColumnDataList.add(DDLColumnData.createColumn(columnName, field.getType(), !isNullable));
     }
 
     //Entity에 @Id가 2개 이상은 아닐지 확인한다.
-    private void confirmIdAnnotationOverTwo(List<ColumnData> columnDataList) {
-        boolean hasPrimaryKey = columnDataList.stream()
-                .anyMatch(ColumnData::isPrimaryKey); // primaryKey가 true인 컬럼이 하나라도 있는지 확인
+    private void confirmIdAnnotationOverTwo(List<DDLColumnData> DDLColumnDataList) {
+        boolean hasPrimaryKey = DDLColumnDataList.stream()
+                .anyMatch(DDLColumnData::isPrimaryKey); // primaryKey가 true인 컬럼이 하나라도 있는지 확인
 
         if (hasPrimaryKey) {
             throw new IllegalArgumentException(ID_ANNOTATION_OVER_ONE);
