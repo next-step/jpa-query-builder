61a460836fcdf80cf8c92cae4f3a75310e08c165
weno <35131529+wenodev@users.noreply.github.com>
2024-10-15 01:29:25 +0900

    feat: add @Table

--- src/main/java/persistence/sql/ddl/Person.java
+++ src/main/java/persistence/sql/ddl/Person.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
+@Table(name = "users")
 @Entity
 public class Person {
 
