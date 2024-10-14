8f68a36b1127de73c689e61bee95fcf43975532e
jeongdalma <jeongdalma@gmail.com>
2024-10-14 17:35:08 +0900

    test: Column 테스트 추가

--- src/test/java/persistence/sql/ddl/ColumnTest.kt
+++ src/test/java/persistence/sql/ddl/ColumnTest.kt
@@ -1,27 +1,41 @@
 package persistence.sql.ddl
 
 import entity.Person
+import entity.Person1
 import exception.ColumnTypeUnavailableException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import persistence.sql.QueryManagerFactory
-import persistence.sql.ddl.column.Column
 import persistence.sql.ddl.table.Table
 
 class ColumnTest: DescribeSpec({
 
     describe("createQuery 메소드는") {
-        val person = Person::class.java
+
 
         context("지원하는 Field 타입은") {
+            val person = Person::class.java
 
-            val idColumn = QueryManagerFactory.column(person.getDeclaredField("id"))
-            val stringColumn = QueryManagerFactory.column(person.getDeclaredField("name"))
-            val intColumn = QueryManagerFactory.column(person.getDeclaredField("age"))
-            it("DDL 컬럼 문자열을 반환한다.") {
+            it("Column 어노테이션으로 지정된 name을 기준으로 DDL 컬럼 문자열을 반환한다") {
+                val idColumn = QueryManagerFactory.column(person.getDeclaredField("id"))
+                val stringColumn = QueryManagerFactory.column(person.getDeclaredField("name"))
+                val intColumn = QueryManagerFactory.column(person.getDeclaredField("age"))
+
                 stringColumn.createQuery() shouldBe "nick_name VARCHAR(255) DEFAULT NULL"
                 intColumn.createQuery() shouldBe "old int DEFAULT NULL"
+                idColumn.createQuery() shouldBe "id bigint NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
+            }
+
+            val person1 = Person1::class.java
+
+            it("Column 어노테이션이 존재하지 않는다면 필드 이름을 기준으로 DDL 컬럼 문자열을 반환한다") {
+                val idColumn = QueryManagerFactory.column(person1.getDeclaredField("id"))
+                val stringColumn = QueryManagerFactory.column(person1.getDeclaredField("name"))
+                val intColumn = QueryManagerFactory.column(person1.getDeclaredField("age"))
+
+                stringColumn.createQuery() shouldBe "name VARCHAR(255) DEFAULT NULL"
+                intColumn.createQuery() shouldBe "age int DEFAULT NULL"
                 idColumn.createQuery() shouldBe "id bigint NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)"
             }
         }
@@ -36,6 +50,7 @@ class ColumnTest: DescribeSpec({
         }
 
         context("Transient 어노테이션이 작성된 필드는") {
+            val person = Person::class.java
             val transientColumn = QueryManagerFactory.column(person.getDeclaredField("index"))
 
             it("공백을 반환한다") {
