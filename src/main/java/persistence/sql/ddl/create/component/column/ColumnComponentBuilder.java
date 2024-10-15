067f7b233eea812e0648634e1ca9a920b43b22b3
seungnong <tmddnrdl333@naver.com>
2024-10-15 23:19:50 +0900

    feat: @Column nullable 적용

--- src/main/java/persistence/sql/ddl/create/component/column/ColumnComponentBuilder.java
+++ src/main/java/persistence/sql/ddl/create/component/column/ColumnComponentBuilder.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 public class ColumnComponentBuilder {
     private static final String INDENT = "\t";
     private static final String COMMA_NEW_LINE = ",\n";
+    public static final String NOT_NULL = "not null";
     private final StringBuilder componentBuilder = new StringBuilder();
 
     private ColumnComponentBuilder() {
@@ -17,16 +18,31 @@ public class ColumnComponentBuilder {
         columnComponentBuilder.componentBuilder
                 .append(INDENT)
                 .append(getTableName(field)).append(INDENT)
-                .append(DataTypeConverter.convert(field.getType())).append(COMMA_NEW_LINE);
+                .append(DataTypeConverter.convert(field.getType())).append(INDENT);
+
+        columnComponentBuilder.appendIfNotNull(field);
+
+        columnComponentBuilder.componentBuilder
+                .append(COMMA_NEW_LINE);
+
         return columnComponentBuilder;
     }
 
     private static String getTableName(Field field) {
-        if (!field.isAnnotationPresent(Column.class)) {
+        if (!field.isAnnotationPresent(Column.class)
+                || "".equals(field.getAnnotation(Column.class).name())) {
             return field.getName();
         }
-
         return field.getAnnotation(Column.class).name();
+    }
+
+    private void appendIfNotNull(Field field) {
+        if (!field.isAnnotationPresent(Column.class)
+                || field.getAnnotation(Column.class).nullable()) {
+            return;
+        }
+        this.componentBuilder
+                .append(NOT_NULL).append(INDENT);
     }
 
     /* TODO : append comment, nullable or not, etc. */
