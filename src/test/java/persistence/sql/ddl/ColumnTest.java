0e1501234a5f1847c82e43212f61f28511ddc8e4
hyeongyeong <kohy0329@naver.com>
2024-10-15 22:15:12 +0900

    docs : display name 변경

--- src/test/java/persistence/sql/ddl/ColumnTest.java
+++ src/test/java/persistence/sql/ddl/ColumnTest.java
@@ -14,7 +14,7 @@ class ColumnTest {
     private static final Logger logger = LoggerFactory.getLogger(ColumnTest.class);
 
     @Test
-    @DisplayName("Person Class로 Column 생성 : id")
+    @DisplayName("Person Class로 Column 생성 : id Field에 대하여 name, Column Type, primary Key 여부가 잘 변환되었는지 확인")
     void createColumnClassWithPersonClass_id() throws Exception {
         ColumnInfo idColumn = ColumnInfo.extract(Person.class.getDeclaredField("id"));
         logger.info("{} 의 타입은 {}, primary 는 {}", idColumn.getName(), idColumn.getColumnType().getJavaType(), idColumn.isPrimaryKey());
@@ -26,7 +26,7 @@ class ColumnTest {
     }
 
     @Test
-    @DisplayName("Person Class로 Column 생성 : name")
+    @DisplayName("Person Class로 Column 생성 : name Field에 대하여 name, Column Type, primary Key 여부가 잘 변환되었는지 확인")
     void createColumnClassWithPersonClass_name() throws Exception {
         ColumnInfo nameColumn = ColumnInfo.extract(Person.class.getDeclaredField("name"));
         logger.info("{} 의 타입은 {}, primary 는 {}", nameColumn.getName(), nameColumn.getColumnType().getJavaType(), nameColumn.isPrimaryKey());
@@ -38,7 +38,7 @@ class ColumnTest {
     }
 
     @Test
-    @DisplayName("Person Class로 Column 생성 : age")
+    @DisplayName("Person Class로 Column 생성 : age Field에 대하여 name, Column Type, primary Key 여부가 잘 변환되었는지 확인")
     void createColumnClassWithPersonClass_age() throws Exception {
         ColumnInfo ageColumn = ColumnInfo.extract(Person.class.getDeclaredField("age"));
         logger.info("{} 의 타입은 {}, primary 는 {}", ageColumn.getName(), ageColumn.getColumnType().getJavaType(), ageColumn.isPrimaryKey());
