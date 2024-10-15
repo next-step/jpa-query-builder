d1020615ee546092af61e1b7db3ebd2f9511e907
yangseung-in <rhfpdk92@naver.com>
2024-10-16 02:05:17 +0900

    chore: 불필요 코드 제거

--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -5,11 +5,6 @@ import database.H2;
 import jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import persistence.sql.ddl.CreateQueryBuilder;
-import persistence.sql.dml.InsertQueryBuilder;
-import persistence.sql.dml.SelectQueryBuilder;
-import persistence.sql.domain.Person;
-import persistence.sql.entity.EntityManagerImpl;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -22,20 +17,6 @@ public class Application {
 
             final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
 
-            CreateQueryBuilder queryBuilder = new CreateQueryBuilder(Person.class);
-            String tableQuery = queryBuilder.createTableQuery(Person.class);
-            InsertQueryBuilder insertQueryBuilder = new InsertQueryBuilder(Person.class);
-
-            Person person = new Person("yang", 23, "rhfp@naver.com", 3);
-            String insertQuery = insertQueryBuilder.getInsertQuery(person);
-
-            jdbcTemplate.execute(tableQuery);
-            jdbcTemplate.execute(insertQuery);
-            SelectQueryBuilder sb = new SelectQueryBuilder(Person.class);
-
-            EntityManagerImpl entityManager = new EntityManagerImpl(server.getConnection());
-            Person person1 = entityManager.find(Person.class, 1L);
-            System.out.println(person1);
 
             server.stop();
         } catch (Exception e) {
