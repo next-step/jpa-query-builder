2a59c552ad242149ea083fcebb79988a7c59bd1e
Park <qwer9w@gmail.com>
2024-10-15 23:04:16 +0900

    feat: create 함수 작성 및 mapFieldTypeToSQLType 기능 추가

--- src/main/java/persistence/sql/ddl/QueryBuilder.java
+++ src/main/java/persistence/sql/ddl/QueryBuilder.java
@@ -1,7 +1,37 @@
 package persistence.sql.ddl;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 public class QueryBuilder {
     public String create(Class<?> entity) {
+        String createTableQuery = this.getCreateTableQuery(entity);
+        String columnDefinitions = this.generateColumnDefinitions(entity);
+
+        return "%s (%s);".formatted(createTableQuery, columnDefinitions);
+    }
+
+    private String getCreateTableQuery(Class<?> entity) {
+        String entityName = entity.getSimpleName();
+
+        return "CREATE TABLE %s".formatted(entityName);
+    }
+
+    private String generateColumnDefinitions(Class<?> entity) {
+        Field[] fields = entity.getDeclaredFields();
+        List<String> columns = Arrays.stream(fields).map(field -> "%s %s".formatted(field.getName(), this.mapFieldTypeToSQLType(field))).toList();
+        return String.join(", ", columns);
+    }
+
+    private String mapFieldTypeToSQLType(Field field) {
+        if (field.getType().equals(String.class)) {
+            return "VARCHAR(255)";
+        } else if (field.getType().equals(Integer.class)) {
+            return "INTEGER";
+        } else if (field.getType().equals(Long.class)) {
+            return "BIGINT";
+        }
         return "";
     }
 }
