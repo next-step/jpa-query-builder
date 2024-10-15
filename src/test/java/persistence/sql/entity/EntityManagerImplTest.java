6859f628aae7e4d103cabdc02454a1389a69b79a
yangseung-in <rhfpdk92@naver.com>
2024-10-15 23:50:45 +0900

    test: find 테스트 추가

--- src/test/java/persistence/sql/entity/EntityManagerImplTest.java
+++ src/test/java/persistence/sql/entity/EntityManagerImplTest.java
@@ -1,4 +1,56 @@
-import static org.junit.jupiter.api.Assertions.*;
+package persistence.sql.entity;
+
+import database.DatabaseServer;
+import database.H2;
+import jdbc.JdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.sql.ddl.CreateQueryBuilder;
+import persistence.sql.dml.InsertQueryBuilder;
+import persistence.sql.domain.Person;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class EntityManagerImplTest {
-  
+
+    private JdbcTemplate jdbcTemplate;
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void init() throws SQLException {
+        final DatabaseServer server = new H2();
+        server.start();
+
+        CreateQueryBuilder queryBuilder = new CreateQueryBuilder(Person.class);
+        String tableQuery = queryBuilder.createTableQuery(Person.class);
+        InsertQueryBuilder insertQueryBuilder = new InsertQueryBuilder(Person.class);
+        Person person = new Person("yang", 23, "rhfp@naver.com", 3);
+        String insertQuery = insertQueryBuilder.getInsertQuery(person);
+
+        jdbcTemplate = new JdbcTemplate(server.getConnection());
+        jdbcTemplate.execute(tableQuery);
+        jdbcTemplate.execute(insertQuery);
+
+        entityManager = new EntityManagerImpl(server.getConnection());
+    }
+
+    @Test
+    @DisplayName("EntityManager의 find구현")
+    void entityManager_find() {
+        Person expectPerson = new Person(1L, "yang", 23, "rhfp@naver.com");
+
+        Person resultPerson = entityManager.find(Person.class, 1L);
+
+        assertAll(
+                () -> assertThat(expectPerson.getAge()).isEqualTo(resultPerson.getAge()),
+                () -> assertThat(expectPerson.getEmail()).isEqualTo(resultPerson.getEmail()),
+                () -> assertThat(expectPerson.getId()).isEqualTo(resultPerson.getId()),
+                () -> assertThat(expectPerson.getIndex()).isEqualTo(resultPerson.getIndex()),
+                () -> assertThat(expectPerson.getName()).isEqualTo(resultPerson.getName())
+        );
+    }
 }
