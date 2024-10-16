58839973578cbe797a4d979dcdea94e4668b8bb9
weno <35131529+wenodev@users.noreply.github.com>
2024-10-16 19:00:13 +0900

    refactor: replace StringBuilder with String

--- src/main/java/persistence/sql/ddl/QueryGenerator.java
+++ src/main/java/persistence/sql/ddl/QueryGenerator.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import java.lang.reflect.Field;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QueryGenerator {
     private static final String INDENTATION = "    ";
@@ -18,12 +20,10 @@ public class QueryGenerator {
     }
 
     public String create(final Class<?> clazz) {
-        final StringBuilder sql = new StringBuilder();
         final String tableName = getTableName(clazz);
-        appendTableHeader(sql, tableName);
-        appendColumnDefinitions(sql, clazz);
-        appendTableFooter(sql);
-        return sql.toString();
+        return getTableHeader(tableName) +
+               getColumnDefinitions(clazz) +
+               getTableFooter();
     }
 
     private String getTableName(final Class<?> clazz) {
@@ -33,42 +33,27 @@ public class QueryGenerator {
         }
         return clazz.getSimpleName().toUpperCase();
     }
-    private void appendTableHeader(final StringBuilder sql, final String tableName) {
-        sql.append("CREATE TABLE ").append(tableName).append(" (\n");
+
+    private String getTableHeader(final String tableName) {
+        return "CREATE TABLE " + tableName + " (\n";
     }
 
-    private void appendColumnDefinitions(final StringBuilder sql, final Class<?> entityClass) {
+    private String getColumnDefinitions(final Class<?> entityClass) {
         final Field[] fields = entityClass.getDeclaredFields();
-        boolean firstColumn = true;
-        for (final Field field : fields) {
-            if (field.isAnnotationPresent(Transient.class)) {
-                continue;
-            }
-            if (!firstColumn) {
-                sql.append(",\n");
-            }
-            appendColumnDefinition(sql, field);
-            firstColumn = false;
-        }    }
+        return Stream.of(fields)
+                .filter(field -> !field.isAnnotationPresent(Transient.class))
+                .map(this::getColumnDefinition)
+                .collect(Collectors.joining(",\n"));
+    }
 
-    private void appendColumnDefinition(final StringBuilder sql, final Field field) {
+    private String getColumnDefinition(final Field field) {
         final String columnName = getColumnName(field);
         final String columnType = getColumnType(field);
 
-        sql.append(INDENTATION)
-                .append(columnName)
-                .append(" ")
-                .append(columnType);
-
-        if (isIdentity(field)) {
-            sql.append(" AUTO_INCREMENT");
-        }
-        if (!isNullable(field)) {
-            sql.append(" NOT NULL");
-        }
-        if (isIdField(field)) {
-            sql.append(" PRIMARY KEY");
-        }
+        return INDENTATION + columnName + " " + columnType +
+               (isIdentity(field) ? " AUTO_INCREMENT" : "") +
+               (isNullable(field) ? "" : " NOT NULL") +
+               (isIdField(field) ? " PRIMARY KEY" : "");
     }
 
     private String getColumnName(final Field field) {
@@ -102,7 +87,7 @@ public class QueryGenerator {
         }
     }
 
-    private void appendTableFooter(final StringBuilder sql) {
-        sql.append(");");
+    private String getTableFooter() {
+        return ");";
     }
 }
