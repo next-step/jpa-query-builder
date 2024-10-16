7c59ad1364924fc9b7c7a2e195ad9526a358b634
EDGAR <mj110602@gmail.com>
2024-10-16 23:11:11 +0900

    refact:  String타입 데이터들 가변형태로 가능하게 끔 DB 데이터 타입 변경( 255도 될수있지만 60등 다양하게 변경 가능 )

--- src/main/java/persistence/sql/ddl/H2DBDataType.java
+++ src/main/java/persistence/sql/ddl/H2DBDataType.java
@@ -13,7 +13,7 @@ public class H2DBDataType {
         typeMap.put(long.class, "BIGINT");
         typeMap.put(Integer.class, "INTEGER");
         typeMap.put(int.class, "INTEGER");
-        typeMap.put(String.class, "VARCHAR(255)");
+        typeMap.put(String.class, "VARCHAR");
         typeMap.put(Boolean.class, "BIT");
         typeMap.put(boolean.class, "BIT");
         typeMap.put(Double.class, "DOUBLE");
@@ -24,10 +24,20 @@ public class H2DBDataType {
     }
 
     public static String castType(Class<?> columnType) {
+        return castType(columnType, 255);
+    }
+
+    public static String castType(Class<?> columnType, int length) {
         String sqlType = typeMap.get(columnType);
+
+        if (columnType == String.class) {
+            return sqlType + "(" + length + ")";
+        }
+
         if (sqlType == null) {
             throw new IllegalArgumentException("Unsupported data type: " + columnType.getName());
         }
+
         return sqlType;
     }
 }
