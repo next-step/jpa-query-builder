93120e4f732bb42b10970f0838c1a1593d267a6a
dohoonkim <dohoonkim023@gmail.com>
2024-10-17 12:16:56 +0900

    feat: 요구사항 3 - 추가된 정보를 통해 create 쿼리 만들어보기2

--- src/main/java/persistence/domain/Person.java
+++ src/main/java/persistence/domain/Person.java
@@ -1,25 +1,31 @@
-package persistence.domain;
+    package persistence.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+    import jakarta.persistence.Column;
+    import jakarta.persistence.Entity;
+    import jakarta.persistence.GeneratedValue;
+    import jakarta.persistence.GenerationType;
+    import jakarta.persistence.Id;
+    import jakarta.persistence.Table;
+    import jakarta.persistence.Transient;
 
-@Entity
-public class Person {
+    @Table(name = "users")
+    @Entity
+    public class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Column(name = "nick_name")
-    private String name;
+        @Column(name = "nick_name")
+        private String name;
 
-    @Column(name = "old")
-    private Integer age;
+        @Column(name = "old")
+        private Integer age;
 
-    @Column(nullable = false)
-    private String email;
+        @Column(nullable = false)
+        private String email;
 
-}
+        @Transient
+        private Integer index;
+
+    }
