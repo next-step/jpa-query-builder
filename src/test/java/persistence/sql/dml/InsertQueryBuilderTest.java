b67dc31da0cbc7b0d33dd710c88fd394b98e7146
hellonayeon <hellonykwon@gmail.com>
2024-10-17 23:54:33 +0900

    test, refactor: insert query TC명 수정

--- src/test/java/persistence/sql/dml/InsertQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/InsertQueryBuilderTest.java
@@ -5,17 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.sql.ddl.Person;
-import persistence.sql.ddl.dialect.H2Dialect;
 
 public class InsertQueryBuilderTest {
 
     @Test
-    @DisplayName("[성공] Person 클래스에 대한 insert query 검증")
+    @DisplayName("[성공] Person Entity 테이블에 대한 insert query 검증")
     void insertQuery() {
         DMLQueryBuilder queryBuilder = new InsertQueryBuilder();
         String expectedQuery = """
                 insert into table users (id, nick_name, old, email) values (?, ?, ?, ?)""";
-        assertEquals(queryBuilder.build(Person.class, new H2Dialect()), expectedQuery);
+        assertEquals(queryBuilder.build(Person.class), expectedQuery);
     }
 
 }
