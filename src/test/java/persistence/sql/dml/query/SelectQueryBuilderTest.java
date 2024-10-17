4143d865978f72ef090fc2048d5b509140f731a9
hellonayeon <hellonykwon@gmail.com>
2024-10-18 01:53:06 +0900

    test: select with where query 검증

--- src/test/java/persistence/sql/dml/query/SelectQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/query/SelectQueryBuilderTest.java
@@ -2,19 +2,34 @@ package persistence.sql.dml.query;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import persistence.sql.ddl.Person;
+import persistence.sql.dml.query.metadata.ColumnName;
+import persistence.sql.dml.query.metadata.WhereCondition;
 
 public class SelectQueryBuilderTest {
 
     @Test
     @DisplayName("[성공] Person Entity 테이블의 모든 컬럼에 대한 select query")
     void selectQuery() {
-        DMLQueryBuilder queryBuilder = new SelectQueryBuilder();
+        SelectQueryBuilder queryBuilder = new SelectQueryBuilder();
         String expectedQuery = """
                 select id, nick_name, old, email from users""";
         assertEquals(queryBuilder.build(Person.class), expectedQuery);
+    }
+
+    @Test
+    @DisplayName("[성공] Person Entity 테이블의 id 컬럼에 대한 select query")
+    void selectQueryById() {
+        SelectQueryBuilder queryBuilder = new SelectQueryBuilder();
+        String expectedQuery = """
+                select id, nick_name, old, email from users where id = ?""";
+        assertEquals(
+                queryBuilder.build(Person.class, List.of(new WhereCondition(new ColumnName("id"), "="))),
+                expectedQuery
+        );
     }
 
 }
