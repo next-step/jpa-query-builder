730276d5ca84d3031609a29477b01018c9cfe6dc
janghyeonjun2 <epfvkdlxj@naver.com>
2024-10-17 23:39:33 +0900

    fix: 사용되지 않는 상수 삭제

--- src/main/java/H2QueryBuilder/H2QueryBuilderDDL.java
+++ src/main/java/H2QueryBuilder/H2QueryBuilderDDL.java
@@ -16,8 +16,6 @@ public class H2QueryBuilderDDL implements QueryBuilderDDL {
     private final static String PRIMARY_KEY    = " PRIMARY KEY";
     private final static String NOT_NULL       = " NOT NULL";
     private final static String AUTO_INCREMENT = " AUTO_INCREMENT";
-    private final static String TABLE_NAME     = "{tableName}";
-    private final static String COLUMN_INFO    = "{columnInfo}";
 
     @Override
     public String create(Class<?> entityClass) {
