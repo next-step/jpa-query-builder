961f09872f1fec1d87368e87acc3d29cc3354f86
seungnong <tmddnrdl333@naver.com>
2024-10-20 23:09:22 +0900

    feat: findById query 구현

--- src/main/java/persistence/sql/dml/select/SelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/select/SelectQueryBuilder.java
@@ -1,9 +1,17 @@
 package persistence.sql.dml.select;
 
+import jakarta.persistence.Id;
 import persistence.sql.NameUtils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SelectQueryBuilder {
     private String tableName;
+    private String idColumnName;
+    private Map<String, Object> whereCondition = new HashMap<>();
 
     private SelectQueryBuilder() {
     }
@@ -14,6 +22,26 @@ public class SelectQueryBuilder {
 
     public SelectQueryBuilder entityClass(Class<?> entityClass) {
         this.tableName = NameUtils.getTableName(entityClass);
+        this.idColumnName = NameUtils.getColumnName(getIdColumn(entityClass));
+        return this;
+    }
+
+    private Field getIdColumn(Class<?> entityClass) {
+        for (Field field : entityClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Id.class)) {
+                return field;
+            }
+        }
+        throw new IllegalArgumentException("Inappropriate entity class!");
+    }
+
+    public SelectQueryBuilder whereCondition(Map<String, Object> whereCondition) {
+        this.whereCondition.putAll(whereCondition);
+        return this;
+    }
+
+    public SelectQueryBuilder whereIdCondition(String id) {
+        this.whereCondition.put(idColumnName, id);
         return this;
     }
 
@@ -25,6 +53,36 @@ public class SelectQueryBuilder {
                 .append("from ")
                 .append(tableName);
 
+        setCondition(stringBuilder);
+
         return stringBuilder.append(";").toString();
+    }
+
+    private void setCondition(StringBuilder stringBuilder) {
+        if (whereCondition.isEmpty()) {
+            return;
+        }
+
+        stringBuilder
+                .append(" where ");
+
+        whereCondition.forEach(
+                (key, value) -> {
+                    stringBuilder.append(key);
+                    if (value instanceof String) {
+                        stringBuilder.append(" = ").append(value);
+                    } else if (value instanceof List) {
+                        stringBuilder.append(" in (");
+                        ((List<String>) value).forEach(
+                                item -> stringBuilder.append(item).append(", ")
+                        );
+                        stringBuilder.setLength(stringBuilder.length() - 2);
+                        stringBuilder.append(")");
+                    }
+                    stringBuilder.append(" and ");
+                }
+        );
+
+        stringBuilder.setLength(stringBuilder.length() - 5);
     }
 }
--- src/test/java/persistence/sql/dml/select/SelectQueryBuilderTest.java
+++ src/test/java/persistence/sql/dml/select/SelectQueryBuilderTest.java
@@ -10,11 +10,24 @@ public class SelectQueryBuilderTest {
     private static final Logger logger = LoggerFactory.getLogger(SelectQueryBuilderTest.class);
 
     @Test
-    @DisplayName("Select query 테스트")
-    void selectQueryTest() {
+    @DisplayName("FindAll query 테스트")
+    void findAllQueryTest() {
         Class<PersonV3> personV3Class = PersonV3.class;
         String selectQuery = SelectQueryBuilder.newInstance()
                 .entityClass(personV3Class)
+                .build();
+
+        logger.debug("query : {}", selectQuery);
+    }
+
+    @Test
+    @DisplayName("FindById query 테스트")
+    void findByIdTest() {
+        Class<PersonV3> personV3Class = PersonV3.class;
+
+        String selectQuery = SelectQueryBuilder.newInstance()
+                .entityClass(personV3Class)
+                .whereIdCondition(String.valueOf(1L))
                 .build();
 
         logger.debug("query : {}", selectQuery);
