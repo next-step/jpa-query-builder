6b39e0da5816f14e0d26fbd8c7c2e14b2a073379
dohoonkim <dohoonkim023@gmail.com>
2024-10-21 12:50:12 +0900

    refactor: EntityTableMetadata 재사용

--- src/main/java/persistence/sql/dml/InsertQuery.java
+++ src/main/java/persistence/sql/dml/InsertQuery.java
@@ -1,22 +1,26 @@
 package persistence.sql.dml;
 
-import persistence.sql.ddl.TableName;
+import java.util.Map;
+import java.util.stream.Collectors;
+import persistence.sql.ddl.ColumnName;
+import persistence.sql.ddl.ColumnType;
+import persistence.sql.ddl.EntityTableMetadata;
 import persistence.sql.ddl.ValidateEntity;
 
 public class InsertQuery<T> {
 
-    private final Class<?> entityClass;
     private final T entity;
+    private final EntityTableMetadata entityTableMetadata;
 
-    public InsertQuery(Class<?> entityClass, T entity) {
-        new ValidateEntity(entityClass);
-        this.entityClass = entityClass;
+    public InsertQuery(T entity) {
+        new ValidateEntity(entity.getClass());
         this.entity = entity;
+        this.entityTableMetadata = new EntityTableMetadata(entity.getClass());
     }
 
     public String generateQuery() throws IllegalAccessException {
-        String tableName = new TableName(entityClass).getTableName();
-        String columns = new ColumnClause<>(entity).getClause();
+        String tableName = entityTableMetadata.getTableName();
+        String columns = getColumns(entityTableMetadata.getColumnDefinitions());
         String values = new ValueClause<>(entity).getClause();
 
         return generateInsertQuery(tableName, columns, values);
@@ -28,6 +32,13 @@ public class InsertQuery<T> {
             .columns(columns)
             .values(values)
             .build();
+    }
+
+    private String getColumns(Map<ColumnName, ColumnType> columnDefinitions) {
+        return columnDefinitions.keySet().stream()
+            .map(ColumnName::getColumnName)
+            .filter(columnName -> !columnName.equalsIgnoreCase("id"))
+            .collect(Collectors.joining(", "));
     }
 
 }
