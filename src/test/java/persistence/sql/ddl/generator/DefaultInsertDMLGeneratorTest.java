eec177396cd29b5794464f2067ccf405be1eff24
soora <soora@nexon.co.kr>
2024-10-21 14:16:29 +0900

    test: 테스트코드 보강

--- src/test/java/persistence/sql/ddl/generator/DefaultInsertDMLGeneratorTest.java
+++ src/test/java/persistence/sql/ddl/generator/DefaultInsertDMLGeneratorTest.java
@@ -7,12 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultInsertDMLGeneratorTest {
     @Test
-    void insert_DML을_생성한다() {
+    void 아이디가_없는_insert_DML을_생성한다() {
         Person person = new Person(null, "soora", 10, "soora@naver.com", 5);
         DefaultInsertDMLGenerator generator = new DefaultInsertDMLGenerator();
 
         String dml = generator.generate(person);
 
-        assertThat(dml).isEqualTo("INSERT INTO users (id,nick_name,old,email) values (null,'soora','10','soora@naver.com');");
+        assertThat(dml).isEqualTo("INSERT INTO users (nick_name,old,email) values ('soora','10','soora@naver.com');");
+    }
+
+    @Test
+    void 아이디가_있는_insert_DML을_생성한다() {
+        Person person = new Person(15L, "soora", 10, "soora@naver.com", 5);
+        DefaultInsertDMLGenerator generator = new DefaultInsertDMLGenerator();
+
+        String dml = generator.generate(person);
+
+        assertThat(dml).isEqualTo("INSERT INTO users (id,nick_name,old,email) values ('15','soora','10','soora@naver.com');");
     }
 }
