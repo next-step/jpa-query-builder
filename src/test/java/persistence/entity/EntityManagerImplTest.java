2b16e8a553f631057aeba884a707cdd1563d5db2
seungnong <tmddnrdl333@naver.com>
2024-10-23 01:07:21 +0900

    feat: EntityManager persist 에 업데이트 구현

--- src/test/java/persistence/entity/EntityManagerImplTest.java
+++ src/test/java/persistence/entity/EntityManagerImplTest.java
@@ -17,7 +17,7 @@ public class EntityManagerImplTest {
 
     @Test
     @DisplayName("USERS 테이블 생성 > 데이터 저장 > 조회 테스트")
-    void persistAndFindTest() throws SQLException {
+    void persistAndFindAndRemoveTest() throws SQLException {
         final DatabaseServer server = new H2();
         server.start();
 
@@ -41,5 +41,41 @@ public class EntityManagerImplTest {
         logger.debug("Found : {}", found);
 
         entityManagerImpl.remove(found);
+    }
+
+    @Test
+    @DisplayName("USERS 테이블 생성 > 데이터 저장 > 조회 테스트")
+    void persistAndUpdateTest() throws SQLException {
+        final DatabaseServer server = new H2();
+        server.start();
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
+
+        EntityManagerImpl<Person, Long> entityManagerImpl = new EntityManagerImpl<>(jdbcTemplate);
+
+        EntityScanner entityScanner = new EntityScanner();
+        entityScanner.scan("example.entity");
+        entityScanner.getDdlCreateQueries().forEach(jdbcTemplate::execute);
+
+        Person inserting = new Person();
+        inserting.setName("이름");
+        inserting.setAge(15);
+        inserting.setEmail("이메일@이메일.이메일");
+
+        entityManagerImpl.persist(inserting);
+
+        Person found = entityManagerImpl.find(Person.class, 1L);
+
+        logger.debug("Found : {}", found);
+
+        found.setName("뉴이름");
+        found.setAge(1500);
+        found.setEmail("뉴이메일@뉴이메일.뉴이메일");
+
+        entityManagerImpl.persist(found);
+
+        Person reFound = entityManagerImpl.find(Person.class, 1L);
+
+        logger.debug("Re found : {}", reFound);
     }
 }
