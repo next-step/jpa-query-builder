a96dde3e78a1a9638da94ed41846b910b4021fde
weno <35131529+wenodev@users.noreply.github.com>
2024-10-23 00:25:40 +0900

    feat: introduce field and clean up

--- src/main/java/persistence/sql/dml/DmlQueryBuilder.java
+++ src/main/java/persistence/sql/dml/DmlQueryBuilder.java
@@ -13,6 +13,10 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class DmlQueryBuilder {
+    private static final String SELECT_ALL_TEMPLATE = "SELECT * FROM %s;";
+    private static final String SELECT_BY_ID_TEMPLATE = "SELECT * FROM %s WHERE %s = %s;";
+    private static final String INSERT_TEMPLATE =  "INSERT INTO %s (%s) VALUES (%s);";
+
     private final DatabaseDialect dialect;
 
     public DmlQueryBuilder(final H2Dialect h2Dialect) {
@@ -21,13 +25,13 @@ public class DmlQueryBuilder {
 
     public String select(final Class<?> clazz) {
         final String tableName = new TableName(clazz).value();
-        return "SELECT * FROM %s;".formatted(tableName);
+        return SELECT_ALL_TEMPLATE.formatted(tableName);
     }
 
     public String select(final Class<?> clazz, final Long id) {
         final String tableName = new TableName(clazz).value();
         final String idColumnName = getIdColumnName(clazz);
-        return "SELECT * FROM %s WHERE %s = %s;".formatted(
+        return SELECT_BY_ID_TEMPLATE.formatted(
                 tableName,
                 idColumnName,
                 formatValue(id)
@@ -47,8 +51,7 @@ public class DmlQueryBuilder {
         final String columns = columnsClause(object.getClass());
         final String values = valueClause(object);
 
-        return String.format("INSERT INTO %s (%s) VALUES (%s);",
-                tableName, columns, values);
+        return String.format(INSERT_TEMPLATE, tableName, columns, values);
     }
 
     private String columnsClause(final Class<?> clazz) {
@@ -95,8 +98,8 @@ public class DmlQueryBuilder {
     private String formatValue(final Object value) {
         return switch (value) {
             case null -> "NULL";
-            case String s -> String.format("'%s'", s.replace("'", "''"));
-            case Number number -> value.toString();
+            case final String s -> String.format("'%s'", s.replace("'", "''"));
+            case final Number number -> value.toString();
             default -> String.format("'%s'", value);
         };
     }
