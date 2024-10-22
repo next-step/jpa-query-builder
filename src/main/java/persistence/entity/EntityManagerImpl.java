5084cb63ab7f20d85b813a9d520663da1904a4d6
seungnong <tmddnrdl333@naver.com>
2024-10-23 00:55:31 +0900

    feat: UpdateQueryBuilder 구현

--- src/main/java/persistence/entity/EntityManagerImpl.java
+++ src/main/java/persistence/entity/EntityManagerImpl.java
@@ -2,15 +2,11 @@ package persistence.entity;
 
 import jdbc.EntityRowMapper;
 import jdbc.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import persistence.sql.dml.delete.DeleteQueryBuilder;
 import persistence.sql.dml.insert.InsertQueryBuilder;
 import persistence.sql.dml.select.SelectQueryBuilder;
 
 public class EntityManagerImpl<T, U> implements EntityManager<T, U> {
-    private static final Logger logger = LoggerFactory.getLogger(EntityManagerImpl.class);
-
     private final JdbcTemplate jdbcTemplate;
 
     public EntityManagerImpl(JdbcTemplate jdbcTemplate) {
